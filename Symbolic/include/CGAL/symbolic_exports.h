69a24c95d2257987318b336095b28ff9d3d3c0e8
Pavel Emeliyanenko <asm@mpi-sb.mpg.de>
2011-05-03 16:25:20 +0000

    up

--- Symbolic/include/CGAL/symbolic_exports.h
+++ Symbolic/include/CGAL/symbolic_exports.h
@@ -7,11 +7,7 @@ namespace CGAL {
 
 namespace internal {
 
-#if (defined CGAL_GPU_SYMBOLIC_SUPPORTED)
-
-// template < class Coeff >
-// Coeff resultant_gpu(const CGAL::Polynomial<Coeff>& F_,
-//                     const CGAL::Polynomial<Coeff>& G_);
+#if (defined CGAL_USE_GPU)
 
 #ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
 
@@ -31,6 +27,7 @@ CGAL::Polynomial< CORE::BigInt > resultant(
 
 // partial specialization to enable gpu'ed gcds
 #ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
+
 template <> 
 CGAL::Polynomial< CGAL::Gmpz > gcd_(
         const CGAL::Polynomial< CGAL::Gmpz >& F_,
@@ -43,19 +40,46 @@ CGAL::Polynomial< CGAL::Gmpz > gcd_utcf_(
 #endif
 
 #ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
-template <> 
+
+template <>
 CGAL::Polynomial< CORE::BigInt > gcd_(
         const CGAL::Polynomial< CORE::BigInt >& F_,
         const CGAL::Polynomial< CORE::BigInt >& G_);
 
-template <> inline
+template <> 
 CGAL::Polynomial< CORE::BigInt > gcd_utcf_(
         const CGAL::Polynomial< CORE::BigInt >& F_,
         const CGAL::Polynomial< CORE::BigInt >& G_);
+
 #endif
 
-#endif // CGAL_GPU_SYMBOLIC_SUPPORTED
+#endif // CGAL_USE_GPU
 
 } // namespace internal
+
+// get rid of modular stuff
+#if (defined CGAL_USE_GPU || defined CGAL_USE_NTL)
+
+#ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
+template <> inline
+CGAL::Polynomial< CGAL::Gmpz > gcd(
+        const CGAL::Polynomial< CGAL::Gmpz >& F_,
+        const CGAL::Polynomial< CGAL::Gmpz >& G_) {
+    return gcd_(F_,G_);
+}
+#endif
+
+#ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
+
+template <> inline
+CGAL::Polynomial< CORE::BigInt > gcd(
+        const CGAL::Polynomial< CORE::BigInt >& F_,
+        const CGAL::Polynomial< CORE::BigInt >& G_) {
+    return gcd_(F_,G_);
+}
+
+#endif
+
+#endif  // CGAL_USE_??
 
 } // namespace CGAL
