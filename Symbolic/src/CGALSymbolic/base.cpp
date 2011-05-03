e38133faa455be6a9089b42ebe7509b768a8f0f2
Pavel Emeliyanenko <asm@mpi-sb.mpg.de>
2011-05-03 16:25:00 +0000

    up

--- Symbolic/src/CGALSymbolic/base.cpp
+++ Symbolic/src/CGALSymbolic/base.cpp
@@ -135,7 +135,7 @@ Poly gcd_gpu(const Poly& F_, const Poly& G_) {
 
 // partial specialization to enable gpu'ed gcds
 #ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
-template <> inline
+template <> 
 CGAL::Polynomial< CGAL::Gmpz > gcd_(
         const CGAL::Polynomial< CGAL::Gmpz >& F_,
         const CGAL::Polynomial< CGAL::Gmpz >& G_) {
@@ -143,7 +143,7 @@ CGAL::Polynomial< CGAL::Gmpz > gcd_(
     return gcd_gpu(F_, G_);
 }
 
-template <> inline
+template <> 
 CGAL::Polynomial< CGAL::Gmpz > gcd_utcf_(
         const CGAL::Polynomial< CGAL::Gmpz >& F_,
         const CGAL::Polynomial< CGAL::Gmpz >& G_) {
@@ -153,7 +153,8 @@ CGAL::Polynomial< CGAL::Gmpz > gcd_utcf_(
 #endif
 
 #ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
-template <> inline
+
+template <>
 CGAL::Polynomial< CORE::BigInt > gcd_(
         const CGAL::Polynomial< CORE::BigInt >& F_,
         const CGAL::Polynomial< CORE::BigInt >& G_) {
@@ -161,7 +162,7 @@ CGAL::Polynomial< CORE::BigInt > gcd_(
     return gcd_gpu(F_, G_);
 }
 
-template <> inline
+template <> 
 CGAL::Polynomial< CORE::BigInt > gcd_utcf_(
         const CGAL::Polynomial< CORE::BigInt >& F_,
         const CGAL::Polynomial< CORE::BigInt >& G_) {
@@ -171,6 +172,192 @@ CGAL::Polynomial< CORE::BigInt > gcd_utcf_(
 #endif
 
 #endif // CGAL_USE_GPU
+
+#if (defined CGAL_USE_NTL)
+
+struct NTL_bigint_rep {
+    long alloc;
+    long size;
+    mp_limb_t data;
+};
+
+template < class _ >
+struct ___ {
+// dummy
+};
+
+#ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
+
+template < >
+struct ___< CORE::BigInt > {
+
+    typedef CORE::BigInt Integer;
+
+    inline mpz_srcptr operator()(const Integer& x) const {
+        return x.get_mp();
+    }
+
+    inline Integer operator()(mpz_srcptr z) const {
+        return Integer(z);
+    }
+};
+
+#endif // CGAL_HAS_CORE_ARITHMETIC_KERNEL
+
+#ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
+
+template < >
+struct ___< CGAL::Gmpz > {
+
+    typedef CGAL::Gmpz Integer;
+
+    inline mpz_srcptr operator()(const Integer& x) const {
+        return x.mpz();
+    }
+
+    inline Integer operator()(mpz_srcptr z) const {
+        return Integer(z);
+    }
+};
+
+#endif // CGAL_HAS_GMP_ARITHMETIC_KERNEL
+
+template < class NT >
+void poly2ntl(const Polynomial< NT >& p, NTL::ZZX& q) {
+
+    q.rep.SetLength(p.degree() + 1);
+    ___< NT > cvt; // instantiate secret object
+    
+    int i;
+    typename Polynomial< NT >::const_iterator pit;
+    for(i = 0, pit = p.begin(); pit != p.end(); pit++, i++) {
+
+        NTL::ZZ& zz = q.rep[i];
+        mpz_srcptr tmp = cvt(*pit);
+        int sz = tmp->_mp_size;
+        if(sz == 0)
+            continue;
+        if(sz < 0)
+            sz = -sz;
+        
+        zz.SetSize(sz);
+        NTL_bigint_rep *rep = (NTL_bigint_rep *)zz.rep;
+        rep->size = tmp->_mp_size;
+        // copy limbs directly
+        memcpy(&rep->data, tmp->_mp_d, sz*sizeof(mp_limb_t));
+    }
+}
+
+template < class NT >
+Polynomial< NT > ntl2poly(const NTL::ZZX& r) {
+
+    typedef Polynomial< NT > Poly_1;
+
+    int d = NTL::deg(r);
+    typename Poly_1::Vector v(d + 1);
+    
+    mpz_t tmp;
+    mpz_init(tmp);
+    for(int i = 0; i <= d; i++) {
+        
+        const NTL::ZZ& zz = r.rep[i];
+        if(NTL::IsZero(zz)) {
+            v[i] = NT(0);
+            continue;
+        } 
+
+        NTL_bigint_rep *rep = (NTL_bigint_rep *)zz.rep;
+        int sz = rep->size;
+        if(sz < 0)
+            sz = -sz;
+         
+        mpz_realloc2(tmp, sz * GMP_NUMB_BITS);
+        tmp->_mp_size = rep->size;
+        memcpy(tmp->_mp_d, &rep->data, sz*sizeof(mp_limb_t));
+         
+        v[i] = NT(tmp);
+    }
+    mpz_clear(tmp);
+    return (Poly_1(v.begin(), v.end()));
+}
+
+template < class NT > inline
+Polynomial< NT > gcd_NTL(const Polynomial< NT >& p1,
+        const Polynomial< NT >& p2) {
+
+    typedef Polynomial< NT > Poly_1;
+
+    if(p1 == Poly_1(0))
+        return p2;
+    if(p2 == Poly_1(0))
+        return p1;
+
+    NTL::ZZX r, q1, q2;
+    poly2ntl(p1, q1);
+    poly2ntl(p2, q2);
+
+    NTL::GCD(r, q1, q2);
+    return ntl2poly< NT >(r);
+}
+
+template < class NT > inline
+void Polynomial< NT >::div_NTL(
+    const Polynomial< NT >& f, const Polynomial< NT >& g,
+        Polynomial< NT >& q, Polynomial< NT >& r) {
+
+    NTL::ZZX q1, r1, f1, g1;
+    internal::poly2ntl(f, f1);
+    internal::poly2ntl(g, g1);
+
+    NTL::DivRem(q1, r1, f1, g1);
+
+    q = internal::ntl2poly< NT >(q1);
+    r = internal::ntl2poly< NT >(r1);
+}
+
+#ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
+
+template < >
+void Polynomial< CGAL::Gmpz >::euclidean_division(
+    const Polynomial< CGAL::Gmpz >& f, const Polynomial< CGAL::Gmpz >& g,
+    Polynomial< CGAL::Gmpz >& q, Polynomial< CGAL::Gmpz >& r) {
+
+    div_NTL(f, g, q, r);
+}
+
+#if !(defined CGAL_USE_GPU) // give preference to gpu impl
+template <> 
+Polynomial< CGAL::Gmpz > gcd_(const Polynomial< CGAL::Gmpz >& p1,
+            const Polynomial< CGAL::Gmpz >& p2) {
+
+    return gcd_NTL(p1, p2);
+}
+#endif
+
+#endif // CGAL_HAS_GMP_ARITHMETIC_KERNEL
+
+#ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
+
+template < >
+void Polynomial< CORE::BigInt >::euclidean_division(
+    const Polynomial< CORE::BigInt >& f, const Polynomial< CORE::BigInt >& g,
+    Polynomial< CORE::BigInt >& q, Polynomial< CORE::BigInt >& r) {
+
+        div_NTL(f, g, q, r);
+}
+
+#if !(defined CGAL_USE_GPU) // give preference to gpu impl
+template <> 
+Polynomial< CORE::BigInt > gcd_(const Polynomial< CORE::BigInt >& p1,
+            const Polynomial< CORE::BigInt >& p2) {
+
+    return gcd_NTL(p1, p2);
+}
+#endif
+
+#endif // CGAL_HAS_CORE_ARITHMETIC_KERNEL
+
+#endif // CGAL_USE_NTL
 
 } // namespace internal
 
