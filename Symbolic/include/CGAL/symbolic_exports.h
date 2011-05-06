cf079709ef57145755531267adfc890a750aa480
Pavel Emeliyanenko <asm@mpi-sb.mpg.de>
2011-05-06 08:57:14 +0000

    removed unnecessary definitions

--- Symbolic/include/CGAL/symbolic_exports.h
+++ Symbolic/include/CGAL/symbolic_exports.h
@@ -57,29 +57,47 @@ CGAL::Polynomial< CORE::BigInt > gcd_utcf_(
 
 } // namespace internal
 
-// get rid of modular stuff
-#if (defined CGAL_USE_GPU || defined CGAL_USE_NTL)
-
 #ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
-template <> inline
-CGAL::Polynomial< CGAL::Gmpz > gcd(
-        const CGAL::Polynomial< CGAL::Gmpz >& F_,
-        const CGAL::Polynomial< CGAL::Gmpz >& G_) {
-    return gcd_(F_,G_);
-}
-#endif
+
+template <>
+void Polynomial< CGAL::Gmpz >::euclidean_division(
+    const Polynomial< CGAL::Gmpz >& f, const Polynomial< CGAL::Gmpz >& g,
+    Polynomial< CGAL::Gmpz >& q, Polynomial< CGAL::Gmpz >& r);
+
+#endif // CGAL_HAS_GMP_ARITHMETIC_KERNEL
 
 #ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
 
-template <> inline
-CGAL::Polynomial< CORE::BigInt > gcd(
-        const CGAL::Polynomial< CORE::BigInt >& F_,
-        const CGAL::Polynomial< CORE::BigInt >& G_) {
-    return gcd_(F_,G_);
-}
+template <>
+void Polynomial< CORE::BigInt >::euclidean_division(
+    const Polynomial< CORE::BigInt >& f, const Polynomial< CORE::BigInt >& g,
+    Polynomial< CORE::BigInt >& q, Polynomial< CORE::BigInt >& r);
 
 #endif
 
-#endif  // CGAL_USE_??
+// get rid of modular stuff
+// #if (defined CGAL_USE_GPU || defined CGAL_USE_NTL)
+// 
+// #ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
+// template <> inline
+// CGAL::Polynomial< CGAL::Gmpz > gcd(
+//         const CGAL::Polynomial< CGAL::Gmpz >& F_,
+//         const CGAL::Polynomial< CGAL::Gmpz >& G_) {
+//     return gcd_(F_,G_);
+// }
+// #endif
+// 
+// #ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
+// 
+// template <> inline
+// CGAL::Polynomial< CORE::BigInt > gcd(
+//         const CGAL::Polynomial< CORE::BigInt >& F_,
+//         const CGAL::Polynomial< CORE::BigInt >& G_) {
+//     return gcd_(F_,G_);
+// }
+// 
+// #endif
+// 
+// #endif  // CGAL_USE_??
 
 } // namespace CGAL
