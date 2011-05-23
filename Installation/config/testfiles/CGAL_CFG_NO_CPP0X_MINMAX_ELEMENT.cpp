5598e2d24828c1a828d1b0fbc51fc0da53e22557
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2011-05-23 16:18:00 +0000

    Added a testfile to check for std::minmax_element

---
+++ Installation/config/testfiles/CGAL_CFG_NO_CPP0X_MINMAX_ELEMENT.cpp
@@ -0,0 +1,14 @@
+//| If a compiler does not support C++0x lambdas
+//| CGAL_CFG_NO_CPP0X_MINMAX_ELEMENT is set. 
+
+#undef NDEBUG
+#include <algorithm> 
+#include <cassert>
+
+int main()
+{
+  int f[5] = {1, 2, 3, 4, 5};
+  std::pair<int*, int*> ret = std::minmax_element(f, f + 5);
+  assert(*(ret.first) == 1);
+  assert(*(ret.second) == 5);
+}
