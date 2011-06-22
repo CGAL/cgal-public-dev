ad1f3630d157b75299011fa025616bc57d26f64b
Eric Berberich <eric.berberich@cgal.org>
2011-06-22 09:40:17 +0000

    refactored some code, added factorize function

--- Symbolic/src/CGALSymbolic/symbolic.cpp
+++ Symbolic/src/CGALSymbolic/symbolic.cpp
@@ -5,10 +5,10 @@
 #define CGAL_BISOLVE_CHECK_GPU_RESULTANTS_SANITY 0 // default 0
 #define CGAL_BISOLVE_CHECK_GPU_GCDS_SANITY 0 // default 1
 
+#include <CGAL/symbolic_exports.h>
+
 #include <CGAL/Arithmetic_kernel.h>
 #include <CGAL/Polynomial.h>
-
-#include <CGAL/symbolic_exports.h>
 
 #if (defined CGAL_USE_GPU)
 #include <CGAL/GPU_algorithms/GPU_algorithm_facade.h>
@@ -16,6 +16,7 @@
 
 #if (defined CGAL_USE_NTL)
 #include <NTL/ZZX.h>
+#include <NTL/ZZXFactoring.h>
 #endif 
 
 namespace CGAL {
@@ -179,112 +180,6 @@ CGAL::Polynomial< CORE::BigInt > gcd_utcf_(
 #endif // CGAL_USE_GPU
 
 #if (defined CGAL_USE_NTL)
-
-struct NTL_bigint_rep {
-    long alloc;
-    long size;
-    mp_limb_t data;
-};
-
-template < class _ >
-struct ___ {
-// dummy
-};
-
-#ifdef CGAL_HAS_CORE_ARITHMETIC_KERNEL
-
-template < >
-struct ___< CORE::BigInt > {
-
-    typedef CORE::BigInt Integer;
-
-    inline mpz_srcptr operator()(const Integer& x) const {
-        return x.get_mp();
-    }
-
-    inline Integer operator()(mpz_srcptr z) const {
-        return Integer(z);
-    }
-};
-
-#endif // CGAL_HAS_CORE_ARITHMETIC_KERNEL
-
-#ifdef CGAL_HAS_GMP_ARITHMETIC_KERNEL
-
-template < >
-struct ___< CGAL::Gmpz > {
-
-    typedef CGAL::Gmpz Integer;
-
-    inline mpz_srcptr operator()(const Integer& x) const {
-        return x.mpz();
-    }
-
-    inline Integer operator()(mpz_srcptr z) const {
-        return Integer(z);
-    }
-};
-
-#endif // CGAL_HAS_GMP_ARITHMETIC_KERNEL
-
-template < class NT >
-void poly2ntl(const Polynomial< NT >& p, NTL::ZZX& q) {
-
-    q.rep.SetLength(p.degree() + 1);
-    ___< NT > cvt; // instantiate secret object
-    
-    int i;
-    typename Polynomial< NT >::const_iterator pit;
-    for(i = 0, pit = p.begin(); pit != p.end(); pit++, i++) {
-
-        NTL::ZZ& zz = q.rep[i];
-        mpz_srcptr tmp = cvt(*pit);
-        int sz = tmp->_mp_size;
-        if(sz == 0)
-            continue;
-        if(sz < 0)
-            sz = -sz;
-        
-        zz.SetSize(sz);
-        NTL_bigint_rep *rep = (NTL_bigint_rep *)zz.rep;
-        rep->size = tmp->_mp_size;
-        // copy limbs directly
-        memcpy(&rep->data, tmp->_mp_d, sz*sizeof(mp_limb_t));
-    }
-}
-
-template < class NT >
-Polynomial< NT > ntl2poly(const NTL::ZZX& r) {
-
-    typedef Polynomial< NT > Poly_1;
-
-    int d = NTL::deg(r);
-    typename Poly_1::Vector v(d + 1);
-    
-    mpz_t tmp;
-    mpz_init(tmp);
-    for(int i = 0; i <= d; i++) {
-        
-        const NTL::ZZ& zz = r.rep[i];
-        if(NTL::IsZero(zz)) {
-            v[i] = NT(0);
-            continue;
-        } 
-
-        NTL_bigint_rep *rep = (NTL_bigint_rep *)zz.rep;
-        int sz = rep->size;
-        if(sz < 0)
-            sz = -sz;
-         
-        mpz_realloc2(tmp, sz * GMP_NUMB_BITS);
-        tmp->_mp_size = rep->size;
-        memcpy(tmp->_mp_d, &rep->data, sz*sizeof(mp_limb_t));
-         
-        v[i] = NT(tmp);
-    }
-    mpz_clear(tmp);
-    return (Poly_1(v.begin(), v.end()));
-}
 
 template < class NT > inline
 Polynomial< NT > gcd_NTL(const Polynomial< NT >& p1,
