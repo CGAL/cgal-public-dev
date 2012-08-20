c8eb7a5fae79c61561e79d2b2e9122bc483cf4d3
Pavel Emeliyanenko <asm@mpi-sb.mpg.de>
2012-08-20 15:03:05 +0000

    update`

--- Polynomial/examples/Polynomial/mixed_floating_point_parser_policy.cpp
+++ Polynomial/examples/Polynomial/mixed_floating_point_parser_policy.cpp
@@ -1,5 +1,5 @@
 
-#include <CGAL/basic.h>
+#include <CGAL/config.h>
 #include <CGAL/Polynomial/Polynomial_parser_d.h>
 #include <CGAL/Polynomial.h>
 #include <CGAL/Polynomial_type_generator.h>
@@ -10,14 +10,14 @@ int main() {
    typedef typename CGAL::Polynomial_type_generator<CGAL::Gmpq,2>::Type 
       Polynomial_2;
     
-      // parse bivariate polynomials with large integer and rational coefficients
+      // parse bivariate polynomials with large integer, rational and fp coefficients
    CGAL::Polynomial_parser_d<Polynomial_2,
-            CGAL::Mixed_rational_parser_policy< Polynomial_2 > > bunny_2;
+            CGAL::Mixed_floating_point_parser_policy< Polynomial_2 > > parser;
   
-   std::string str="x^5*8345345/3234234234*y^17-5/7*(-111345345y-x^2)+10";
+   std::string str="-13.003274*(x+y)^2-123.123e-10123x+112/233*(y+x^2-2323)^3";
     
    Polynomial_2 p2;
-   bunny_2(str, p2);
+   parser(str, p2);
    
    std::cout << "p2: " << p2 << "\n\n";
    return 1;
