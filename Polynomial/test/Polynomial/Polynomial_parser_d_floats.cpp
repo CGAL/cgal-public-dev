6048799abf595eac34408e46a655f373ca849caa
Eric Berberich <eric.berberich@cgal.org>
2012-09-04 11:19:07 +0000

    moved

---
+++ Polynomial/test/Polynomial/Polynomial_parser_d_floats.cpp
@@ -0,0 +1,23 @@
+// TODO: Add licence
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Eric Berberich <eric@mpi-inf.mpg.de> 
+//
+// ============================================================================
+
+#ifndef CGAL_POLYNOMIAL_PARSE_FLOATING_POINT
+#define CGAL_POLYNOMIAL_PARSE_FLOATING_POINT 1
+#endif
+
+#include "Polynomial_parser_d.cpp"
+
+
+#if 0
+int main() {} // make cgal_create_cmake_script happy
+#endif
