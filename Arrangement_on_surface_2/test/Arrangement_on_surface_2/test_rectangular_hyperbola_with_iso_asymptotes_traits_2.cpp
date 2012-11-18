0f38f26c919e95abf03d6ccbbf75fa323c788bae
Philipp Möller <bootsarehax@googlemail.com>
2012-11-18 13:13:45 +0100

    Delete the failed attempt at Rectangular_hyperbola

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_rectangular_hyperbola_with_iso_asymptotes_traits_2.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <CGAL/Arr_rectangular_hyperbola_with_iso_asymptotes_traits_2.h> 
-#include <CGAL/Arrangement_on_surface_2.h> 
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h> 
-
-
-int main(){return 0;}
