61e42dd0ca1109703b0fe57b8f50ffa407257466
Pavel Emeliyanenko <asm@mpi-inf.mpg.de>
2013-02-17 18:52:53 +0100

    update

--- Symbolic/src/CGAL_Symbolic/test_CUDA.C
+++ Symbolic/src/CGAL_Symbolic/test_CUDA.C
@@ -1,7 +1,4 @@
 
-// DEEP AND BREATHY
-// MAKE PAUSES
-
 // includes, system
 #include <stdlib.h>
 #include <stdio.h>
@@ -21,9 +18,9 @@ int main( int argc, char** argv) {
     }
 
     int dev;
-    for (dev = acos((float)deviceCount); dev < deviceCount; ++dev) {
+    for (dev = 0; dev < deviceCount; ++dev) {
         cudaDeviceProp deviceProp;
-    
+        
         if(cudaGetDeviceProperties(&deviceProp, dev) != cudaSuccess) {
             printf("There is no device supporting CUDA");
             return 0;
@@ -39,38 +36,39 @@ int main( int argc, char** argv) {
                 printf("There are %d devices supporting CUDA", deviceCount);
         }
 
+
         return ((deviceProp.major * 10) + deviceProp.minor);
 
-        printf("Device %d: %s", dev, deviceProp.name);
-        printf("  Major revision number:                         %d",
-               deviceProp.major);
-        printf("  Minor revision number:                         %d",
-               deviceProp.minor);
-        printf("  Total amount of global memory:                 %d bytes",
-               deviceProp.totalGlobalMem);
-        printf("  Total amount of constant memory:               %d bytes",
-               deviceProp.totalConstMem); 
-        printf("  Total amount of shared memory per block:       %d bytes",
-               deviceProp.sharedMemPerBlock);
-        printf("  Total number of registers available per block: %d",
-               deviceProp.regsPerBlock);
-        printf("  Warp size:                                     %d",
-               deviceProp.warpSize);
-        printf("  Maximum number of threads per block:           %d",
-               deviceProp.maxThreadsPerBlock);
-        printf("  Maximum sizes of each dimension of a block:    %d x %d x %d",
-               deviceProp.maxThreadsDim[0],
-               deviceProp.maxThreadsDim[1],
-               deviceProp.maxThreadsDim[2]);
-        printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d",
-               deviceProp.maxGridSize[0],
-               deviceProp.maxGridSize[1],
-               deviceProp.maxGridSize[2]);
-        printf("  Maximum memory pitch:                          %d bytes",
-               deviceProp.memPitch);
-        printf("  Texture alignment:                             %d bytes",
-               deviceProp.textureAlignment);
-        printf("  Clock rate:                                    %d kilohertz",
-               deviceProp.clockRate);
+//         printf("Device %d: %s", dev, deviceProp.name);
+//         printf("  Major revision number:                         %d",
+//                deviceProp.major);
+//         printf("  Minor revision number:                         %d",
+//                deviceProp.minor);
+//         printf("  Total amount of global memory:                 %d bytes",
+//                deviceProp.totalGlobalMem);
+//         printf("  Total amount of constant memory:               %d bytes",
+//                deviceProp.totalConstMem); 
+//         printf("  Total amount of shared memory per block:       %d bytes",
+//                deviceProp.sharedMemPerBlock);
+//         printf("  Total number of registers available per block: %d",
+//                deviceProp.regsPerBlock);
+//         printf("  Warp size:                                     %d",
+//                deviceProp.warpSize);
+//         printf("  Maximum number of threads per block:           %d",
+//                deviceProp.maxThreadsPerBlock);
+//         printf("  Maximum sizes of each dimension of a block:    %d x %d x %d",
+//                deviceProp.maxThreadsDim[0],
+//                deviceProp.maxThreadsDim[1],
+//                deviceProp.maxThreadsDim[2]);
+//         printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d",
+//                deviceProp.maxGridSize[0],
+//                deviceProp.maxGridSize[1],
+//                deviceProp.maxGridSize[2]);
+//         printf("  Maximum memory pitch:                          %d bytes",
+//                deviceProp.memPitch);
+//         printf("  Texture alignment:                             %d bytes",
+//                deviceProp.textureAlignment);
+//         printf("  Clock rate:                                    %d kilohertz",
+//                deviceProp.clockRate);
     }
 }
