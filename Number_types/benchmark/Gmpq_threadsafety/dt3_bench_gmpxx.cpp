2c559678295a6ed1ee29bb668f637c9da1028a90
Sébastien Loriot <sebastien.loriot@cgal.org>
2013-02-18 09:20:14 +0100

    add a benchmark with gmpxx

---
+++ Number_types/benchmark/Gmpq_threadsafety/dt3_bench_gmpxx.cpp
@@ -0,0 +1,39 @@
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/gmpxx.h>
+#include <CGAL/Timer.h>
+#include <CGAL/point_generators_3.h>
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+typedef CGAL::Simple_cartesian<mpq_class> Kernel;
+typedef CGAL::Delaunay_triangulation_3<Kernel> DT3;
+typedef CGAL::Creator_uniform_3<mpq_class,Kernel::Point_3>  Creator;
+
+
+int main(int argc,char** argv)
+{
+  std::vector<Kernel::Point_3> points;
+  CGAL::Random rand(0);
+  CGAL::Random_points_in_sphere_3<Kernel::Point_3,Creator> g(1,rand);
+  
+  int nb_pts = 1000;
+  if (argc!=1)
+    nb_pts = atoi(argv[1]);
+  
+  std::cout << "Using " << nb_pts << " input points\n";
+  
+  
+  CGAL::cpp11::copy_n( g, nb_pts, std::back_inserter(points));  
+  
+  CGAL::Timer timer;
+  
+  timer.start();
+  DT3 dt3(points.begin(),points.end());
+  timer.stop();
+  std::cout << "Insert "<<  dt3.number_of_vertices() << " pts in "
+            << timer.time() << "s.\n";
+  
+}
