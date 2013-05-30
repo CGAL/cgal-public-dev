7d13e8d3f4ef9359559ba05f007c39ebf00ac705
Eric Berberich <eric.berberich@cgal.org>
2013-05-30 11:26:36 +0200

    first step to make notifications working for all platforms
    Idea: check directory, use the respective yml file

--- post-receive-git-commit-notifier.sh
+++ post-receive-git-commit-notifier.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/var/lib/gems/1.8/bin/git-commit-notifier /var/git/cgal.git/hooks/cgal-git-commit-notifier.yml
+PWD=$(pwd); echo ${PWD:9}
+/var/lib/gems/1.8/bin/git-commit-notifier hooks/git-commit-notifier/${PWD:9}.yml
