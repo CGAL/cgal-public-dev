ebe0ebaee09eb5ab2b97abedafcbe92b53e1eb45
Eric Berberich <eric.berberich@cgal.org>
2013-05-30 20:00:03 +0200

    use the own clone (with branch CGAL) instead of the ruby gem

--- post-receive-git-commit-notifier-grouped.sh
+++ post-receive-git-commit-notifier-grouped.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PWD=$(pwd)
-/var/lib/gems/1.8/bin/git-commit-notifier hooks/git-commit-notifier/${PWD:9}-grouped.yml
+/var/local/git-commit-notifier/bin/git-commit-notifier hooks/git-commit-notifier/${PWD:9}-grouped.yml
