b6b0cfa5153a8ee7d598f265212b8cfe3e7785bc
Chamra Jayalath <jaya.chamara@gmail.com>
2013-07-10 15:59:51 +0200

    The class AABB_geom_traits

--- AABB_tree/include/CGAL/AABB_geom_traits.h
+++ AABB_tree/include/CGAL/AABB_geom_traits.h
@@ -1,9 +1,23 @@
-/*
- * AABB_geomtraits.h
- *
- *  Created on: 5 juil. 2013
- *      Author: Mehdious
- */
+// Copyright (c) 2009 INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+//
+// Author(s) :
+//
 
 #ifndef AABB_GEOMTRAITS_H_
 #define AABB_GEOMTRAITS_H_
