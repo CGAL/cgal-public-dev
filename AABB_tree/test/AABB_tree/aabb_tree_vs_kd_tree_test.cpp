b0a542880f4ca7fe862ed18cb588d63c8fdb9323
jayalatn <jaya.chamara@gmail.com>
2013-09-03 17:21:59 +0200

    test file to check the performance of aabb tree and kd tree for range queries

--- AABB_tree/test/AABB_tree/aabb_tree_vs_kd_tree_test.cpp
+++ AABB_tree/test/AABB_tree/aabb_tree_vs_kd_tree_test.cpp
@@ -1,57 +1,144 @@
-// Author : Pierre Alliez
+// Author : 
 
 #include <iostream>
 #include <list>
+#include <algorithm>
+#include <exception>
 
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/AABB_tree.h>
 #include <CGAL/AABB_traits.h>
-#include <CGAL/AABB_segment_primitive.h>
+#include <CGAL/AABB_point_primitive.h>
+#include <CGAL/Bbox_3.h>
+
+#include <CGAL/point_generators_2.h>
+#include <CGAL/algorithm.h>
+#include <CGAL/random_selection.h>
+
+//search tree
+#include <CGAL/Cartesian_d.h>
+#include <CGAL/Kd_tree.h>
+#include <CGAL/Fuzzy_sphere.h>
+#include <CGAL/Fuzzy_iso_box.h>
+#include <CGAL/Search_traits_2.h>
+#include <CGAL/Timer.h>
 
 typedef CGAL::Simple_cartesian<double> K;
 
-typedef K::FT FT;
-typedef K::Point_3 Point;
-typedef K::Plane_3 Plane;
-typedef K::Segment_3 Segment;
-typedef K::Triangle_3 Triangle;
+//typedefs search tree
+typedef K::Point_2 Point_d;
+typedef CGAL::Search_traits_2<K> Traits_Search;
+typedef CGAL::Kd_tree<Traits_Search> Tree1;
+typedef CGAL::Fuzzy_sphere<Traits_Search> Fuzzy_sphere;
 
-typedef std::list<Segment>::iterator Iterator;
-typedef CGAL::AABB_segment_primitive<K, Iterator> Primitive;
+//tydefs AABB Tree
+typedef K::FT FT;
+typedef K::Point_2 Point;
+typedef K::Circle_2 Circle;
+
+typedef std::list<Point>::iterator Iterator;
+typedef CGAL::AABB_point_primitive<K, Iterator> Primitive;
 typedef CGAL::AABB_traits<K, Primitive> Traits;
 typedef CGAL::AABB_tree<Traits> Tree;
 
+//tepedefs point generator
+typedef CGAL::Creator_uniform_2<double,Point>  Creator;
+
+template<typename LIST>
+void test_spatial_search(LIST &lst)
+{
+    
+
+	Tree1 search_tree(lst.begin(),lst.end());
+	lst.clear();
+
+	std::size_t number_of_points = 0;
+	CGAL::Timer timer;
+	for(int i=0;i<1000;i++)
+	{
+	   LIST itr;
+	   Fuzzy_sphere fs(Point_d(0.05*i,0.05*i), 10.0, 0.0);
+
+	   timer.start();
+	   search_tree.search(std::back_inserter(itr), fs);
+	   timer.stop();
+	   number_of_points+=itr.size();
+	   itr.clear();
+	}
+	
+	std::cout << "K-d Total Points Searched: "<< number_of_points<< std::endl;
+	std::cout << "Total Time Taken: " <<	timer.time() << std::endl;
+
+	search_tree.clear();
+}
+
+
+
+
+
+
+template<typename LIST>
+void test_aabb_tree(LIST &lst)
+{
+	Tree aabb_tree(lst.begin(), lst.end());
+	aabb_tree.build();
+
+
+	unsigned int number_of_range_queries = 0;
+	std::size_t number_of_points = 0;
+	CGAL::Timer timer;
+	for(int i=0;i<1000;i++)
+	{
+	   std::list<typename Primitive::Id> primitives;
+	   Circle circular_query(Point_d(0.05*i,0.05*i), 10.0*10.0);
+	   timer.start();
+	   aabb_tree.all_contained_primitives(circular_query,std::back_inserter(primitives));
+	   timer.stop();
+	   number_of_points+=primitives.size();
+	   primitives.clear();
+	}
+	
+	std::cout << "AABB Total Points Searched: "<< number_of_points<< std::endl;
+	std::cout << "Total Time Taken: " <<	timer.time() << std::endl;
+
+	aabb_tree.clear();
+}
+
+
+
+
 int main()
 {
-    Point a(1.0, 0.0, 0.0);
-    Point b(0.0, 1.0, 0.0);
-    Point c(0.0, 0.0, 1.0);
-    Point d(0.0, 0.0, 0.0);
+   	
+	
+ 
+	//change the number of points in 10^n
+	int number_of_points;
+	std::cout<<"Enter the number of points to be generated"<<std::endl;
+	std::cin>>number_of_points;
+	
+	std::list<Point> points;
 
-    std::list<Segment> segments;
-    segments.push_back(Segment(a, b));
-    segments.push_back(Segment(a, c));
-    segments.push_back(Segment(c, d));
+	CGAL::Random_points_in_disc_2<Point,Creator> g( 150.0);
+	try
+	{
+		CGAL::cpp11::copy_n( g, number_of_points, std::back_inserter(points));
+		std::cout<<"Created "<<number_of_points<<" points"<<std::endl;
+	}
+	catch (std::bad_alloc& )
+	{
+		std::cerr<<"Can not create "<<number_of_points<<" points"<<std::endl;
+	}
+		
 
-    // constructs the AABB tree and the internal search tree for 
-    // efficient distance computations.
-    Tree tree(segments.begin(), segments.end());
-    tree.accelerate_distance_queries();
 
-    // counts #intersections with a plane query
-    Plane plane_query(a, b, d);
-    std::cout << tree.number_of_intersected_primitives(plane_query)
-        << " intersections(s) with plane" << std::endl;
+	//test_spatial_search<std::list<Point>>(points);
 
-    // counts #intersections with a triangle query
-    Triangle triangle_query(a, b, c);
-    std::cout << tree.number_of_intersected_primitives(triangle_query)
-        << " intersections(s) with triangle" << std::endl;
+	test_aabb_tree<std::list<Point>>(points);
 
-    // computes the closest point from a point query 
-    Point point_query(2.0, 2.0, 2.0);
-    Point closest = tree.closest_point(point_query);
 
-    std::cerr << "closest point is: " << closest << std::endl;
-    return EXIT_SUCCESS;
+	points.clear();
+   	
+	std::cin>>number_of_points;
+	return EXIT_SUCCESS;
 }
