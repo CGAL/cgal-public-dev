02427792ebeb9d9072071ea3e0ec0e18e08911d8
Alexandru Tifrea <alex.tifrea93@gmail.com>
2013-09-04 17:43:13 +0300

    Changes to tester of FSD

--- Generator/test/Generator/test_uniformity_of_discrete_distr.cpp
+++ Generator/test/Generator/test_uniformity_of_discrete_distr.cpp
@@ -80,7 +80,157 @@ CGAL::internal::Weighted_random_generator<Probability_1_generator<int> > Generat
 
 int main()
 {
-	for (int cont = 0; cont < 100; cont++) {
+// Testing for small values of N
+	for (int cont = 1; cont < 10; cont++) {
+		CGAL::Random rand;
+		const int N = cont;
+#ifdef TEST_VERBOSE
+		std::cout << "N = " << N << std::endl;
+#endif
+		const int MIN_POINTS = 1000;
+		const int MAX_POINTS = 1000000;
+		const int number_points = rand.get_int(MIN_POINTS, MAX_POINTS);
+#ifdef TEST_VERBOSE
+		std::cout << "number_points = " << number_points << std::endl;
+#endif
+		for (int cont2 = 1; cont2 < 10; cont2++) {
+			const int parts = cont2;
+			GeneratorWithWeight *containing_structure = new GeneratorWithWeight[N];
+			for (int i = 0; i < N; i++) {
+				int aux = i;
+				Probability_1_generator<int> randGen( aux );
+				GeneratorWithWeight tmp = GeneratorWithWeight (randGen, (double) 1/N);
+				containing_structure[i] = tmp;
+			}
+
+			CGAL::internal::Finite_support_distribution<GeneratorWithWeight
+				> randomGen(containing_structure, N, parts);
+
+			int *ret = (int *) calloc(N, sizeof(int));
+			for (int i = 0; i < number_points; i++) {
+				int index = randomGen.generate(rand);
+				assert(inside_generation_range(0, N, index));
+				ret[index]++;
+			}
+
+#ifdef TEST_VERBOSE
+			for(int i = 0; i < N; i++) {
+				std::cout << ret[i] << std::endl;
+			}
+#endif
+	//		int avg = 0;
+	//		for(int i = 0; i < N; i++) {
+	//			avg += ret[i];
+	//		}
+	//		avg = avg / N;
+	//		assert(is_uniform(ret, N, avg));
+
+			assert(all_posibilities_covered(ret, N));
+			free(ret);
+		}
+	}
+
+// Testing for small values of parts and N
+	for (int cont = 1; cont < 10; cont++) {
+		CGAL::Random rand;
+		const int N = cont;
+#ifdef TEST_VERBOSE
+		std::cout << "N = " << N << std::endl;
+#endif
+		const int MIN_POINTS = 1000;
+		const int MAX_POINTS = 1000000;
+		const int number_points = rand.get_int(MIN_POINTS, MAX_POINTS);
+#ifdef TEST_VERBOSE
+		std::cout << "number_points = " << number_points << std::endl;
+#endif
+		const int MIN_PARTS = 1;
+		const int MAX_PARTS = 1<<20;
+		const int parts = rand.get_int(MIN_PARTS, MAX_PARTS);
+		GeneratorWithWeight *containing_structure = new GeneratorWithWeight[N];
+		for (int i = 0; i < N; i++) {
+			int aux = i;
+			Probability_1_generator<int> randGen( aux );
+			GeneratorWithWeight tmp = GeneratorWithWeight (randGen, (double) 1/N);
+			containing_structure[i] = tmp;
+		}
+
+		CGAL::internal::Finite_support_distribution<GeneratorWithWeight
+			> randomGen(containing_structure, N, parts);
+
+		int *ret = (int *) calloc(N, sizeof(int));
+		for (int i = 0; i < number_points; i++) {
+			int index = randomGen.generate(rand);
+			assert(inside_generation_range(0, N, index));
+			ret[index]++;
+		}
+
+#ifdef TEST_VERBOSE
+		for(int i = 0; i < N; i++) {
+			std::cout << ret[i] << std::endl;
+		}
+#endif
+//		int avg = 0;
+//		for(int i = 0; i < N; i++) {
+//			avg += ret[i];
+//		}
+//		avg = avg / N;
+//		assert(is_uniform(ret, N, avg));
+
+		assert(all_posibilities_covered(ret, N));
+		free(ret);
+	}
+
+// Testing for small values of number_points
+	for (int cont = 1; cont < 10; cont++) {
+		CGAL::Random rand;
+		const int MIN_N = 1;
+		const int MAX_N = cont+1;
+		const int N = rand.get_int(MIN_N, MAX_N);
+#ifdef TEST_VERBOSE
+		std::cout << "N = " << N << std::endl;
+#endif
+		const int number_points = cont;
+#ifdef TEST_VERBOSE
+		std::cout << "number_points = " << number_points << std::endl;
+#endif
+		const int MIN_PARTS = 1;
+		const int MAX_PARTS = 1<<20;
+		const int parts = rand.get_int(MIN_PARTS, MAX_PARTS);
+		GeneratorWithWeight *containing_structure = new GeneratorWithWeight[N];
+		for (int i = 0; i < N; i++) {
+			int aux = i;
+			Probability_1_generator<int> randGen( aux );
+			GeneratorWithWeight tmp = GeneratorWithWeight (randGen, (double) 1/N);
+			containing_structure[i] = tmp;
+		}
+
+		CGAL::internal::Finite_support_distribution<GeneratorWithWeight
+			> randomGen(containing_structure, N, parts);
+
+		int *ret = (int *) calloc(N, sizeof(int));
+		for (int i = 0; i < number_points; i++) {
+			int index = randomGen.generate(rand);
+			assert(inside_generation_range(0, N, index));
+			ret[index]++;
+		}
+
+#ifdef TEST_VERBOSE
+		for(int i = 0; i < N; i++) {
+			std::cout << ret[i] << std::endl;
+		}
+#endif
+//		int avg = 0;
+//		for(int i = 0; i < N; i++) {
+//			avg += ret[i];
+//		}
+//		avg = avg / N;
+//		assert(is_uniform(ret, N, avg));
+
+		assert(all_posibilities_covered(ret, N));
+		free(ret);
+	}
+
+	for (int cont = 0; cont < 10; cont++) {
 		CGAL::Random rand;
 		const int MIN_N = 90;
 		const int MAX_N = 100;
