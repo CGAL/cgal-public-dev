44ec913b7e49b60661c7e8da594eb9040d51093b
jayalatn <jaya.chamara@gmail.com>
2013-09-18 19:25:10 +0200

    Code scrub

--- AABB_tree/examples/AABB_tree/AABB_handling_queried_primitive_example.cpp
+++ AABB_tree/examples/AABB_tree/AABB_handling_queried_primitive_example.cpp
@@ -1,5 +1,5 @@
 
-// Author(s)     :
+// Author(s)     : Chamara Jayalath
 
 #include <fstream>
 #include <iostream>
@@ -21,41 +21,41 @@ typedef CGAL::AABB_segment_primitive<K, Iterator> Primitive;
 typedef CGAL::AABB_traits<K, Primitive> Traits;
 typedef CGAL::AABB_tree<Traits> Tree;
 
-//types to return primitive results, template argument is the query type
-typedef boost::optional< Tree::Intersection_and_primitive_id<Segment>::Type > Segment_intersection;
+// types to return primitive results, template argument is the query type
+typedef boost::optional<Tree::Intersection_and_primitive_id<Segment>::Type> Segment_intersection;
 
-//types to return primitive results for point queries
+// types to return primitive results for point queries
 typedef Tree::Point_and_primitive_id Point_and_primitive_id;
 
 
 int main()
 {
-	Point a(1,0);
-	Point b(0,1);
-	Point c(-1,0);
-	Point d(0,-1);
+	Point a(1, 0);
+	Point b(0, 1);
+	Point c(-1, 0);
+	Point d(0, -1);
 
 	std::vector<Segment> segments;
-	segments.push_back(Segment(a,b));
-	segments.push_back(Segment(b,c));
-	segments.push_back(Segment(c,d));
-	segments.push_back(Segment(d,a));
+	segments.push_back(Segment(a, b));
+	segments.push_back(Segment(b, c));
+	segments.push_back(Segment(c, d));
+	segments.push_back(Segment(d, a));
 
 
 	Tree tree(segments.begin(),segments.end());
 
 	// segment intersection query
-	Point p((FT) -2,  (FT) -2);
-	Point q((FT) 2,  (FT) 2);
-	Segment pq(p,q);
+	Point p((FT) -2.0,  (FT) -2.0);
+	Point q((FT) 2.0,  (FT) 2.0);
+	Segment pq(p, q);
 
-	//retrieve an intersection from the AABB tree
-	//The return type contain the intersection by segment query and primitive that has been intersected.
+	// retrieve an intersection from the AABB tree
+	// The return type contain the intersection by segment query and primitive that has been intersected.
 	
 	Segment_intersection intersection = tree.any_intersection(pq);
 
-	//The first element of the pair Intersection_and_primitive_id contains the intersection
-	//result. In this example it is a point.
+	// The first element of the pair Intersection_and_primitive_id contains the intersection
+	// result. In this example it is a point.
 	Point *point;
 	if( point = boost::get<Point>(&(intersection->first)))
         std::cout << "Intersection point " << *point << std::endl;
@@ -70,8 +70,8 @@ int main()
 	//Get the integer index
 	unsigned int pindex = std::distance(segments.begin(), index);
 
-	std::cout<<"Intersected primitive index: "<<pindex<<std::endl;
-	std::cout<<"Intersected primitive: "<<*index<<std::endl;
+	std::cout << "Intersected primitive index: " << pindex << std::endl;
+	std::cout << "Intersected primitive: " << *index << std::endl;
 
 	// Same as above the closest point and corresponding primitive can be retrieved.
 
@@ -84,15 +84,15 @@ int main()
 	Point_and_primitive_id pp = *point_primitive;
 	Point result = pp.first;
 
-	std::cout<<"Closest point to "<<r<< " is "<<result<<std::endl;
+	std::cout << "Closest point to " << r << " is " << result << std::endl;
 
 
 	//index of the primitive containing closest point
 	index = pp.second;
 	pindex = std::distance(segments.begin(), index);
 
-	std::cout<<"Closest primitive index: "<< pindex<<std::endl;
-	std::cout<<"Closest primitive: "<<*index<<std::endl;
+	std::cout << "Closest primitive index: " << pindex << std::endl;
+	std::cout << "Closest primitive: " << *index << std::endl;
 
 	return EXIT_SUCCESS;
 }
--- AABB_tree/examples/AABB_tree/AABB_triangle_2_example.cpp
+++ AABB_tree/examples/AABB_tree/AABB_triangle_2_example.cpp
@@ -1,4 +1,4 @@
-// Author(s) : 
+// Author(s) : Chamara Jayalath
 
 #include <iostream>
 #include <list>
@@ -10,14 +10,14 @@
 
 typedef CGAL::Simple_cartesian<double> K;
 
-//2D geometric data types 
+// 2D geometric data types 
 typedef K::FT FT;
 typedef K::Ray_2 Ray;
 typedef K::Line_2 Line;
 typedef K::Point_2 Point;
 typedef K::Triangle_2 Triangle;
 
-//AABB tree is instantiated with the 2D geometric data, notice that there is no need to explicitly define the dimension. 
+// AABB tree is instantiated with the 2D geometric data, notice that there is no need to explicitly define the dimension. 
 typedef std::list<Triangle>::iterator Iterator;
 typedef CGAL::AABB_triangle_primitive<K, Iterator> Primitive;
 typedef CGAL::AABB_traits<K, Primitive> AABB_triangle_traits;
@@ -25,27 +25,27 @@ typedef CGAL::AABB_tree<AABB_triangle_traits> Tree;
 
 int main()
 {
-    Point a(0,0);
-	Point b(1,-2);
-	Point c(2,-1);
-	Point d(2,1);
-	Point e(1,2);
-	Point f(-1,-2);
-	Point g(-2,1);
-	Point h(-2,-1);
-	Point i(-1,-2);
+    Point a(0, 0);
+	Point b(1, -2);
+	Point c(2, -1);
+	Point d(2, 1);
+	Point e(1, 2);
+	Point f(-1, -2);
+	Point g(-2, 1);
+	Point h(-2, -1);
+	Point i(-1, -2);
 
     std::list<Triangle> triangles;
-    triangles.push_back(Triangle(a,b,c));
-    triangles.push_back(Triangle(a,d,e));
-    triangles.push_back(Triangle(a,f,g));
-    triangles.push_back(Triangle(a,h,i));
+    triangles.push_back(Triangle(a, b, c));
+    triangles.push_back(Triangle(a, d, e));
+    triangles.push_back(Triangle(a, f, g));
+    triangles.push_back(Triangle(a, h, i));
 
     // constructs 2D AABB tree
-    Tree tree(triangles.begin(),triangles.end());
+    Tree tree(triangles.begin(), triangles.end());
 
     // counts #intersections
-    Ray ray_query(g,d);
+    Ray ray_query(g, d);
     std::cout << tree.number_of_intersected_primitives(ray_query)
         << " intersections(s) with ray query" << std::endl;
 
