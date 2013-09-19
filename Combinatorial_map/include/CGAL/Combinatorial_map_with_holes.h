98697f3a99ad6e44243a45b33e840603a3f34677
Junfei Huang <marcohjf@gmail.com>
2013-09-18 22:12:41 -0400

    cmap_with_holes

--- Combinatorial_map/include/CGAL/Combinatorial_map_with_holes.h
+++ Combinatorial_map/include/CGAL/Combinatorial_map_with_holes.h
@@ -4,7 +4,9 @@
 
 #include <iostream>
 #include <list>
+#include <vector>
 #include <CGAL/Combinatorial_map.h>
+#include <CGAL/Compact_container.h>
 namespace CGAL {
 
 template < unsigned int d_, 
@@ -17,18 +19,21 @@ class Combinatorial_map_with_holes :
 {
 public:
 
-  typedef Combinatorial_map_with_holes<d_, Items_,Alloc_>  Self;
-  typedef CGAL::Combinatorial_map_base<d_, Self, Items_, Alloc_> Base;
-  typedef typename Base::Dart_handle Dart_handle;
-  typedef typename Base::Dart_const_handle Dart_const_handle;
-  typedef typename Base::Alloc Alloc;
+  typedef Combinatorial_map_with_holes<d_, Items_,Alloc_>   Self;
+  typedef CGAL::Combinatorial_map_base<d_, Self, Items_, Alloc_>
+                                                            Base;
+  typedef typename Base::Dart_handle                        Dart_handle;
+  typedef typename Base::Dart_const_handle                  Dart_const_handle;
+  typedef typename Base::Alloc                              Alloc;
 
 public:
+
   class Face
   {
+
   public:
 
-    std::list<Dart_handle>    dart_list;
+    std::vector<Dart_handle>    dart_list;
 
   public:
 
@@ -38,7 +43,7 @@ public:
     /* Add the associated outer_boundary to the face */
     void add_boundary(Dart_handle dh) {
       if (dart_list.size() == 0) dart_list.push_back(dh);
-      else dart_list.front() = dh;
+      else dart_list[0] = dh;
     }
 
     /* Add a hole into the face */
@@ -54,27 +59,63 @@ public:
     /* Erase the outer_boundary of the face */
     void erase_boundary()
     {
-      dart_list.front() = Base::null_dart_handle;
+      if (dart_list.size() == 1)
+          dart_list.clear();
+      else dart_list[0] = Base::null_dart_handle;
     }
 
     /* Erase the hole of the face */
     void erase_hole(Dart_handle dh)
     {
-      dart_list.remove(dh);
+      for (int i = 0; i<dart_list.size(); ++i)
+      {
+        if (dart_list[i] == dh)
+        {
+          dart_list.erase(dh);
+          break;
+        }
+      }
     }
+
+  public:
+
+    void * for_compact_container() const
+    { return vp; }
+    void * & for_compact_container()
+    { return vp; }
+
+  private:
+    void * vp;
+
   };
 
 public:
+    
+    typedef Compact_container<Face>                      Face_container;
+    typedef typename Face_container::iterator            Face_handle;
+    typedef typename Face_container::const_iterator      Face_const_handle;
+    typedef typename Face_container::size_type           size_type;
 
-  std::list<Face>    facets;
-      
+public:
+
+  Face_container       facets;
+
 public:
 
   /* Define an instance of Face, and return this Face object */
-  Face create_face() {
+  Face_handle create_face() {
     Face fa;
-    facets.push_back(fa);
-    return fa;
+    return facets.template emplace<Face>(fa);
+  }
+
+  void delete_face(Face_handle fit)
+  {
+    facets.erase(fit);
+  }
+
+  size_type number_of_faces() const
+  {
+    return facets.size();
   }
 };
   
