01df387cfaf76c8f30c24683707c7775b3c45b32
Philipp Möller <bootsarehax@googlemail.com>
2013-12-05 18:40:08 +0100

    Make CGAL_use_library honor CGAL_WITH_*

--- Installation/cmake/modules/CGAL_use_library.cmake
+++ Installation/cmake/modules/CGAL_use_library.cmake
@@ -11,45 +11,32 @@ if(NOT CGAL_USE_LIBRARY_FILE_INCLUDED)
       target_link_libraries(${target_name} ${lib}) 
     elseif(${lib} MATCHES "Qt")
       
-    else()
-      # confirm we support it
-      list(FIND CGAL_EXTERNAL_LIBRARIES ${lib} lib_pos)
-      if(${lib_pos} EQUAL -1)
-        message(FATAL_ERROR "${lib} is not a supported external library.")
+    else(NOT ${lib_pos} EQUAL -1)
+      if(DEFINED CGAL_WITH_${lib})
+        set(use_it ${CGAL_WITH_${lib}})
+      else()
+        set(use_it ${${vlib}_FOUND})
       endif()
 
-      if(NOT ${vlib}_FOUND AND NOT optional)
-        message(FATAL_ERROR "${target_name} requires ${lib} but it could not be found.")
+      if(NOT use_it)
+        return()
       endif()
-    endif()
 
-    
-    if(NOT WITH_${lib})
-      message(FATAL_ERROR "${target_name} requires ${lib}, but WITH_${lib} is \"OFF\".")
+      if(${lib} MATCHES "Boost_") # a boost component library has to link with singular LIBRARY
+        target_link_libraries(${target_name} ${${vlib}_LIBRARY})
+      else()
+        target_link_libraries(${target_name} ${${vlib}_LIBRARIES})
+      endif()
+      
+      if(${CMAKE_VERSION} VERSION_LESS 2.8.12)
+        target_include_directories(${target_name} PUBLIC "${${vlib}_INCLUDE_DIR}")
+      else()
+        target_include_directories(${target_name} SYSTEM PUBLIC "${${vlib}_INCLUDE_DIR}")
+      endif()
+      
+      target_compile_definitions(${target_name} PUBLIC "${${vlib}_DEFINITIONS}" PUBLIC "-DCGAL_USE_${use_define}")
+    elseif(${lib_pos} EQUAL -1)
+      message(FATAL_ERROR "CGAL_use_library called with ${lib} which is not a supported library.")
     endif()
-    
-    if(NOT ${vlib}_FOUND)
-      message(FATAL_ERROR "Trying to use ${lib} which could not be found.")
-    endif()
-
-    # if none of the above set use_define, set it here
-    if(NOT DEFINED use_define)
-      set(use_define ${vlib})
-    endif()
-    
-    if(${lib} MATCHES "Boost_") # a boost component library has to link with singular LIBRARY
-      target_link_libraries(${target_name} ${${vlib}_LIBRARY})
-    else()
-      target_link_libraries(${target_name} ${${vlib}_LIBRARIES})
-    endif()
-    
-    if(${CMAKE_VERSION} VERSION_LESS 2.8.12)
-      target_include_directories(${target_name} PUBLIC "${${vlib}_INCLUDE_DIR}")
-    else()
-      target_include_directories(${target_name} SYSTEM PUBLIC "${${vlib}_INCLUDE_DIR}")
-    endif()
-    
-    target_compile_definitions(${target_name} PUBLIC "${${vlib}_DEFINITIONS}" PUBLIC "-DCGAL_USE_${use_define}")
   endfunction()
-
 endif()
