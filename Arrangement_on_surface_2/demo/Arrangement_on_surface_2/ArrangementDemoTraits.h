847018be41ecb5609d54da80e7c7debb0f6c33a6
Alex Tsui <atsui@ucdavis.edu>
2014-08-11 14:39:37 -0700

    Add BezierDemoTraits.

--- Arrangement_on_surface_2/demo/Arrangement_on_surface_2/ArrangementDemoTraits.h
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2/ArrangementDemoTraits.h
@@ -16,6 +16,8 @@
 #include "ArrangementDemoTab.h"
 #include "DcelTypes.h"
 
+#include <CGAL/Arr_Bezier_curve_traits_2.h>
+
 /**
 Provided types
 -----
@@ -44,5 +46,33 @@ struct AlgebraicDemoTraits
   typedef Algebraic_arrangement_2 ArrangementType;
   static const std::string Name;
 }; // struct AlgebraicDemoTraits
+
+struct BezierDemoTraits
+{
+  typedef CGAL::CORE_algebraic_number_traits              Nt_traits;
+  typedef Nt_traits::Rational                             Rational;
+  typedef Nt_traits::Algebraic                            Algebraic;
+
+  typedef CGAL::Cartesian<Rational>                       Rat_kernel;
+  typedef CGAL::Cartesian<Algebraic>                      Alg_kernel;
+
+  typedef Rat_kernel::Point_2                             Rat_point_2;
+  typedef CGAL::Arr_Bezier_curve_traits_2<Rat_kernel, Alg_kernel, Nt_traits>
+                                                          Bezier_traits_2;
+  typedef Bezier_traits_2::Curve_2                        Bezier_curve_2;
+  typedef Dcel<Bezier_traits_2>                           Bezier_dcel;
+  typedef CGAL::Arrangement_with_history_2<Bezier_traits_2, Bezier_dcel>
+    Bezier_arrangement_2;
+
+  typedef CGAL::Qt::ArrangementGraphicsItem< Bezier_arrangement_2 >
+    BezierArrangementGraphicsItem;
+
+  typedef ArrangementDemoTab< Bezier_arrangement_2 > BezierTab;
+
+  // exports
+  typedef Bezier_traits_2 ArrTraitsType;
+  typedef Bezier_arrangement_2 ArrangementType;
+  static const std::string Name;
+}; // struct BezierDemoTraits
 
 #endif //ARRANGEMENT_DEMO_TRAITS_H
