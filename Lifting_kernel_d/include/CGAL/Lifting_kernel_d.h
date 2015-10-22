43ce4c325f31666dfe368b690ba9461c61657ecf
Luis Peñaranda <luis.penaranda@gmx.com>
2015-10-22 21:20:25 -0200

    first try

--- Lifting_kernel_d/include/CGAL/Lifting_kernel_d.h
+++ Lifting_kernel_d/include/CGAL/Lifting_kernel_d.h
@@ -1,23 +1,49 @@
-#include <CGAL/Lifting_kernel_d/determinant.h>
+#include <CGAL/Lifting_kernel_d/sort_swap.h>
+//#include <CGAL/Lifting_kernel_d/determinant.h>
 
-template <class _K,class _P=_K::Point_d,class _NT=P::NT>
+// The boost implementation of hash tables appeared in version 1.36. If the
+// installed version is older, we abort compilation.
+#include <boost/version.hpp>
+#if BOOST_VERSION < 103600
+#error This needs Boost 1.36 or newer
+#endif
+
+#include <boost/functional/hash.hpp>
+#include <boost/unordered_map.hpp>
+#include <vector>
+
+#ifndef CGAL_HASH_TABLE_SIZE_LIMIT
+// This value comes from empirical observations. When working with rational
+// points, coming from doubles, this limits the process size to around 2Gb.
+#define CGAL_HASH_TABLE_SIZE_LIMIT 7999999
+#endif
+
+namespace CGAL{
+
+template <class _K,typename _P=typename _K::Point_d,typename _NT=typename _P::NT>
 class Lifting_kernel_d:
-public K{
+public _K{
         private:
-        typedef typename _K                     Kernel_d;
-        typedef typename _P                     Point_d;
-        typedef typename _NT                    NT;
+        typedef _K                                              Kernel_d;
+        typedef _P                                              Point_d;
+        typedef _NT                                             NT;
+        typedef std::vector<const Point_d&>                     Index;
+        typedef boost::unordered_map<Index,NT>                  Table;
+        typedef typename Table::iterator                        It;
 
-        private:
+        public:
         struct Orientation_d;
-}
+};
 
-template <class _K,class _P,class _NT>
-struct Lifting_kernel_d::Orientation_d{
-        template <class PointInputIterator,LiftingInputIterator>
+template <class _K,typename _P,typename _NT>
+struct Lifting_kernel_d<_K,_P,_NT>::Orientation_d{
+        template <class PointInputIterator,class LiftingInputIterator>
         CGAL::Orientation
         operator()(PointInputIterator,PointInputIterator,
                    LiftingInputIterator,LiftingInputIterator){
                 // TODO
+                return COLLINEAR;
         }
-}
+};
+
+} // namespace CGAL
