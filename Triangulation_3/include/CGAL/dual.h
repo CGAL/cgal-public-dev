d8a731cfd69780b294e9b5325a1c4a996ff5548a
Maxim Torgonsky <kriolog@gmail.com>
2015-11-18 22:50:59 +0100

    Add processing of convex hull verticies in the dual function.
    Based on a halfspace_intersection_3 function. Tested on a single simple example.

--- Triangulation_3/include/CGAL/dual.h
+++ Triangulation_3/include/CGAL/dual.h
@@ -1,3 +1,23 @@
+// Copyright (c) 1999-2004   INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+
+#ifndef DUAL_H // TODO: give a better name
+#define DUAL_H
+
 #include <utility>
 #include <iterator>
 #include <vector>
@@ -5,88 +25,121 @@
 
 #include <boost/bind.hpp>
 #include <boost/function_output_iterator.hpp>
+#include <boost/range/join.hpp>
 
-template < class Delaunay_triangulation_3, class Polyhedron, class Polyhedron_incremental_builder, class Bbox >
-Polyhedron
-dual(const Delaunay_triangulation_3& t, typename Delaunay_triangulation_3::Vertex_handle v, const Bbox& bbox/* = Bbox(0., 0., 0., 0., 0., 0.)*/)
+#include <CGAL/Bbox_3.h>
+#include <CGAL/Polyhedron_incremental_builder_3.h>
+#include <CGAL/Convex_hull_3/dual/halfspace_intersection_3.h>
+
+namespace CGAL {
+
+template < class Polyhedron_3, class DelaunayTriangulation_3, class PlaneIterator >
+Polyhedron_3
+dual_support(const DelaunayTriangulation_3& t, typename DelaunayTriangulation_3::Vertex_handle v, PlaneIterator planes_begin, PlaneIterator planes_end)
 {
-  //TODO: sweep typedefs
-  typedef typename Delaunay_triangulation_3::Point Point;
-  typedef typename Delaunay_triangulation_3::Vertex Vertex;
-  typedef typename Delaunay_triangulation_3::Edge Edge;
-  typedef typename Delaunay_triangulation_3::Facet Facet;
-  typedef typename Delaunay_triangulation_3::Cell Cell;
-  typedef typename Delaunay_triangulation_3::Vertex_handle Vertex_handle;
-  typedef typename Delaunay_triangulation_3::Cell_handle Cell_handle;
-  typedef typename Delaunay_triangulation_3::Cell_circulator Cell_circulator;
+  typedef typename DelaunayTriangulation_3::Point Point;
+  typedef typename DelaunayTriangulation_3::Geom_traits::Plane_3 Plane;
+  typedef typename DelaunayTriangulation_3::Edge Edge;
+  typedef typename DelaunayTriangulation_3::Vertex_handle Vertex_handle;
+  typedef typename DelaunayTriangulation_3::Cell_handle Cell_handle;
+  typedef typename DelaunayTriangulation_3::Cell_circulator Cell_circulator;
 
   CGAL_triangulation_precondition(v != Vertex_handle());
-  CGAL_triangulation_precondition(!t.is_infinite(v) );
+  CGAL_triangulation_precondition(!t.is_infinite(v));
   CGAL_triangulation_precondition(t.dimension() == 3);
-  CGAL_triangulation_expensive_precondition(t.tds().is_vertex(v) );
+  CGAL_triangulation_expensive_precondition(tds().is_vertex(v));
 
-  typedef std::map<Cell_handle, size_t> Cell_id_map;
-  // Indices correspond to the incident to v cells and to its dual vertices.
-  Cell_id_map cell_ids;
+  Polyhedron_3 result;
   {
-    typedef std::pair<typename Cell_id_map::iterator, bool> Iter_bool_pair;
-    typedef typename Cell_id_map::value_type Value_type;
-    typedef Iter_bool_pair(Cell_id_map::*Insert)(const Value_type&);
-    t.incident_cells(v, boost::make_function_output_iterator(
-      boost::bind(static_cast<Insert>(&Cell_id_map::insert), &cell_ids,
-        // Need const references for C++11 compability.
-        boost::bind(&std::make_pair<const Cell_handle&, const size_t&>,
-          _1, boost::bind(&Cell_id_map::size, &cell_ids)))));
-  }
+    std::vector<Edge> finite_inc_edges;
+    finite_inc_edges.reserve(16); // < 16 in average?
+    t.finite_incident_edges(v, std::back_inserter(finite_inc_edges));
+    CGAL_assertion(!finite_inc_edges.empty());
 
-  // Dual vertices of finite cells incident to v.
-  std::vector<Point> points(cell_ids.size());
-  // A first found facet which is incident to v and which belongs to the hull.
-  // of the triangulation, if any.
-  Facet first_hull_facet;
-  bool hull_facet_found = false;
-  for(typename Cell_id_map::iterator cit = cell_ids.begin(), end = cell_ids.end(); cit != end; ++cit) {
-    Cell_handle cell = cit->first;
-    int inf_v_id;
-    if(cell->has_vertex(t.infinite_vertex(), inf_v_id) && !hull_facet_found) {
-      hull_facet_found = true;
-      first_hull_facet = t.mirror_facet(std::make_pair(cell, inf_v_id));
+    if(finite_inc_edges.size() == t.degree(v)) {
+      // Vertex v is not on the convex hull, dual cell is finite.
+      typedef std::map<Cell_handle, size_t> Cell_id_map;
+      // Indices correspond to cells incident to v and to its dual vertices.
+      Cell_id_map cell_ids;
+      {
+        typedef std::pair<typename Cell_id_map::iterator, bool> Iter_bool_pair;
+        typedef typename Cell_id_map::value_type Value_type;
+        typedef Iter_bool_pair(Cell_id_map::*Insert)(const Value_type&);
+        t.incident_cells(v, boost::make_function_output_iterator(
+          boost::bind(static_cast<Insert>(&Cell_id_map::insert), &cell_ids,
+            // Const references are necessary for C++11 compability.
+            boost::bind(&std::make_pair<const Cell_handle&, const size_t&>,
+              _1, boost::bind(&Cell_id_map::size, &cell_ids)))));
+      }
+
+      // Dual points of finite cells incident to v.
+      std::vector<Point> points(cell_ids.size());
+      for(typename Cell_id_map::iterator cit = cell_ids.begin(), end = cell_ids.end(); cit != end; ++cit) {
+        points[cit->second] = t.dual(cit->first);
+      }
+
+      Polyhedron_incremental_builder_3<typename Polyhedron_3::HalfedgeDS> builder(result.hds()/*TODO: keep it?*/, true);
+      builder.begin_surface(cell_ids.size(), finite_inc_edges.size(), 3 * cell_ids.size());
+      for(typename std::vector<Point>::iterator pit = points.begin(), pend = points.end(); pit != pend; ++pit) {
+        builder.add_vertex(*pit);
+      }
+
+      for(typename std::vector<Edge>::iterator eit = finite_inc_edges.begin(), eend = finite_inc_edges.end(); eit != eend; ++eit) {
+        Cell_circulator ccir = t.incident_cells(*eit);
+        Cell_circulator cend = ccir;
+        builder.begin_facet();
+        do {
+          builder.add_vertex_to_facet(cell_ids[ccir]);
+          ++ccir;
+        } while ( ccir != cend );
+        builder.end_facet();
+      }
+      builder.end_surface();
+
+      CGAL_triangulation_expensive_postcondition(result.is_valid());
+      CGAL_triangulation_postcondition(result.is_closed());
+
+      return result;
     }
-    points[cit->second] = t.dual(cell);
   }
 
-  // First step: add finite facets to the resulting polyhedron.
-  std::vector<Edge> edges;
-  t.incident_edges(v, std::back_inserter(edges));
+  // Vertex v is on the convex hull, dual cell will be closed with the input planes.
+  std::vector<Vertex_handle> finite_adj_vertices;
+  finite_adj_vertices.reserve(16); // TODO: < 16 in average?
+  t.finite_adjacent_vertices(v, std::back_inserter(finite_adj_vertices));
+  CGAL_assertion(!finite_adj_vertices.empty());
 
-  Polyhedron result;
-  Polyhedron_incremental_builder builder(result.hds()/*TODO: TMP*/, true);
-  builder.begin_surface(cell_ids.size(), edges.size(), 3 * cell_ids.size());
-  for(typename std::vector<Point>::iterator pit = points.begin(), pend = points.end(); pit != pend; ++pit) {
-    builder.add_vertex(*pit);
+  std::vector<Plane> voronoi_planes;
+  voronoi_planes.reserve(finite_adj_vertices.size());
+  for(typename std::vector<Vertex_handle>::iterator vit = finite_adj_vertices.begin(), vend = finite_adj_vertices.end(); vit != vend; ++vit) {
+    voronoi_planes.push_back(t.geom_traits().construct_bisector_3_object()((*vit)->point(), v->point()));
   }
 
-  for(typename std::vector<Edge>::iterator eit = edges.begin(), eend = edges.end(); eit != eend; ++eit) {
-    Cell_circulator ccir = t.incident_cells(*eit);
-    Cell_circulator cend = ccir;
-    builder.begin_facet();
-    do {
-      builder.add_vertex_to_facet(cell_ids[ccir]);
-      ++ccir;
-    } while ( ccir != cend );
-    builder.end_facet();
-  }
-  builder.end_surface();
+  boost::range::joined_range<const std::vector<Plane>, const boost::iterator_range<PlaneIterator> >
+  plane_range = boost::join(voronoi_planes, boost::make_iterator_range(planes_begin, planes_end));
+  halfspace_intersection_3(boost::begin(plane_range), boost::end(plane_range), result, boost::make_optional(v->point()));
 
-  if(!hull_facet_found) {
-  // Dual cell is finite, it does not need to be intersected with the bbox.
-    CGAL_triangulation_expensive_postcondition(result.is_valid());
-    CGAL_triangulation_postcondition(result.is_closed());
-
-    return result;
-  }
-
-  // Second step: close the resulting infinite polyhedron, if any.
-  // TODO: implement
   return result;
 }
+
+template < class Polyhedron_3, class DelaunayTriangulation_3 >
+Polyhedron_3
+dual(const DelaunayTriangulation_3& t, typename DelaunayTriangulation_3::Vertex_handle v, const Bbox_3& bbox)
+{
+  typedef typename DelaunayTriangulation_3::Geom_traits::Plane_3 Plane;
+
+  std::vector<Plane> bbox_planes;
+  bbox_planes.reserve(6);
+  bbox_planes.push_back(Plane(-1, 0, 0, bbox.xmin()));
+  bbox_planes.push_back(Plane(1, 0, 0, -bbox.xmax()));
+  bbox_planes.push_back(Plane(0, -1, 0, bbox.ymin()));
+  bbox_planes.push_back(Plane(0, 1, 0, -bbox.ymax()));
+  bbox_planes.push_back(Plane(0, 0, -1, bbox.zmin()));
+  bbox_planes.push_back(Plane(0, 0, 1, -bbox.zmax()));
+
+  return dual_support<Polyhedron_3>(t, v, bbox_planes.begin(), bbox_planes.end());
+}
+
+} //namespace CGAL
+
+#endif // DUAL_H
