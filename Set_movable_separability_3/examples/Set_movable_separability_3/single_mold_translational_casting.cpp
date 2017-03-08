8f1d436e1feaebcf91b88d46c92649fb6fd47ee1
shahar shamai <shasha94@gmail.com>
2017-03-08 14:54:49 +0200

    casting-3: tmp

--- Set_movable_separability_3/examples/Set_movable_separability_3/single_mold_translational_casting.cpp
+++ Set_movable_separability_3/examples/Set_movable_separability_3/single_mold_translational_casting.cpp
@@ -8,7 +8,7 @@
 #include <CGAL/single_mold_translational_casting_3.h>
 
 #include <CGAL/boost/graph/named_function_params.h>
-
+#include <algorithm>
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel>                          Polyhedron;
 typedef Kernel::Direction_3                                 Direction_3;
@@ -22,6 +22,17 @@ typedef std::pair<boost::graph_traits<Polyhedron>::face_descriptor,
                   Direction_patch>                          Top_facet;
 
 namespace SMS = CGAL::Set_movable_separability_3;
+
+struct Plane_equation {
+    template <class Facet>
+    typename Facet::Plane_3 operator()( Facet& f) {
+        typename Facet::Halfedge_handle h = f.halfedge();
+        typedef typename Facet::Plane_3  Plane;
+        return Plane( h->vertex()->point(),
+                      h->next()->vertex()->point(),
+                      h->next()->next()->vertex()->point());
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -37,6 +48,9 @@ int main(int argc, char* argv[])
   CGAL::cgal_bgl_named_params<bool, CGAL::all_default_t, boost::no_property>
     params;
   std::list<Top_facet> top_facets;
+
+  std::transform( poly.facets_begin(), poly.facets_end(), poly.planes_begin(),
+                      Plane_equation());
   SMS::single_mold_translational_casting_3(poly, params,
                                            std::back_inserter(top_facets));
 
