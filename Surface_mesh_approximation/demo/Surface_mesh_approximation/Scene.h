ab22b88e8a791aec7b2ee9d0d2d794aa8bb646cf
aboudev <aboudev@users.noreply.github.com>
2017-05-25 21:55:27 +0800

    fix incoherent tab/whitespace in demo

--- Surface_mesh_approximation/demo/Surface_mesh_approximation/Scene.h
+++ Surface_mesh_approximation/demo/Surface_mesh_approximation/Scene.h
@@ -10,55 +10,51 @@
 class Scene
 {
 public:
-    Scene();
-    ~Scene();
+  Scene();
+  ~Scene();
 public:
-    // types
-    typedef CGAL::Bbox_3 Bbox;
+  // types
+  typedef CGAL::Bbox_3 Bbox;
 
 public:
-    void update_bbox();
-    Bbox bbox() { return m_bbox; }
+  void update_bbox();
+  Bbox bbox() { return m_bbox; }
 
 private:
-    // member data
-    Bbox m_bbox;
-    Line m_line;
-    Plane m_plane;
-    Point m_centroid;
-    Polyhedron *m_pPolyhedron;
+  // member data
+  Bbox m_bbox;
+  Line m_line;
+  Plane m_plane;
+  Point m_centroid;
+  Polyhedron *m_pPolyhedron;
 
-    // view options
-    bool m_view_polyhedron;
+  // view options
+  bool m_view_polyhedron;
 
 public:
-    // file menu
-    int open(QString filename);
+  // file menu
+  int open(QString filename);
 
-    // toggle view options
-    void toggle_view_poyhedron();
+  // toggle view options
+  void toggle_view_poyhedron();
 
-    // algorithms
-    Vector normalize(const Vector& v);
+  // algorithms
+  Vector normalize(const Vector& v);
 
-    void refine_loop();
-    void fit_edges();
-    void fit_vertices();
-    void fit_triangles();
+  void refine_loop();
+  void fit_edges();
+  void fit_vertices();
+  void fit_triangles();
 
-    // rendering
-    void draw(); 
-    void render_line();
-    void render_plane();
-    void render_centroid();
-    void render_polyhedron();
+  // rendering
+  void draw(); 
+  void render_line();
+  void render_plane();
+  void render_centroid();
+  void render_polyhedron();
 
 private:
-
-
-
 
 }; // end class Scene
-
 
 #endif // SCENE_H
--- Surface_mesh_approximation/demo/Surface_mesh_approximation/VSA_demo.cpp
+++ Surface_mesh_approximation/demo/Surface_mesh_approximation/VSA_demo.cpp
@@ -56,9 +56,9 @@ int main(int argc, char **argv)
   return app.exec();
 }
 
-#  include "Scene.cpp"
-#  include "Viewer.cpp"
-#  include "Viewer_moc.cpp"
-#  include "MainWindow.cpp"
-#  include "MainWindow_moc.cpp"
+#include "Scene.cpp"
+#include "Viewer.cpp"
+#include "Viewer_moc.cpp"
+#include "MainWindow.cpp"
+#include "MainWindow_moc.cpp"
 
