b19562956be03c39b78d2dd6f0bbba31aee2309d
earthling2304 <earthling2304@gmail.com>
2017-07-22 22:15:32 +0530

    handling singularities

--- HexEx/hexextr.h
+++ HexEx/hexextr.h
@@ -1,3 +1,183 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:867d89548777e1e5caf98945d199b2f5437ba332e3c81ccc9ff1d4fe9f490259
-size 8336
+#ifndef HEXEXTR_H
+#define HEXEXTR_H
+#include"typedefs.h"
+#include"handles.h"
+#include"triangulation.h"
+//#include"triangulation_to_LCC.h"
+#include<unordered_map>
+#include<map>
+#include"functions.h"
+#include"hexahedron_extraction.h"
+#include"sanitization.h"
+//#include"dart_extraction.h"
+#include<vector>
+#include"frame_field.h"
+
+
+namespace std{ //TODO: is this needed? 
+  int dart_count = 0;
+  template<>
+  struct hash<Face_handle>{
+    std::size_t operator()(const Face_handle& fh) const{
+      return (fh.enumeration)%1000;
+    } 
+  };
+
+}
+
+class HexExtr{
+  public:
+  
+// Currently,meshing commands are executed when the constructor is called. This will be changes later to incorporate the fucntions as methos of this class.
+    HexExtr(std::string infilename): identity(1,0,0,0,1,0,0,0,1,1){
+//input_tet_mesh to lcc
+      load_off_to_LCC(infilename, input_tet_mesh); 
+
+//a paramtrization function to test the extraction:
+      dummy_parametrize(input_tet_mesh); 
+
+//six directions in 3D to obtain the 24 affine transformations in chiral cubical symmetry group:
+      directions.push_back(Direction(1,0,0));
+      directions.push_back(Direction(0,1,0));
+      directions.push_back(Direction(0,0,1));
+      directions.push_back(Direction(-1,0,0));
+      directions.push_back(Direction(0,-1,0));
+      directions.push_back(Direction(0,0,-1));
+      for(int i = 0;i < 6;i++) 
+        for(int j = 0;j < 6;j++) 
+          for(int k = 0;k < 6;k++) 
+            if(CGAL::cross_product(directions[i].vector(),directions[j].vector()) == directions[k].vector()) 
+              G.push_back(Aff_transformation(directions[i].dx(), directions[j].dx(), directions[k].dx(),
+directions[i].dy(), directions[j].dy(), directions[k].dy(), 
+directions[i].dz(), directions[j].dz(), directions[k].dz(), 1)); //chiral cubical symmetry group
+
+
+      int cell = 0;
+
+//iterate through each tet in the input mesh:
+      for(LCC_3::One_dart_per_cell_range<3>::iterator it = input_tet_mesh.one_dart_per_cell<3>().begin(), itend = input_tet_mesh.one_dart_per_cell<3>().end(); it != itend; it++){ 
+
+//iterate through all the darts of each input mesh, to assign a cell_no unique to darts of each tet. Two darts belonging to different tets will have different cell_no
+        for(LCC_3::Dart_of_cell_range<3>::iterator it1 = input_tet_mesh.darts_of_cell<3>(it).begin(), it1end = input_tet_mesh.darts_of_cell<3>(it).end(); it1 != it1end; it1++){ 
+          (input_tet_mesh.info(it1)).cell_no = cell;
+        }
+        cell++;
+      }
+
+// a vector of size of the number of tets in the input mesh, to contain transition matrices of each tet to adjacent tets
+      std::vector<std::vector<Aff_transformation>> g(cell);
+
+
+/** initializing each element of g to be another vector of transition matrices.
+* g[i][j] will correspond to the transition fucntion(matrix) applied to tet i to transform to tet j, where i, j are cell_no of corresponding tets.
+*/
+      for(int j = 0; j<cell; j++){
+        std::vector<Aff_transformation> temp(cell);
+        g[j] = temp;
+      }
+
+// A vector of affine transformations to constain parametrization matrices of each tet indexed by cell_no
+      std::vector<Aff_transformation> parametrization_matrices(cell);  
+
+//iterate through each tet:    
+      for(LCC_3::One_dart_per_cell_range<3>:: iterator it = input_tet_mesh.one_dart_per_cell<3>().begin(), itend = input_tet_mesh.one_dart_per_cell<3>().end(); it != itend; it++){
+        std::vector<Point> points, parameters;
+
+//iterate through one dart corresponding to vertices of the given tet, so as to extract the coordinates in initial and parametrized space, and stored in the vectors 'points' and 'parameters'.
+        for(LCC_3::One_dart_per_incident_cell_range<0,3>::iterator it2 = input_tet_mesh.one_dart_per_incident_cell<0,3>(it).begin(), it2end = input_tet_mesh.one_dart_per_incident_cell<0,3>(it).end(); it2 != it2end; it2++){
+          points.push_back(input_tet_mesh.point(it2)); parameters.push_back((input_tet_mesh.info(it2)).parameters);
+        }
+
+//finding the parametrization matrix of the tet
+        Aff_transformation at = get_parametrization_matrix(points[0], points[1], points[2], points[3], parameters[0], parameters[1], parameters[2], parameters[3]); 
+
+        //std::cout<<points[0]<<" "<<points[0]<<" "<<points[1]<<" "<<points[2]<<" "<<parameters[0]<<" "<<parameters[1]<<" "<<parameters[2]<<std::endl; //for debug purposes
+        //print_aff_transformation(at); //for debugging
+
+// assigning the vector parametrization_matrices with the affine transformation indexed using cell_no
+        parametrization_matrices[(input_tet_mesh.info(it)).cell_no] = at;
+
+//creating a cell handle. TODO: needed? Try to do away with cell_handle, face_handle, vertex_handle and edge_handle.
+        Cell_handle ch(it, points, parameters, at);
+        cells.push_back(ch);
+        
+      }
+
+
+      for(LCC_3::One_dart_per_cell_range<2>::iterator it = input_tet_mesh.one_dart_per_cell<2>().begin(), 
+itend = input_tet_mesh.one_dart_per_cell<2>().end(); it != itend; it++){
+        // Face_handle fh(input_tet_mesh, it, i); i++;
+         //dart_in_face.emplace(it, fh);        
+        Aff_transformation at = extract_transition_function(it, input_tet_mesh, G);
+        g[(input_tet_mesh.info(it)).cell_no][(input_tet_mesh.info(input_tet_mesh.alpha(it, 3))).cell_no] = at;
+        g[(input_tet_mesh.info(input_tet_mesh.alpha(it, 3))).cell_no][(input_tet_mesh.info(it)).cell_no] = at.inverse();
+         //std::cout<<i<<std::endl;
+       // print_aff_transformation(at);
+        // faces_with_transitions.emplace(fh, at);
+         //faces.push_back(fh);
+		
+      }
+
+
+     /* int v=0;
+      for(LCC_3::Dart_range::iterator it = lcc.darts().begin(), itend = lcc.darts().end(); it != itend; it++){
+        lcc.info(it) = v; v++;
+      }*/
+/*
+      int nv = 0;
+      for(LCC_3::One_dart_per_cell_range<0>::iterator it = input_tet_mesh.one_dart_per_cell<0>().begin(), itend = input_tet_mesh.one_dart_per_cell<0>().end(); it!=itend; it++){
+        Vertex_handle vh(input_tet_mesh, it, nv, input_tet_mesh.is_free(it, 3));
+        vertices.push_back(vh);
+      }
+
+      
+      for(LCC_3::One_dart_per_cell_range<1>::iterator it = input_tet_mesh.one_dart_per_cell<1>().begin(), itend = input_tet_mesh.one_dart_per_cell<1>().end(); it != itend; it++){
+        Vertex_handle from, to;
+        for(std::vector<Vertex_handle>::iterator i = vertices.begin(), iend = vertices.end(); i != iend; i++){
+          if(input_tet_mesh.point((*i).incident_dart) == input_tet_mesh.point(it)) from = (*i); //there must be a better way to implement this.
+          if(input_tet_mesh.point((*i).incident_dart) == input_tet_mesh.point(input_tet_mesh.alpha(it, 0))) to = (*i);
+        }
+        Edge_handle eh(input_tet_mesh, it, from, to); 
+        edges.push_back(eh); 
+      }     
+
+      optimise_frame_field(input_tet_mesh, vertices, edges, 1); 
+
+*/
+
+//We have a parametrization matrix associated with each tet to transform a given set of points to parametric space. Since we have rounded off the translational terms in the extract_transition_function step
+
+//Sanitization
+    sanitize(input_tet_mesh, g);
+  
+
+
+
+//Extract vertices
+//Extract darts
+//making hexahedrons in the output mesh incorporting vertex extraction and dart extraction in a single step:
+     extract_hexes(input_tet_mesh, output_mesh, parametrization_matrices);
+
+    /* for(LCC_3::Vertex_attribute_range::iterator v = input_tet_mesh.vertex_attributes().begin(), vend = input_tet_mesh.vertex_attributes().end(); v != vend; v++){
+       Point p = input_tet_mesh.point_of_vertex_attribute(v);
+       output_mesh.create_vertex_attribute(p);
+     }*/
+
+     //extract_darts(input_tet_mesh, output_mesh, parametrization_matrices);
+    
+    }
+    std::unordered_map<Face_handle, Aff_transformation> faces_with_transitions; //Take this as input and make dart_handle face_handle map using this
+    std::map<Dart_handle, Face_handle> dart_in_face;
+    std::vector<Face_handle> faces;
+    std::vector<Edge_handle> edges;
+    std::vector<Vertex_handle> vertices;
+    std::vector<Cell_handle> cells;
+    //std::vector<Point> hvertices;
+    std::vector<Direction> directions;
+    Aff_transformation identity;//(1,0,0,0,1,0,0,0,1,1);
+    LCC_3 input_tet_mesh, output_mesh;//, parametrized_mesh, 
+    std::vector<Aff_transformation> G; //chiral cubical symmetry group
+   
+};
+
+#endif
---
+++ HexEx/triangulation.h
@@ -0,0 +1,182 @@
+#ifndef TRIANGULATION_H
+#define TRIANGULATION_H
+#include <CGAL/Linear_cell_complex_for_generalized_map.h>
+//#include"typedefs.h"
+//#include <CGAL/Linear_cell_complex_for_combinatorial_map.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <iostream>
+#include<fstream>
+#include <CGAL/Triangulation_3_to_lcc.h>
+#include<CGAL/Linear_cell_complex_constructors.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include<cstdlib>
+//#define USE_MESH 1 // comment to use triangulation_3 instead of Mesh_3
+#define DEBUG 1
+#ifdef USE_MESH
+// Mesh_3
+#include <CGAL/Mesh_triangulation_3.h>
+#include <CGAL/Mesh_complex_3_in_triangulation_3.h>
+#include <CGAL/Mesh_criteria_3.h>
+#include <CGAL/Polyhedral_mesh_domain_3.h>
+#include <CGAL/make_mesh_3.h>
+#include <CGAL/refine_mesh_3.h>
+#include<CGAL/Triangulation_3>
+
+#else
+// Triangulation_3
+#include <CGAL/Delaunay_triangulation_3.h>
+
+#endif
+/*
+typedef struct dartinfo{
+
+//all the darts corresponding to the same tet will have the same cell_no. This is to make it easier to assign a parametrization matrix, which can be determined from any dart of the tet.
+  double cell_no;
+
+//a variable to store if the particular dart is singular.
+  bool singular;
+
+//parameters is a point corresponding to the point of gven dart in input mesh in the parametrized space 
+  CGAL::Point_3<CGAL::Exact_predicates_inexact_constructions_kernel> parameters;
+}dart_info;
+
+struct myItem
+{
+  template < class GMap >
+  struct Dart_wrapper
+  {
+    typedef dart_info Dart_info; 
+    //typedef CGAL::Cell_attribute<GMap, int> Edge_attrib;
+   // typedef CGAL::cpp11::tuple<void,Edge_attrib> Attributes;
+    typedef CGAL::Cell_attribute_with_point< GMap, int, CGAL::Tag_true>   Vertex_attribute;
+    
+    typedef CGAL::cpp11::tuple<Vertex_attribute> Attributes;
+  };
+};
+*/
+typedef CGAL::Exact_predicates_inexact_constructions_kernel                    K; 
+typedef CGAL::Linear_cell_complex_traits<3, K>	                               Traits;
+typedef CGAL::Linear_cell_complex_for_generalized_map<3, 3, Traits, myItem>    LCC_3;
+typedef LCC_3::Dart_handle                                                     Dart_handle;
+typedef LCC_3::Dart_const_handle                                               Dart_const_handle;
+
+#ifdef USE_MESH
+// for Mesh
+typedef CGAL::Polyhedron_3<K> Polyhedron;
+typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron, K> Mesh_domain;
+
+#ifdef CGAL_CONCURRENT_MESH_3
+typedef CGAL::Parallel_tag Concurrency_tag;
+#else
+typedef CGAL::Sequential_tag Concurrency_tag;
+#endif
+
+typedef CGAL::Mesh_triangulation_3<Mesh_domain,CGAL::Default,Concurrency_tag>::type Tr;
+typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
+typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
+using namespace CGAL::parameters;
+
+struct Cell_in_complex
+{
+  Cell_in_complex(const C3t3& ac3t3) : c3t3(ac3t3)
+  {}
+
+  bool operator() (C3t3::Cell_handle c)
+  { return c3t3.is_in_complex(c); }
+  
+protected:
+  C3t3 c3t3;
+};
+
+#else 
+
+typedef CGAL::Delaunay_triangulation_3<LCC_3::Traits> Triangulation;
+
+#endif
+
+
+bool load_off_to_LCC(std::string filename, LCC_3& lcc)
+{
+  const char* fname = filename.c_str();
+
+#ifdef USE_MESH
+  // For Mesh_3
+  double fa = 25, fs = 0.15, fd = 0.008, crer = 3;
+  Polyhedron polyhedron;
+  std::ifstream input(fname);
+  input >> polyhedron;
+  if(input.fail()){
+    std::cerr << "Error: Cannot read file " <<fname<< std::endl;
+    return false;
+  }
+  input.close();
+  Mesh_domain domain(polyhedron);
+
+  Mesh_criteria criteria(facet_angle=fa, facet_size=fs, facet_distance=fd,
+                         cell_radius_edge_ratio=crer);
+  
+  // Mesh generation
+  C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria, no_perturb(), no_exude());
+
+ // Set tetrahedron size (keep cell_radius_edge_ratio), ignore facets
+  Mesh_criteria new_criteria(crer, cell_size=1.6); //originally cell_size = 0.3
+
+  // Mesh refinement
+  CGAL::refine_mesh_3(c3t3, domain, new_criteria);
+
+  // To convert to lcc
+  Cell_in_complex cic(c3t3);  
+  lcc.clear();
+  C3t3::Triangulation &atr= c3t3.triangulation();
+  CGAL::import_from_triangulation_3(lcc, atr, cic);
+  std::ofstream ofile;
+  ofile.open("tri.off");
+  CGAL::write_off(lcc, ofile);
+  ofile.close();
+#else
+  std::ifstream iFile(filename.c_str());
+  if (!iFile)
+  {
+    std::cout << "Problem reading file " << filename << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::istream_iterator<Point> begin(iFile), end;
+  Triangulation T;
+  T.insert(begin, end);
+  CGAL_assertion(T.is_valid(false));
+  CGAL::import_from_triangulation_3(lcc, T);
+#endif
+  if(DEBUG) std::cout<<"Checkpoint 10"<<std::endl;
+  //assert(lcc.is_valid());
+  //CGAL_assertion(lcc.is_valid());
+ if(lcc.is_valid()){ if(DEBUG) std::cout<<"Checkpoint 11 - true"<<std::endl;}
+  else {if(DEBUG) std::cout<<"Checkpoint 11 - false"<<std::endl;}
+
+if(DEBUG) std::cout<<"Checkpoint 12"<<std::endl;
+  std::ofstream ofile;
+  if(DEBUG) std::cout<<"Checkpoint 13"<<std::endl;
+  ofile.open("triangulation");
+if(DEBUG) std::cout<<"Checkpoint 14"<<std::endl;
+  ofile<<lcc; //works
+if(DEBUG) std::cout<<"Checkpoint 15"<<std::endl;
+  ofile.close();
+if(DEBUG) std::cout<<"Checkpoint 16"<<std::endl;
+  std::ifstream in;
+if(DEBUG) std::cout<<"Checkpoint 17"<<std::endl;
+  lcc.clear();
+if(DEBUG) std::cout<<"Checkpoint 18"<<std::endl;
+  in.open("triangulation");
+  if (in.is_open())
+  {
+    in>>lcc; //doesn't work: segfault - figure out why
+    in.close();
+  }
+  std::ofstream outfile;
+  outfile.open("test.off");
+  CGAL::write_off(lcc, outfile);
+  outfile.close();
+  lcc.display_characteristics(std::cout);
+  
+return lcc.is_valid();
+}
+#endif
