53e21eab7e774b422541dcbdede6cbc33bc6274d
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-10-09 12:17:55 +0200

    Improved readability

--- BGL/test/BGL/test_circulator.cpp
+++ BGL/test/BGL/test_circulator.cpp
@@ -11,30 +11,29 @@
 #include <iostream>
 #include <fstream>
 
-typedef CGAL::Simple_cartesian<double> Kernel;
-typedef CGAL::Polyhedron_3<Kernel>     Polyhedron;
+typedef CGAL::Simple_cartesian<double>                      Kernel;
+typedef CGAL::Polyhedron_3<Kernel>                          Polyhedron;
 
-typedef boost::graph_traits<Polyhedron> GraphTraits;
-typedef GraphTraits::vertex_descriptor vertex_descriptor;
-typedef GraphTraits::halfedge_descriptor halfedge_descriptor;
-typedef GraphTraits::edge_descriptor edge_descriptor;
-typedef GraphTraits::out_edge_iterator out_edge_iterator;
-typedef GraphTraits::in_edge_iterator in_edge_iterator;
+typedef boost::graph_traits<Polyhedron>                     GraphTraits;
+typedef GraphTraits::vertex_descriptor                      vertex_descriptor;
+typedef GraphTraits::halfedge_descriptor                    halfedge_descriptor;
+typedef GraphTraits::edge_descriptor                        edge_descriptor;
+typedef GraphTraits::in_edge_iterator                       in_edge_iterator;
+typedef GraphTraits::out_edge_iterator                      out_edge_iterator;
 
-typedef CGAL::Halfedge_around_face_circulator<Polyhedron> halfedge_around_face_circulator;
-typedef CGAL::Halfedge_around_target_circulator<Polyhedron> halfedge_around_target_circulator;
-typedef CGAL::Vertex_around_target_circulator<Polyhedron> vertex_around_target_circulator;
-typedef CGAL::Face_around_target_circulator<Polyhedron> face_around_target_circulator;
-
+typedef CGAL::Vertex_around_target_circulator<Polyhedron>   vertex_around_target_circulator;
 typedef CGAL::Halfedge_around_source_circulator<Polyhedron> halfedge_around_source_circulator;
+typedef CGAL::Halfedge_around_target_circulator<Polyhedron> halfedge_around_target_circulator;
+typedef CGAL::Halfedge_around_face_circulator<Polyhedron>   halfedge_around_face_circulator;
+typedef CGAL::Face_around_target_circulator<Polyhedron>     face_around_target_circulator;
 
-typedef CGAL::Halfedge_around_target_iterator<Polyhedron> halfedge_around_target_iterator;
-typedef CGAL::Halfedge_around_face_iterator<Polyhedron> halfedge_around_face_iterator;
-typedef CGAL::Face_around_face_iterator<Polyhedron> face_around_face_iterator;
-typedef CGAL::Vertex_around_target_iterator<Polyhedron> vertex_around_target_iterator;
+typedef CGAL::Vertex_around_target_iterator<Polyhedron>     vertex_around_target_iterator;
+typedef CGAL::Halfedge_around_target_iterator<Polyhedron>   halfedge_around_target_iterator;
+typedef CGAL::Halfedge_around_face_iterator<Polyhedron>     halfedge_around_face_iterator;
+typedef CGAL::Face_around_face_iterator<Polyhedron>         face_around_face_iterator;
+
 int main(int, char* argv[])
 {
-
   BOOST_CONCEPT_ASSERT((CGAL::Concepts::BidirectionalCirculator<halfedge_around_face_circulator>)) CGAL_UNUSED;
   BOOST_CONCEPT_ASSERT((CGAL::Concepts::BidirectionalCirculator<halfedge_around_target_circulator>)) CGAL_UNUSED;
   BOOST_CONCEPT_ASSERT((CGAL::Concepts::BidirectionalCirculator<vertex_around_target_circulator>)) CGAL_UNUSED;
@@ -56,9 +55,12 @@ int main(int, char* argv[])
   in >> P;
 
   halfedge_descriptor hd = *halfedges(P).first;
+
+  // Circulators
   {
+    std::cout << "halfedge_around_face_circulator: " << std::endl;
     halfedge_around_face_circulator hafc(hd,P), done(hafc);
-    
+
     do {
       std::cout << get(CGAL::vertex_point, P, target(*hafc,P)) << std::endl;
       ++hafc;
@@ -66,8 +68,10 @@ int main(int, char* argv[])
   }
 
   {
+    std::cout << "halfedge_around_target_circulator: " << std::endl;
     halfedge_around_target_circulator havc(hd,P), done(havc);
     vertex_descriptor vd = target(hd,P);
+
     do {
       halfedge_descriptor hd2 = *havc;
       assert(target(hd2,P) == vd);
@@ -75,24 +79,29 @@ int main(int, char* argv[])
       ++havc;
     }while(havc != done);
   }
+
   {
+    std::cout << "vertex_around_target_circulator: " << std::endl;
     vertex_around_target_circulator havc(hd,P), done(havc);
-    
+
     do {
       std::cout << get(CGAL::vertex_point, P, *havc) << std::endl;
       ++havc;
     }while(havc != done);
   }
+
   {
     face_around_target_circulator havc(hd,P), done(havc);
-    
+
     do {
-      //std::cout << get(CGAL::vertex_point, P, *havc) << std::endl;
       ++havc;
     }while(havc != done);
   }
+
   {
+    std::cout << "halfedge_around_source_circulator: " << std::endl;
     halfedge_around_source_circulator havc(hd,P), done(havc);
+
     vertex_descriptor vd = source(hd,P);
     do {
       halfedge_descriptor hd2 = *havc;
@@ -102,7 +111,9 @@ int main(int, char* argv[])
     }while(havc != done);
   }
 
+  // Iterators
   {
+    std::cout << "halfedge_around_target_iterator: " << std::endl;
     halfedge_around_target_iterator vit, end;
     vertex_descriptor vd = target(hd,P);
     boost::tie(vit,end) = halfedges_around_target(hd,P);
@@ -114,7 +125,8 @@ int main(int, char* argv[])
     }
   }
 
-   {
+  {
+    std::cout << "halfedge_around_face_iterator: " << std::endl;
     halfedge_around_face_iterator vit, end;
     boost::tie(vit,end) = halfedges_around_face(hd,P);
     
@@ -124,19 +136,20 @@ int main(int, char* argv[])
       ++vit;
     }
   }
- 
 
   {
+    std::cout << "out_edge_iterator: " << std::endl;
     out_edge_iterator ohi, end;
     for(boost::tie(ohi,end) = out_edges(target(hd,P),P); ohi != end; ++ohi){
       edge_descriptor ed = *ohi;
       halfedge_descriptor hd2 = halfedge(ed,P);
       std::cout << get(CGAL::vertex_point, P, target(hd2,P)) << std::endl;
     }
-  } 
+  }
 
   {
-    BOOST_FOREACH(edge_descriptor ed,out_edges(target(hd,P),P)){
+    std::cout << "out_edges: " << std::endl;
+    BOOST_FOREACH(edge_descriptor ed, out_edges(target(hd,P),P)){
       halfedge_descriptor hd2 = halfedge(ed,P);
       std::cout << get(CGAL::vertex_point, P, target(hd2,P)) << std::endl;
     }
