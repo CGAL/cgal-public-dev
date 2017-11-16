dae6d9da88ea7b2de7c1a003ab8cad0e8d686f5c
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2017-11-16 15:55:38 +0100

    Update test of Christophe. Some DOES NOT COMPILE to correct.

--- Generalized_map/test/Generalized_map/Algo1_only_GMAP.cpp
+++ Generalized_map/test/Generalized_map/Algo1_only_GMAP.cpp
@@ -3,20 +3,21 @@
 #include "Test_algo1test.cpp"
 using namespace std;
 
-int main(int argc, char** argv) {
-	//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+int main(int argc, char** argv)
+{
+  //cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
-	int geometric_weights=1;
-	int boundary=0;
+  int geometric_weights=1;
+  int boundary=0;
 
-	if (argv[2])
-			geometric_weights=atoi(argv[2]);
-	if (argv[3])
-		boundary=atoi(argv[3]);
-	Test_algo1test bt;
-	if (argv[1])
-		bt.test(argv[1],geometric_weights,boundary);
-	else
-		bt.test("sphere2L.off",geometric_weights,boundary);
-	return 0;
+  if (argv[2])
+    geometric_weights=atoi(argv[2]);
+  if (argv[3])
+    boundary=atoi(argv[3]);
+  Test_algo1test bt;
+  if (argv[1])
+    bt.test(argv[1],geometric_weights,boundary);
+  else
+    bt.test("sphere2L.off",geometric_weights,boundary);
+  return 0;
 }
