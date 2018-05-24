1be30c794e5c03e56f28744b5d80782771aa28bd
Dmitry Anisimov <danston@ymail.com>
2018-05-24 14:59:46 +0200

    old code is moved to the test folder

---
+++ Level_of_detail/test/Level_of_detail/include/old/Tools/Tools_include.h
@@ -0,0 +1,25 @@
+#ifndef CGAL_LEVEL_OF_DETAIL_TOOLS_INCLUDE_H
+#define CGAL_LEVEL_OF_DETAIL_TOOLS_INCLUDE_H
+
+#include "../../../../test/Level_of_detail/include/temporary/Level_of_detail_data_structure.h"
+#include <CGAL/Level_of_detail/Tools/Data/Polygon_data_estimator_2.h>
+
+#include <CGAL/Level_of_detail/Tools/General/Preprocessor.h>
+#include <CGAL/Level_of_detail/Tools/General/Simple_utilities.h>
+#include <CGAL/Level_of_detail/Tools/General/Utilities.h>
+
+#include <CGAL/Level_of_detail/Tools/Parameters/Parameters_estimator.h>
+#include <CGAL/Level_of_detail/Tools/Projection/Projector.h>
+
+#include <CGAL/Level_of_detail/Tools/Quality/Level_of_detail_complexity.h>
+#include <CGAL/Level_of_detail/Tools/Quality/Level_of_detail_coverage.h>
+#include <CGAL/Level_of_detail/Tools/Quality/Level_of_detail_distortion.h>
+
+#include <CGAL/Level_of_detail/Tools/Sampling/Polygon_sampler_2.h>
+#include <CGAL/Level_of_detail/Tools/Sampling/Sample_generator_2.h>
+
+#include <CGAL/Level_of_detail/Tools/Selection/Classification_labels_matcher.h>
+#include "../../../../test/Level_of_detail/include/temporary/Tools/Selection/Selection_strategies.h"
+#include "../../../../test/Level_of_detail/include/temporary/Tools/Selection/Selector.h"
+
+#endif // CGAL_LEVEL_OF_DETAIL_TOOLS_INCLUDE_H
