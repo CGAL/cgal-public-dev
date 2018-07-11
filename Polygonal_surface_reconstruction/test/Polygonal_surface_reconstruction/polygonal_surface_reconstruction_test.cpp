32a80eff78333fa14edffe2162fc580685c6bace
Liangliang Nan <15526536+LiangliangNan@users.noreply.github.com>
2018-07-11 23:54:09 +0200

    minor fix

--- Polygonal_surface_reconstruction/test/Polygonal_surface_reconstruction/polygonal_surface_reconstruction_test.cpp
+++ Polygonal_surface_reconstruction/test/Polygonal_surface_reconstruction/polygonal_surface_reconstruction_test.cpp
@@ -34,11 +34,10 @@ int main(int argc, char * argv[])
 
 
 
-//	  argc = 2;
-//    argv[1] = "data/cube.pwn";
-//    argv[1] = "data/ball.ply";
-//    argv[1] = "data/building.ply";
-
+	argc = 2;
+	//argv[1] = "data/cube.pwn";
+	argv[1] = "data/ball.ply";
+	//argv[1] = "data/building.ply";
 
 
 	// usage
