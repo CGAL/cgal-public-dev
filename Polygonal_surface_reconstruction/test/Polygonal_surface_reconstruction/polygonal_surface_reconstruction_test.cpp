2cdaaffeb58044fd2836819c326aa4e5c16ef60b
Liangliang Nan <liangliang.nan@gmail.com>
2018-07-11 23:57:39 +0200

    finished test

--- Polygonal_surface_reconstruction/test/Polygonal_surface_reconstruction/polygonal_surface_reconstruction_test.cpp
+++ Polygonal_surface_reconstruction/test/Polygonal_surface_reconstruction/polygonal_surface_reconstruction_test.cpp
@@ -34,9 +34,9 @@ int main(int argc, char * argv[])
 
 
 
-	argc = 2;
+    //argc = 2;
 	//argv[1] = "data/cube.pwn";
-	argv[1] = "data/ball.ply";
+    //argv[1] = "data/ball.ply";
 	//argv[1] = "data/building.ply";
 
 
