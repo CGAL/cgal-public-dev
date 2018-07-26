ee59ef209582e31ded19bb0939ce90be99702338
kychern <kychern@outlook.com>
2018-07-27 02:20:28 +0800

    add a concave polygon.

--- Barycentric_coordinates_2/examples/Barycentric_coordinates_2/Maximum_entropy_coordinates_example.cpp
+++ Barycentric_coordinates_2/examples/Barycentric_coordinates_2/Maximum_entropy_coordinates_example.cpp
@@ -32,24 +32,19 @@ using std::cout; using std::endl; using std::string;
 
 int main()
 {
-    // Construct a star-shaped polygon.
-    const int number_of_vertices = 3;
+    // Construct a concave polygon.
+    const int number_of_vertices = 6;
     Point_vector vertices(number_of_vertices);
 
     Input_range point_range(number_of_vertices);
 
+    vertices[0]=Point(0.0,0.0); vertices[1]=Point(-1.0,2.0); vertices[2]=Point(-2.0,-2.0);
+    vertices[3]=Point(0.0,-0.5); vertices[4]=Point(3.0,-3.0); vertices[5]=Point(1.0,2.0);
 
-    //vertices[0] = Point(0.0, 0.0); vertices[1] = Point(0.1, -0.8); vertices[2] = Point(0.3, 0.0); vertices[3] = Point(0.6, -0.5); vertices[4]  = Point(0.6 , 0.1);
-    //vertices[5] = Point(1.1, 0.6); vertices[6] = Point(0.3,  0.2); vertices[7] = Point(0.1, 0.8); vertices[8] = Point(0.1,  0.2); vertices[9] = Point(-0.7, 0.0);
-    vertices[0]=Point(0.0,0.0); vertices[1]=Point(2.0,0.5);
-    vertices[2]=Point(1.0,2.0);
-
-    point_range[0]=Point_with_property(vertices[0],false);
-    point_range[1]=Point_with_property(vertices[1],false);
-    point_range[2]=Point_with_property(vertices[2],false);
-
-    Point_map point_map;
-
+    for(size_t i = 0; i < number_of_vertices; ++i)
+    {
+        point_range[i]=Point_with_property(vertices[i],false);
+    }
 
     // Create an std::vector to store coordinates.
     Scalar_vector coordinates;
@@ -61,24 +56,11 @@ int main()
 
 
     // Instantiate some interior points in the polygon.
-    //const int number_of_interior_points = 2;
-    //const Point interior_points[] = { Point(0.12, -0.45), Point(0.55, -0.3), Point(0.9 , 0.45),
-    //                                  Point(0.15,  0.35), Point(-0.4, 0.04), Point(0.11, 0.11),
-    //                                  Point(0.28,  0.12), // the only point in the kernel of the star shaped polygon
-    //                                  Point(0.55,  0.11)
-    //                                };
-    //const Point interior_points[] = { Point(0.5, 0.5), Point(0.01, 0.01) };
-    const int number_of_interior_points = 9;
-    const Point interior_points[] = { Point(0.5f , 0.5f ), Point(1.0f, 0.5f ), Point(1.0f , 0.75f), Point(1.0f , 1.0f),                     // interior query points
-                                   Point(1.0f , 1.25f), Point(1.0f, 1.5f ), Point(0.75f, 1.0f ), Point(1.25f, 1.0f), Point(1.5f, 0.75f),
-                                   //Point(1.0f , 0.25f), Point(0.5f, 1.0f ), Point(1.5f , 1.25f), Point(1.0f , 2.0f), Point(2.0f, 0.5f ), // boundary query points
-                                   //Point(0.25f, 1.0f ), Point(0.5f, 1.75f), Point(1.5f , 1.75f), Point(1.75f, 1.5f)                      // exterior query points
+    const int number_of_interior_points = 7;
+    const Point interior_points[] = { Point(-1.0f , 1.0f ), Point(-1.5f, -1.5f ), Point(0.0f , -0.25f), Point(1.0f , 1.5f),
+                                   Point(0.75f , -0.25f), Point(2.0f, -2.0f ), Point(2.75f, -2.75f )
                                  };
 
-    // Compute mean value coordinates for all the defined interior points.
-
-    // We speed up the computation using the O(n) algorithm called with the parameter CGAL::Barycentric_coordinates::FAST.
-    // The default one is CGAL::Barycentric_coordinates::PRECISE.
     const CGAL::Barycentric_coordinates::Type_of_algorithm type_of_algorithm = CGAL::Barycentric_coordinates::FAST;
 
     // We also speed up the computation by using the parameter query_point_location = CGAL::Barycentric_coordinates::ON_BOUNDED_SIDE.
@@ -94,38 +76,6 @@ int main()
         for(int j = 0; j < number_of_vertices; ++j)
             cout << "Coordinate " << j + 1 << " = " << coordinates[i * number_of_vertices + j] << endl;
     }
-
-    /*
-    // If we need only the unnormalized weights for some point (lets take the last one), we can compute them as follows.
-
-    // Instantiate an std::vector to store weights.
-    Scalar_vector weights;
-
-    // Compute mean value weights.
-    const int last_point_index = number_of_interior_points - 1;
-    const Output_type result = maximum_entropy_coordinates.compute_weights(interior_points[last_point_index], std::back_inserter(weights));
-
-    // Compute their sum.
-    Scalar mv_denominator = Scalar(0);
-    for(int j = 0; j < number_of_vertices; ++j) mv_denominator += weights[j];
-
-    // Invert this sum.
-    const Scalar mv_inverted_denominator = Scalar(1) / mv_denominator;
-
-    // Output the mean value weights.
-    const string status = (result ? "SUCCESS." : "FAILURE.");
-    cout << endl << "Status of the weights' computation for the point " << last_point_index + 1 << ": " << status << endl;
-
-    for(int j = 0; j < number_of_vertices; ++j)
-        cout << "Weight " << j + 1 << " = " << weights[j] << endl;
-
-    // Now, if we normalize the weights, we recover values of the mean value coordinates for the last point computed earlier.
-    cout << endl << "After normalization, for the point " << last_point_index + 1 << " mean value coordinates are " << endl;
-    for(int j = 0; j < number_of_vertices; ++j)
-        cout << "Coordinate " << j + 1 << " = " << weights[j] * mv_inverted_denominator << endl;
-    cout << endl;
-    */
-
 
     return EXIT_SUCCESS;
 }
