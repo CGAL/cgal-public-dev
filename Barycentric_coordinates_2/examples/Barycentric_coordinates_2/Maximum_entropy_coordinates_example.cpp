80e112a62f079fe726053656f6a915fe6ec6bd3a
kychern <kychern@outlook.com>
2018-08-01 00:24:32 +0800

    parameterized Maximum_entropy_2 class with a new template `Parameters`.

--- Barycentric_coordinates_2/examples/Barycentric_coordinates_2/Maximum_entropy_coordinates_example.cpp
+++ Barycentric_coordinates_2/examples/Barycentric_coordinates_2/Maximum_entropy_coordinates_example.cpp
@@ -1,4 +1,5 @@
 #include <CGAL/Barycentric_coordinates_2/Maximum_entropy_2.h>
+#include <CGAL/Barycentric_coordinates_2/Maximum_entropy_2/Maximum_entropy_parameters.h>
 #include <CGAL/Barycentric_coordinates_2/Maximum_entropy_2/Maximum_entropy_solver.h>
 #include <CGAL/Barycentric_coordinates_2/Maximum_entropy_2/Maximum_entropy_prior_function_type_one.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
@@ -22,10 +23,11 @@ typedef std::vector<Point_with_property> Input_range;
 typedef std::back_insert_iterator<Scalar_vector> Vector_insert_iterator;
 typedef boost::optional<Vector_insert_iterator> Output_type;
 
+typedef CGAL::Barycentric_coordinates::Maximum_entropy_parameters<Kernel> MEC_parameters;
 typedef CGAL::Barycentric_coordinates::Maximum_entropy_newton_solver<Kernel> MEC_newton_solver;
 typedef CGAL::Barycentric_coordinates::Maximum_entropy_prior_function_type_one<Kernel> MEC1_prior;
 
-typedef CGAL::Barycentric_coordinates::Maximum_entropy_2<Kernel, MEC1_prior, MEC_newton_solver> Maximum_entropy;
+typedef CGAL::Barycentric_coordinates::Maximum_entropy_2<Kernel, MEC1_prior, MEC_newton_solver, MEC_parameters> Maximum_entropy;
 typedef CGAL::Barycentric_coordinates::Generalized_barycentric_coordinates_2<Maximum_entropy, Input_range, Point_map, Kernel> Maximum_entropy_coordinates;
 
 using std::cout; using std::endl; using std::string;
