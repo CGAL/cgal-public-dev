d0dbe88edf2c0717b5bbaf8658996dfed4390485
Baskin Senbaslar <basbursen@gmail.com>
2019-06-20 20:11:53 +0300

    libigl simplification example, move matrix definitions outside of duration calculations

--- Surface_mesh_simplification/examples/Surface_mesh_simplification/other_library_examples/libigl_simplification.cpp
+++ Surface_mesh_simplification/examples/Surface_mesh_simplification/other_library_examples/libigl_simplification.cpp
@@ -17,17 +17,16 @@ int main(int argc, char* argv[]) {
   Eigen::MatrixXi F;
   igl::readOFF(argv[1], V, F);
 
-  //igl::opengl::glfw::Viewer viewer;
-  //viewer.data().set_mesh(V, F);
-  //viewer.launch();
-  std::chrono::steady_clock::time_point start_time
-    = std::chrono::steady_clock::now();
+
 
   Eigen::MatrixXd U;
   Eigen::MatrixXi G;
   Eigen::VectorXi J, I;
-  igl::qslim(V, F, std::stoi(argv[2]), U, G, J, I);
 
+  std::chrono::steady_clock::time_point start_time
+    = std::chrono::steady_clock::now();
+
+  igl::qslim(V, F, std::stoi(argv[2]), U, G, J, I);
 
   std::chrono::steady_clock::time_point end_time
     = std::chrono::steady_clock::now();
