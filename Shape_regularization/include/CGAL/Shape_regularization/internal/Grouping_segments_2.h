54733ece6a76434aa7cd7551e3aac43b21cddc4a
Gennadii Sytov <silvics@protonmail.com>
2019-07-25 00:04:25 -0700

    Cleaned up the "Grouping_segments_2" class; Deleted the "Grouping_segments_ordinates_2" class.

--- Shape_regularization/include/CGAL/Shape_regularization/internal/Grouping_segments_2.h
+++ Shape_regularization/include/CGAL/Shape_regularization/internal/Grouping_segments_2.h
@@ -51,25 +51,24 @@ namespace internal {
       CGAL_precondition(qp_result.size() > 0);
       groups_by_value.clear();
       
-      std::map <std::size_t, std::vector<std::size_t>> groups;
-      std::map<std::size_t, int> segments_to_groups_hashmap;
+      m_groups.clear();
+      m_segments_to_groups_hashmap.clear();
 
       for (const auto & it : segments) {
         std::size_t seg_index = it.second.m_index;
-        segments_to_groups_hashmap[seg_index] = -1;
+        m_segments_to_groups_hashmap[seg_index] = -1;
       }
 
-      build_initial_groups(n, t_ijs, r_ijs, segments, qp_result, groups, segments_to_groups_hashmap);
+      build_initial_groups(n, t_ijs, r_ijs, segments, qp_result);
 
       std::map<int, FT> values;
 
-      build_map_of_values(qp_result, segments, groups, 
-                          segments_to_groups_hashmap, values);
+      build_map_of_values(qp_result, segments, values);
 
       // Try to assign segments whose orientation has not been optimized thanks to the regularization process, to an existing group.
-      assign_segments_to_groups(segments, groups, segments_to_groups_hashmap, values);
+      assign_segments_to_groups(segments, values);
 
-      build_groups_by_value(segments_to_groups_hashmap, values, groups_by_value);
+      build_groups_by_value(values, groups_by_value);
 
     } 
 
@@ -77,119 +76,69 @@ namespace internal {
     FT m_eps;
     const FT m_tolerance;
     Regularization_type m_type;
+    std::map<std::size_t, int> m_segments_to_groups_hashmap;
+    std::map <std::size_t, std::vector<std::size_t>> m_groups;
     
     void build_initial_groups(const std::size_t n,
                               const std::map <std::pair<std::size_t, std::size_t>, std::pair<FT, std::size_t>> & t_ijs,
                               const std::map <std::pair<std::size_t, std::size_t>, std::pair<int, std::size_t>> & r_ijs,
                               const std::map <std::size_t, Segment_data> & segments,
-                              const std::vector<FT> & qp_result,
-                              std::map<std::size_t, std::vector<std::size_t>> & groups,
-                              std::map<std::size_t, int> & segments_to_groups_hashmap) {
+                              const std::vector<FT> & qp_result) {
 
       std::size_t g = 0;
-      int g_i = -1; 
-      int g_j = -1;
-
       auto rel_it = r_ijs.begin();
-      for (const auto & tar_it : t_ijs) {
 
+      for (const auto & tar_it : t_ijs) {
         const std::size_t i = tar_it.first.first;
         const std::size_t j = tar_it.first.second;
         const std::size_t p = tar_it.second.second;
+
         int r = 0;
         if (rel_it != r_ijs.end()) {
           CGAL_precondition(rel_it->second.second == p);
           r = rel_it->second.first;
         }
+        CGAL_postcondition(r == 0 || r == 1);
 
-        if (CGAL::abs(qp_result[n + p]) < m_tolerance) { 
-
-          if (segments_to_groups_hashmap[i] == -1 && segments_to_groups_hashmap[j] == -1) {
-            switch (r) {
-              case 0:
-                // Then segments i and j belong to the same group of parallel segments.
-                // We should create a group of segments, that is initialized with these two individuals.
-                segments_to_groups_hashmap[i] = g;
-                segments_to_groups_hashmap[j] = g;
-
-                groups[g].push_back(i);
-                groups[g].push_back(j);
-
-                ++g;
-                break;
-              case 1:
-                // The segments i and j are orthogonal.
-                // We create two different groups of parallel segments.
-                segments_to_groups_hashmap[i] = g;
-                groups[g].push_back(i);
-                segments_to_groups_hashmap[j] = ++g;
-                groups[g].push_back(j);
-                ++g;
-                break;
-            }
-          }
-          else if (segments_to_groups_hashmap[i] == -1 && segments_to_groups_hashmap[j] != -1) {
-            switch (r) {
-              case 0:
-                // Then segment i is parallel to j, and can be assigned to the same group.
-                g_j = segments_to_groups_hashmap[j];
-                segments_to_groups_hashmap[i] = g_j;
-                groups[g_j].push_back(i);
-                break;
-              case 1:
-                // Then segment i is orthogonal to j, and we should initialize a new group with this segment.
-                segments_to_groups_hashmap[i] = g;
-                groups[g].push_back(i);
-                ++g;
-                break;
-            }
-          }
-          else if (segments_to_groups_hashmap[i] != -1 && segments_to_groups_hashmap[j] == -1) {
-            // Symmetrical situation to before.
-            switch (r) {
-              case 0:
-                g_i = segments_to_groups_hashmap[i];
-                segments_to_groups_hashmap[j] = g_i;
-                groups[g_i].push_back(j);
-                break;
-              case 1:
-                segments_to_groups_hashmap[j] = g;
-                groups[g].push_back(j);
-                ++g;
-                break;
-            }
-          }
-          else {
-            g_i = segments_to_groups_hashmap[i];
-            g_j = segments_to_groups_hashmap[j];
-            if (g_i != g_j) {
-              if (r == 0) {                       
-                // Segments i and j have been assigned to different groups, but in fact
-                // they are parallel and belong to the same group. That's why we merge them.
-                for (const auto gr : groups[g_j]) {
-                  segments_to_groups_hashmap[gr] = g_i;
-                  groups[g_i].push_back(gr);
-                }
-                groups[g_j].clear();
-              } else if (r == 1) {
-                // We do nothing here.
-              }
-            }
-          }
+        if (CGAL::abs(qp_result[n + p]) >= m_tolerance) { 
+          if(rel_it != r_ijs.end()) ++rel_it;
+          continue;
         }
-        if(rel_it != r_ijs.end())
-          ++rel_it;
+
+        const int g_i = m_segments_to_groups_hashmap[i];
+        const int g_j = m_segments_to_groups_hashmap[j];
+        const int groups_status = check_group_status(g_i, g_j);
+
+        switch (groups_status) {
+          case -1: break;
+
+          case 1:
+            r == 0 ? create_single_group(i, j, g) : create_separate_groups(i, j, g);
+            break;
+
+          case 2:
+            r == 0 ? assign_segment_to_group(i, j) : create_new_group(i, g);
+            break;
+
+          case 3:
+            r == 0 ? assign_segment_to_group(j, i) : create_new_group(j, g);
+            break;
+
+          case 4:
+            if (r == 0) merge_two_groups(g_i, g_j);
+            break;
+        }
+  
+        if(rel_it != r_ijs.end()) ++rel_it;
       }
 
     }
 
     void build_map_of_values(const std::vector<FT> & qp_result,
                              const std::map <std::size_t, Segment_data> & segments,
-                             std::map<std::size_t, std::vector<std::size_t>> & groups,
-                             std::map<std::size_t, int> & segments_to_groups_hashmap,
                              std::map<int, FT> & values) {
 
-      for (const auto & sm_i : segments_to_groups_hashmap) {
+      for (const auto & sm_i : m_segments_to_groups_hashmap) {
         int g_i = sm_i.second;
 
         if (g_i != -1 && (values.find(g_i) == values.end())) {
@@ -203,58 +152,50 @@ namespace internal {
             if (CGAL::abs(it_m.second - val) < m_eps) 
               g_j = it_m.first;
           }
+
           if (g_j == -1) 
             values[g_i] = val;
-          else {                       
-            // Merge groups.
-            for (const auto gr : groups[g_i]) {
-              segments_to_groups_hashmap[gr] = g_j;
-              groups[g_j].push_back(gr);
-            }
-            groups[g_i].clear();
-          }
+          else                       
+            merge_two_groups(g_j, g_i);
+
         }
       }
 
     }
 
     void assign_segments_to_groups(const std::map <std::size_t, Segment_data> & segments,
-                                   std::map<std::size_t, std::vector<std::size_t>> & groups,
-                                   std::map<std::size_t, int> & segments_to_groups_hashmap,
                                    std::map<int, FT> & values) {
 
-      for (const auto & sm_i : segments_to_groups_hashmap) {
+      for (const auto & sm_i : m_segments_to_groups_hashmap) {
         int g_i = sm_i.second;
         if (g_i == -1) {
           const std::size_t seg_index = sm_i.first;
           const Segment_data & seg_data = segments.at(seg_index);
           const FT val = get_value_2(seg_data);
           int g_j = -1;
+
           for (const auto & it_m : values) {
             const FT val_j = it_m.second;
             g_j = get_g_j(val, val_j, it_m);
             if (g_j != -1) 
               break;
           }
+
           if (g_j == -1) {   
-            if (values.size() > 0)               
-              g_i = values.rbegin()->first + 1;
-            else
-              g_i = 0;
+            values.size() > 0 ? g_i = values.rbegin()->first + 1 : g_i = 0;
             values[g_i] = val;
           } 
           else 
             g_i = g_j;
 
-          segments_to_groups_hashmap[seg_index] = g_i; // Segmentation fault (core dumped) for the example with 3 segments and bounds = 25
-          groups[g_i].push_back(seg_index);
+          m_segments_to_groups_hashmap[seg_index] = g_i;
+          m_groups[g_i].push_back(seg_index);
         }
       }
 
     }
 
-    void build_groups_by_value(const std::map <std::size_t, int> & segments_to_groups_hashmap,
-                               std::map <int, FT> & values, 
+    void build_groups_by_value(std::map <int, FT> & values, 
                                std::map <FT, std::vector<std::size_t>> & groups_by_value) {
 
       for (const auto & it_m : values) {
@@ -263,7 +204,7 @@ namespace internal {
           groups_by_value[val] = std::vector<std::size_t>();
       }
 
-      for (const auto & sm_i : segments_to_groups_hashmap) {
+      for (const auto & sm_i : m_segments_to_groups_hashmap) {
         // If segment s_i is included in a group of parallel segments,
         // then it should be assigned to a leaf of the regularization tree.
         const FT val = values.at(sm_i.second);
@@ -272,6 +213,60 @@ namespace internal {
       }
 
     } 
+
+    int check_group_status(const int g_i, const int g_j) {
+
+      if (g_i == -1 && g_j == -1) return 1;
+      if (g_i == -1 && g_j != -1) return 2;
+      if (g_i != -1 && g_j == -1) return 3;
+      if (g_i != -1 && g_j != -1 && g_i != g_j) return 4;
+      return -1;
+
+    }
+
+    void create_single_group (const std::size_t i, const std::size_t j, std::size_t & g) {
+
+      m_segments_to_groups_hashmap[i] = g;
+      m_segments_to_groups_hashmap[j] = g;
+
+      m_groups[g].push_back(i);
+      m_groups[g].push_back(j); 
+
+      ++g;
+    }
+
+    void create_separate_groups(const std::size_t i, const std::size_t j, std::size_t & g) {
+
+      create_new_group(i, g);
+      create_new_group(j, g);
+
+    }
+
+    void assign_segment_to_group(const std::size_t i, const std::size_t j) {
+
+      const int g_j = m_segments_to_groups_hashmap[j];
+      m_segments_to_groups_hashmap[i] = g_j;
+      m_groups[g_j].push_back(i);
+
+    }
+
+    void create_new_group(const std::size_t i, std::size_t & g) {
+
+      m_segments_to_groups_hashmap[i] = g;
+      m_groups[g].push_back(i);
+      ++g;
+
+    }
+
+    void merge_two_groups(const int g_i, const int g_j) {
+
+      for (const auto gr : m_groups[g_j]) {
+        m_segments_to_groups_hashmap[gr] = g_i;
+        m_groups[g_i].push_back(gr);
+      }
+      m_groups[g_j].clear();
+      
+    }
 
     FT get_value_1(const Segment_data & seg_data,
                      const std::vector<FT> & qp_result) {
@@ -289,6 +284,7 @@ namespace internal {
 
           break;
         }
+
         case ORDINATES:
           val = seg_data.m_reference_coordinates.y() + qp_result[seg_index];
           break;
