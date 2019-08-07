0cbcc3d2f5609983b7d58ef7266147392b87eae7
Gennadii Sytov <silvics@protonmail.com>
2019-08-07 01:58:53 -0700

    Modified the bounds in the test_216_segments; Work in progress: test_ordiantes_150_random_segments.

--- Shape_regularization/test/Shape_regularization/test_ordinates_150_random_segments.cpp
+++ Shape_regularization/test/Shape_regularization/test_ordinates_150_random_segments.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <list>
 
 // CGAL includes.
 #include <CGAL/property_map.h>
@@ -12,12 +11,17 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Shape_regularization.h>
 
+// for random generator
+#include <algorithm>
+#include <CGAL/point_generators_2.h>
+#include <CGAL/function_objects.h>
+#include <CGAL/Join_input_iterator.h>
+#include <CGAL/Counting_iterator.h>
+
 #include "saver_segments_2.h"
 
 // Typedefs.
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Traits;
-// typedef CGAL::Simple_cartesian<double> Traits;
-// typedef CGAL::Exact_predicates_exact_constructions_kernel Traits;
 
 using Segment_2 = typename Traits::Segment_2;
 using Point_2 = typename Traits::Point_2;
@@ -33,25 +37,35 @@ using Regularization_type_ordinates = CGAL::Regularization::Ordinate_regularizat
 using Shape_regularization_angles = CGAL::Regularization::Shape_regularization
   <Traits, Input_range, Neighbor_query, Regularization_type_angles>;
 using Shape_regularization_ordinates = CGAL::Regularization::Shape_regularization
-  <Traits, Input_range, Neighbor_query, Regularization_type_ordinates>;
-using Parallel_groups = CGAL::Regularization::Parallel_groups_2<Traits, Input_range, Segment_map>;
+    <Traits, Input_range, Neighbor_query, Regularization_type_ordinates>;
 
 using Saver = CGAL::Regularization::Saver_segments_2<Traits>;
+typedef CGAL::Points_on_segment_2<Point_2>              PG;
+typedef CGAL::Creator_uniform_2< Point_2, Segment_2>    Creator;
+typedef CGAL::Join_input_iterator_2< PG, PG, Creator>   Segm_iterator;
+typedef CGAL::Counting_iterator<Segm_iterator, Segment_2>  Count_iterator;
+
 
 int main() {
 
   Input_range input_range;
-  const std::string testpath = "/media/D/gsoc2019/cgal-dev/Shape_regularization/examples/Shape_regularization/data/test.polylines";
-  std::cout << testpath << std::endl;
-  std::ifstream file(testpath.c_str(), std::ifstream::in);
-  file.precision(15);
+  input_range.reserve(100);
 
-  Point_2 s, t; double stub;
-  while (!file.eof()) {
-    file >> stub >> s >> stub >> t >> stub;
-    input_range.push_back(Segment_2(s, t));
-  }
-  input_range.erase(input_range.begin() + input_range.size() - 1);
+  // A vertical like fan.
+  PG p1( Point_2(-50,-20), Point_2(0,-20),50);
+  PG p2( Point_2(-50, 0), Point_2( 0, 0),50);
+  Segm_iterator  t1( p1, p2);
+  Count_iterator t1_begin( t1);
+  Count_iterator t1_end( t1, 50);
+  std::copy( t1_begin, t1_end, std::back_inserter(input_range));
+
+  PG p3( Point_2(-51, 1), Point_2(-1, 1),50);
+  PG p4( Point_2(-51, 21), Point_2( -1, 21),50);
+  Segm_iterator  t2( p3, p4);
+  Count_iterator t2_begin( t2);
+  Count_iterator t2_end( t2, 50);
+  std::copy( t2_begin, t2_end, std::back_inserter(input_range));
+
 
   std::cout.precision(15);
 
@@ -62,17 +76,15 @@ int main() {
   std::cout << std::endl;
 
   Saver saver;
-  saver.save_segments(input_range, "test_216_segments_before");
+  saver.save_segments(input_range, "test_ordinates_150_random_segments_before");
 
-  // Create instances of the classes Neighbor_query and Regularization_type.
-  // Neighbor_query neighbor_query_angles(input_range);
   Neighbor_query neighbor_query(input_range);
   std::vector<std::size_t> vec;
   vec.resize(input_range.size());
   std::iota(vec.begin(), vec.end(), 0);
   neighbor_query.add_group(vec);
 
-  const FT bound_angles = FT(90);
+  const FT bound_angles = FT(40);
   Regularization_type_angles regularization_type_angles(input_range, bound_angles);
   regularization_type_angles.add_group(vec);
 
@@ -81,12 +93,16 @@ int main() {
 
   shape_regularization_angles.regularize();
 
+  std::cout << "Number of modified segments angles: " << regularization_type_angles.number_of_modified_segments() << std::endl;
 
-  // Regularization for ordinates:
   std::vector <std::vector <std::size_t>> parallel_groups;
   regularization_type_angles.parallel_groups(std::back_inserter(parallel_groups));
 
-  const FT bound_ordinates = FT(5);
+  std::cout << "parallel_groups.size() = " << parallel_groups.size() << std::endl;
+
+  // Regularization for ordinates:
+
+  const FT bound_ordinates = FT(1.1);
   Regularization_type_ordinates regularization_type_ordinates(input_range, bound_ordinates);
 
   neighbor_query.clear();
@@ -95,16 +111,32 @@ int main() {
     regularization_type_ordinates.add_group(group);
   }
 
-  Shape_regularization_ordinates shape_regularization_ordinates(
+  Shape_regularization_ordinates Shape_regularization_ordinates(
     input_range, neighbor_query, regularization_type_ordinates);
-
-  shape_regularization_ordinates.regularize();
+  Shape_regularization_ordinates.regularize();
   
   std::cout << "AFTER:" << std::endl;
   for (const auto& segment : input_range)
     std::cout << segment << std::endl;
-  std::cout << std::endl; 
-  saver.save_segments(input_range, "test_216_segments_after"); 
+  std::cout << std::endl;
+  saver.save_segments(input_range, "test_4_segments_after"); 
+
+  std::size_t counter_ord = 0;
+  std::cout << "After ordinates: " << std::endl;
+  for (const auto& segment : input_range) {
+    const FT point1 = segment.source().x() + segment.source().y();
+    const FT point2 = segment.target().x() + segment.target().y();
+    const FT both = point1 + point2;
+    std::cout << ++counter_ord << "). " << both << std::endl;
+  }
+  std::cout << "Number of modified segments ordinates: " << regularization_type_ordinates.number_of_modified_segments() << std::endl;
+
   
+  std::cout << "AFTER:" << std::endl;
+  for (const auto& segment : input_range)
+    std::cout << segment << std::endl;
+  std::cout << std::endl;
+  saver.save_segments(input_range, "test_ordinates_150_random_segments_after"); 
+
   return EXIT_SUCCESS;
 }
