fb9565c440b605abef630710528fae4a49a0c2eb
Jackson Campolattaro <jackcamp@vt.edu>
2020-06-15 12:52:26 -0400

    Create benchmark with reliable results

--- Octree/benchmark/Octree/bench.cpp
+++ Octree/benchmark/Octree/bench.cpp
@@ -6,11 +6,13 @@
 
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Point_set_3.h>
+#include <CGAL/Point_set_3/IO.h>
 
 #include <fstream>
 #include <string>
 #include <chrono>
 #include <ctime>
+#include <string>
 
 typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_3 Point;
@@ -19,8 +21,39 @@ typedef CGAL::Octree
         <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
         Octree;
 
+#define DATA_FILE_PATH "../data/archer_statue_scan.ply"
 #define BUCKET_SIZE 20
 #define MAX_DEPTH 10
+#define NUM_RUNS 40
+
+std::chrono::duration<double> benchmark(Point_set points) {
+
+  std::cout << "Benchmarking" << std::endl;
+
+  std::chrono::duration<double> elapsed(0);
+
+  for (int i = 0; i < NUM_RUNS; ++i) {
+
+    auto points_copy = points;
+    auto point_map = points_copy.point_map();
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    Octree octree(points, point_map);
+    octree.refine(MAX_DEPTH, BUCKET_SIZE);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "." << std::flush;
+
+    elapsed += (end - start) / NUM_RUNS;
+  }
+
+
+  std::cout << std::endl << "Averaged "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() << " ms" << std::endl;
+  return elapsed;
+}
 
 int main(void) {
 
@@ -34,24 +67,30 @@ int main(void) {
   // Create a new file at that path
   std::ofstream file(file_path.str());
 
-  // Add a header to the file
+  // Add a header to the file with the time it was run
   file << std::put_time(std::localtime(&time_run), "%c") << std::endl;
   file << std::endl;
 
   // Load the test data
-  // TODO
+  std::ifstream stream(DATA_FILE_PATH);
+  Point_set points;
+  stream >> points;
 
   // Describe the tests
   file << "Benchmark Configuration" << std::endl;
   file << "~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
   file << "Bucket Size: " << BUCKET_SIZE << std::endl;
   file << "Max Depth: " << MAX_DEPTH << std::endl;
-  file << "Point Count: " << 5 << std::endl;
+  file << "Dataset Source: " << DATA_FILE_PATH << std::endl;
+  file << "Point Count: " << points.number_of_points() << std::endl;
+  file << "Number of Runs: " << NUM_RUNS << std::endl;
+  file << std::endl;
 
   // Show the results
   file << "Results" << std::endl;
   file << "~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
-  // TODO
+  file << "Tree construction time: "
+       << std::chrono::duration_cast<std::chrono::milliseconds>(benchmark(points)).count() << " ms" << std::endl;
 
   // Close the file
   file.close();
