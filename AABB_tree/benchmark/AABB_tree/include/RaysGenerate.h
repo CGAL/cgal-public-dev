aa38815c71f08bbc2378d22f42cc11c8dad2f62f
Ayush Saraswat <ayushsara12@gmail.com>
2020-06-18 16:46:36 +0530

    Seed contructor added.

--- AABB_tree/benchmark/AABB_tree/include/RaysGenerate.h
+++ AABB_tree/benchmark/AABB_tree/include/RaysGenerate.h
@@ -12,15 +12,31 @@ class RaysGenerate{
 private:
 public:
     RaysGenerate(int n);
+    RaysGenerate(int n, unsigned int seed);
     ~RaysGenerate();
 
     int _n;
+    unsigned int _seed;
     std::vector<directions> rayDirections;
 };
 
 RaysGenerate::RaysGenerate(int n): _n(n){
 
     CGAL::Random rand;
+
+    for(size_t i=0; i!=n; ++i){
+
+        directions direction;
+        direction._x = rand.get_double(-1.0, 1.0);
+        direction._y = rand.get_double(-1.0, 1.0); 
+        direction._z = rand.get_double(-1.0, 1.0); 
+        rayDirections.push_back(direction);
+    }
+}
+
+RaysGenerate::RaysGenerate(int n, unsigned int seed): _n(n), _seed(seed){
+
+    CGAL::Random rand(seed);
 
     for(size_t i=0; i!=n; ++i){
 
