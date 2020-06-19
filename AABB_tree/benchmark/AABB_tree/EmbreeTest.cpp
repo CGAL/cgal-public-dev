292b16df3dfa8bcd49c332ea7fb71a2eb43b9ec7
Ayush Saraswat <ayushsara12@gmail.com>
2020-06-19 16:54:34 +0530

    added visualisation feature.

--- AABB_tree/benchmark/AABB_tree/EmbreeTest.cpp
+++ AABB_tree/benchmark/AABB_tree/EmbreeTest.cpp
@@ -11,7 +11,7 @@
 
 #include "RaysGenerate.h"
 
-typedef CGAL::Simple_cartesian<double> K;
+typedef CGAL::Simple_cartesian<float> K;
 typedef K::Point_3 Point;
 typedef CGAL::Surface_mesh<Point> Mesh;
 typedef Mesh::Vertex_index vertex_descriptor;
@@ -36,16 +36,30 @@ RTCDevice initializeDevice()
   return device;
 }
 
+void visualisation(const RTCRayHit& rayhit,std::ofstream& output){
+        if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID){
+            float factor = rayhit.ray.tfar/ sqrt(pow(rayhit.ray.dir_x, 2)+ pow(rayhit.ray.dir_y, 2)+ pow(rayhit.ray.dir_z, 2));
+            float outX = rayhit.ray.org_x + factor * rayhit.ray.dir_x;
+            float outY = rayhit.ray.org_y + factor * rayhit.ray.dir_y;
+            float outZ = rayhit.ray.org_z + factor * rayhit.ray.dir_z;
+            // std::cout<<rayhit.hit.primID<<std::endl;
+            output<<outX<<" "<<outY<<" "<<outZ<<std::endl; 
+        }
+}
 
 int main(int argc, char *argv[])
 {   
     bool help = false;
     bool offFile = false;
+    bool visual = false;
+
     for (int i = 1; i < argc; i++) {
         if ( (strcmp( "-h", argv[i]) == 0) || (strcmp( "-help", argv[i]) == 0)) 
             help = true;
         else if ( strcmp( "-o", argv[i]) == 0)
-            offFile = true;    
+            offFile = true; 
+        else if ( strcmp( "-v", argv[i]) == 0)
+            visual = true; 
     }
     if(argc == 1 || help){
         std::cerr << "Usage: " << argv[0] << " <infile> <NumberOfRays> <XPoint> <YPoint> <ZPoint> <-o>[if the input file is .off]"<< std::endl;
@@ -58,6 +72,9 @@ int main(int argc, char *argv[])
 
     const char* filename =  argv[1];
     std::ifstream input(filename);
+
+    std::ofstream output;
+    if(visual) output.open("EmbreeOut.xyz");
 
     std::stringstream ss(argv[2]);
     int _numberOfRays = 0;
@@ -99,7 +116,9 @@ int main(int argc, char *argv[])
                                                               sizeof(Triangle),
                                                               surfaceMesh.number_of_faces());
     
-    if (vertices && triangles) std::cout<<"Error in creating Buffer objects."<<std::endl;
+    if ( vertices && triangles) ;
+    else
+        std::cout<<"Error in creating Buffer objects."<<std::endl;
 
     for(vertex_descriptor vd : surfaceMesh.vertices()){
         Point data = surfaceMesh.point(vd);
@@ -120,6 +139,8 @@ int main(int argc, char *argv[])
         triangles[fd.idx()].v0 = temp[0];
         triangles[fd.idx()].v1 = temp[1];
         triangles[fd.idx()].v2 = temp[2];
+        // std::cout<<"Index: "<<fd.idx()<<std::endl;
+        // std::cout<<triangles[fd.idx()].v0<<" "<<triangles[fd.idx()].v1<<" "<<triangles[fd.idx()].v2<<std::endl;
     }
 
     CGAL::Real_timer time;
@@ -136,8 +157,6 @@ int main(int argc, char *argv[])
     std::cout << "  Construction time: " << time.time() << std::endl;
     time.reset();
 
-    struct RTCIntersectContext context;
-    rtcInitIntersectContext(&context);
 
     int numberOfRays = _numberOfRays; /*NUMBER OF RAY QUERIES*/
     RaysGenerate rg(numberOfRays); 
@@ -146,14 +165,17 @@ int main(int argc, char *argv[])
     
     for(size_t n=0; n!=numberOfRays; ++n){
     
-        RTCRayHit rayhit;
+        struct RTCIntersectContext context;
+        rtcInitIntersectContext(&context);
+
+        struct RTCRayHit rayhit;
         rayhit.ray.org_x =  _xPoint; /*POINT.X*/ 
         rayhit.ray.org_y =  _yPoint; /*POINT.Y*/
         rayhit.ray.org_z =  _zPoint; /*POINT.Z*/
 
-        rayhit.ray.dir_x = rg.rayDirections[n]._x;
-        rayhit.ray.dir_x = rg.rayDirections[n]._y;
-        rayhit.ray.dir_x = rg.rayDirections[n]._z;
+        rayhit.ray.dir_x = rg.normalisedRayDirections[n]._x;
+        rayhit.ray.dir_y = rg.normalisedRayDirections[n]._y;
+        rayhit.ray.dir_z = rg.normalisedRayDirections[n]._z;
 
         rayhit.ray.tnear = 0;
         rayhit.ray.tfar = std::numeric_limits<float>::infinity();
@@ -166,14 +188,8 @@ int main(int argc, char *argv[])
         rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
 
         rtcIntersect1(scene, &context, &rayhit);
-        // if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID){
-        //     std::cout<<"Found intersection on geometry "<<
-        //     rayhit.hit.geomID<<", primitive "<<rayhit.hit.primID<<
-        //     " at tfar="<<rayhit.ray.tfar<<std::endl;
-        // }
-        // else
-        //     printf("Did not find any intersection.\n");
-    
+        if (visual) visualisation(rayhit, output);
+
     }
     time.stop();
     std::cout << "  Function() time: " << time.time() << std::endl;
