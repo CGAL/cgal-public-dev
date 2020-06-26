d59ab3fc117b16d37b5491a688e58a0ba43a6987
Jing Yang <jingyang.carl.work@gmail.com>
2020-06-26 16:33:04 -0700

    clean code

--- Surface_mesh/examples/Surface_mesh/draw_surface_mesh.cpp
+++ Surface_mesh/examples/Surface_mesh/draw_surface_mesh.cpp
@@ -10,9 +10,7 @@ typedef CGAL::Surface_mesh<Point>                            Mesh;
 int main(int argc, char* argv[])
 {
   Mesh sm1;
-  // std::ifstream in1((argc>1)?argv[1]:"data/elephant.off");
-  // std::ifstream in1((argc>1)?argv[1]:"D:/project/gsoc2020-basic-viewer-jyang/Polyhedron/demo/Polyhedron/data/anchor.off");
-  std::ifstream in1((argc>1)?argv[1]:"D:/project/gsoc2020-basic-viewer-jyang/Polyhedron/demo/Polyhedron/data/bones.off");
+  std::ifstream in1((argc>1)?argv[1]:"data/elephant.off");
   in1 >> sm1;
 
   CGAL::draw(sm1);
