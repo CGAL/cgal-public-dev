501ae7d25250d23f22cec1d863f2f780d54f724f
Ayush Saraswat <ayushsara12@gmail.com>
2020-07-06 17:05:44 +0530

    update hit object in intersection function and minor fixes.

--- AABB_tree/benchmark/AABB_tree/UserGeomSM.cpp
+++ AABB_tree/benchmark/AABB_tree/UserGeomSM.cpp
@@ -2,7 +2,6 @@
 #include <limits>
 #include <fstream>
 
-
 #include <embree3/rtcore.h> 
 
 #include <CGAL/Simple_cartesian.h>
@@ -49,7 +48,6 @@ void visualisation(const RTCRayHit& rayhit,std::ofstream& output){
         float outX = rayhit.ray.org_x + factor * rayhit.ray.dir_x;
         float outY = rayhit.ray.org_y + factor * rayhit.ray.dir_y;
         float outZ = rayhit.ray.org_z + factor * rayhit.ray.dir_z;
-        // std::cout<<rayhit.hit.primID<<std::endl;
         output<<outX<<" "<<outY<<" "<<outZ<<std::endl; 
     }
 }
@@ -59,7 +57,7 @@ void SmBoundFunction(const struct RTCBoundsFunctionArguments* args){
     RTCBounds* bounds_o = args->bounds_o;
     unsigned int primID = args->primID;
 
-    std::vector<Point> FacePoints(3);
+    std::vector<Point> FacePoints;
 
     Mesh::Face_index fd(primID); 
     Mesh::Halfedge_index hf = sm->surfaceMesh.halfedge(fd);
@@ -68,7 +66,6 @@ void SmBoundFunction(const struct RTCBoundsFunctionArguments* args){
         Point data = sm->surfaceMesh.point(vi);
         FacePoints.push_back(data);
     }
-
     bounds_o->lower_x = std::min({FacePoints[0].x(), FacePoints[1].x(), FacePoints[2].x()});
     bounds_o->lower_y = std::min({FacePoints[0].y(), FacePoints[1].y(), FacePoints[2].y()});
     bounds_o->lower_z = std::min({FacePoints[0].z(), FacePoints[1].z(), FacePoints[2].z()});
@@ -78,13 +75,17 @@ void SmBoundFunction(const struct RTCBoundsFunctionArguments* args){
 }
 
 void SmIntersectionFunction(const RTCIntersectFunctionNArguments* args){
+    
     int* valid = args->valid;
     SM* sm = (SM*) args->geometryUserPtr;
     struct RTCRayHit* rayhit = (RTCRayHit*)args->rayhit;
     unsigned int primID = args->primID;
 
     assert(args->N == 1);
-    std::vector<Point> FacePoints(3);
+    
+    std::vector<Point> FacePoints;
+    
+    if (!valid[0]) return;
 
     Mesh::Face_index fd(primID); 
     Mesh::Halfedge_index hf = sm->surfaceMesh.halfedge(fd);
@@ -96,17 +97,18 @@ void SmIntersectionFunction(const RTCIntersectFunctionNArguments* args){
     Triangle face(FacePoints[0], FacePoints[1], FacePoints[2]);
 
     Vector rayDirection(rayhit->ray.dir_x, rayhit->ray.dir_y, rayhit->ray.dir_z);
-    Point rayOrgin(rayhit->ray.org_x, rayhit->ray.org_z, rayhit->ray.org_z);
+    Point rayOrgin(rayhit->ray.org_x, rayhit->ray.org_y, rayhit->ray.org_z);
     Ray ray(rayOrgin, rayDirection);
 
     auto v = CGAL::intersection(ray, face);
-    if(v) {
+    if(v){
+        rayhit->hit.geomID = sm->geomID;
+        rayhit->hit.primID = primID;
         if (const Point *intersectionPoint = boost::get<Point>(&*v) ){
             float _distance = sqrt(CGAL::squared_distance(rayOrgin, *intersectionPoint));
             rayhit->ray.tfar = _distance;
         }
     }
-
 }
 
 int main(int argc, char  *argv[])
@@ -124,7 +126,7 @@ int main(int argc, char  *argv[])
             visual = true;
     }
     if(argc == 1 || help){
-        std::cerr << "Usage: " << argv[0] << " <infile> <NumberOfRays> <XPoint> <YPoint> <ZPoint> <-o>[if the input file is .off]"<< std::endl;
+        std::cerr << "Usage: " << argv[0] << " <infile> <NumberOfRays> <XPoint> <YPoint> <ZPoint> <-o>[if the input file is .off]  <-v>[for visualisation]"<< std::endl;
         return 0;
     }
     else if(argc<5){
@@ -168,8 +170,8 @@ int main(int argc, char  *argv[])
     sm.geometry = geom;
     sm.geomID = rtcAttachGeometry(scene, geom);
 
-    rtcSetGeometryUserData(geom, &sm);
     rtcSetGeometryUserPrimitiveCount(geom, surfaceMesh.number_of_faces());
+    rtcSetGeometryUserData(geom, &sm);
     rtcSetGeometryBoundsFunction(geom, SmBoundFunction, nullptr);
     rtcSetGeometryIntersectFunction(geom, SmIntersectionFunction);
     rtcCommitGeometry(geom);
@@ -177,14 +179,14 @@ int main(int argc, char  *argv[])
 
     rtcCommitScene(scene);
 
-    struct RTCIntersectContext context;
-    rtcInitIntersectContext(&context);
-    
     int numberOfRays = _numberOfRays; /*NUMBER OF RAY QUERIES*/
     RaysGenerate rg(numberOfRays);
 
+    struct RTCIntersectContext context;
+    rtcInitIntersectContext(&context);
+
     for(size_t n=0; n!=numberOfRays; ++n){
-    
+        
         struct RTCRayHit rayhit;
         rayhit.ray.org_x =  _xPoint; /*POINT.X*/ 
         rayhit.ray.org_y =  _yPoint; /*POINT.Y*/
