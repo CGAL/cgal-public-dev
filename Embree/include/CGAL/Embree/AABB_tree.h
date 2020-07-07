14b82d5d743ed0e24d305459f44bf470a87f9f4c
Andreas Fabri <andreas.fabri@geometryfactory.com>
2020-07-07 11:28:06 +0100

    Add an Embree package

---
+++ Embree/include/CGAL/Embree/AABB_tree.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2020 GeometryFactory (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s)     : Andreas Fabri, Ayush Saraswat
+
+
+#ifndef CGAL_EMBREE_AABB_TREE_H
+#define CGAL_EMBREE_AABB_TREE_H
+
+#include <CGAL/license/Embree.h>
+
+namespace CGAL {
+namespae Embree {
+/**
+ * \ingroup PkgEmbreeRef
+ * This class...
+ */
+
+template <typename TriangleMesh, typename GeomTraits>
+class AABB_tree {
+
+};
+
+} // namespace Embree
+} // namespace CGAL
+#endif // CGAL_EMBREE_AABB_TREE_H
