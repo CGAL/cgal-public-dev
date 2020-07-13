7a93e434ee5616c925a93247661f81e242eeff9d
Ayush Saraswat <ayushsara12@gmail.com>
2020-07-13 11:39:22 +0530

    comments added..

--- Embree/test/Embree/TestEmbreeAABB.cpp
+++ Embree/test/Embree/TestEmbreeAABB.cpp
@@ -14,19 +14,6 @@ typedef CGAL::Surface_mesh<Point> Mesh;
 typedef CGAL::Embree::Triangle_mesh_geometry<Mesh, K> TriangleMesh;
 typedef CGAL::Embree::AABB_tree<TriangleMesh, K> Tree;
 
-// void visualisation(const boost::optional<TriangleMesh::Primitive_id>& intersection, std::ofstream& output){
-//     if(intersection){
-//         Mesh::face_index fd = intersection->first;
-//         Mesh* surfaceMesh = intersection->second;
-//         Mesh::Halfedge_index hf = surfaceMesh->halfedge(fd);
-//         for(Mesh::Halfedge_index hi : halfedges_around_face(hf, *surfaceMesh)){
-//             Mesh::Vertex_index vi = target(hi, *surfaceMesh);
-//             Point data = surfaceMesh->point(vi);
-//             FacePoints.push_back(data);
-//         }
-//     }
-// }
-
 int main(int argc, char const *argv[])
 {
     bool help = false;
@@ -41,19 +28,23 @@ int main(int argc, char const *argv[])
     std::ifstream input(filename);
 
     Mesh surfaceMesh;
-        if(offFile)
+    if(offFile)
         input >> surfaceMesh;
-    else
+    else    /*Support for both Ply and off files. */ 
         CGAL::read_ply(input, surfaceMesh);
+    
+    /*Initialise the tree. The tree constructor automatically creates a single scene and attaches it to the device.*/
     Tree tree;
     tree.insert(surfaceMesh);
 
+    // Configure the ray.
     Point rayOrigin(0.1f, 0.1f, -1.0f);
-    Vector rayDirection(0.0f, 0.0f, 1.0f);
+    Vector rayDirection(0.0f, 0.0f, 1.0f); /*Direction need not be normalized.*/
     Ray ray(rayOrigin, rayDirection);
 
     boost::optional<Tree::Intersection_and_primitive_id> intersection = tree.first_intersection(ray);
 
+    // Below snippet explains how to recover the point from the Intersection_and_primitive_id return value.
     if(intersection){
         Point p = intersection->first;
         std::cout<<"Point of intersection : "<<p<<std::endl;
