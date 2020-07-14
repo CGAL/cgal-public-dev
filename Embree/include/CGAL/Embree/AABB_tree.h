8222db0880f2e9d5f1a7e6cc2d479e2e8b6121b8
Ayush Saraswat <ayushsara12@gmail.com>
2020-07-14 16:40:37 +0530

    Id2descriptor added.

--- Embree/include/CGAL/Embree/AABB_tree.h
+++ Embree/include/CGAL/Embree/AABB_tree.h
@@ -30,9 +30,51 @@ namespace CGAL {
 namespace Embree {
 
 
+template <typename TriangleMesh, bool b>
+struct Id2descriptor
+{};
+ 
+template <typename TriangleMesh>
+struct Id2descriptor<TriangleMesh,true>
+{
+  typedef typename boost::graph_traits<TriangleMesh>::face_descriptor face_descriptor;
+  std::vector<face_descriptor> faceDescriptors;
+
+  Id2descriptor(){}
+  Id2descriptor(const TriangleMesh& tm)
+  {
+    for(face_descriptor fd : faces(tm))
+    {
+      faceDescriptors.push_back(fd);
+    }
+  }
+ 
+  face_descriptor operator()(unsigned int i) const
+  {
+     return faceDescriptors[i];
+  }
+};
+ 
+template <typename TriangleMesh>
+struct Id2descriptor<TriangleMesh,false>
+{
+  typedef typename boost::graph_traits<TriangleMesh>::face_descriptor face_descriptor;
+  
+  Id2descriptor(){}
+  Id2descriptor(const TriangleMesh& tm)
+  {}
+ 
+ 
+  face_descriptor operator()(unsigned int i) const
+  {
+     return face_descriptor(i);
+  }
+};
+
+
   // AF: This is what you had called SM
 
-template <typename TriangleMesh, typename GeomTraits>
+template <typename TriangleMesh, typename GeomTraits, bool b>
 struct Triangle_mesh_geometry {
 
   typedef typename boost::graph_traits<TriangleMesh>::face_descriptor face_descriptor;
@@ -52,12 +94,17 @@ struct Triangle_mesh_geometry {
   unsigned int rtc_geomID;
   Vertex_point_map vpm;
 
+  Id2descriptor<TriangleMesh, b> id2desc;
+
   Triangle_mesh_geometry()
   {}
 
   Triangle_mesh_geometry(const TriangleMesh& tm)
     : surfaceMesh(&tm), vpm(get(CGAL::vertex_point, tm))
-  {}
+  {
+    Id2descriptor<TriangleMesh, b> _id2desc(tm);
+    id2desc = _id2desc;
+  }
 
   static void bound_function(const struct RTCBoundsFunctionArguments* args)
   {
@@ -66,8 +113,7 @@ struct Triangle_mesh_geometry {
     unsigned int primID = args->primID;
 
     std::vector<Point> FacePoints;
-
-    face_descriptor fd(primID);
+    face_descriptor fd = self->id2desc(primID);
     halfedge_descriptor hf = halfedge(fd, *self->surfaceMesh);
     for(halfedge_descriptor hi : halfedges_around_face(hf, *(self->surfaceMesh))){
         vertex_descriptor vi = target(hi, *(self->surfaceMesh));
@@ -94,8 +140,8 @@ struct Triangle_mesh_geometry {
     std::vector<Point> FacePoints;
     if (!valid[0]) return;
 
-    face_descriptor fd(primID);
-
+    // face_descriptor fd(primID);
+    face_descriptor fd = self->id2desc(primID);
     halfedge_descriptor hf = halfedge(fd, *self->surfaceMesh);
     for(halfedge_descriptor hi : halfedges_around_face(hf, *(self->surfaceMesh))){
         vertex_descriptor vi = target(hi, *(self->surfaceMesh));
@@ -139,131 +185,9 @@ struct Triangle_mesh_geometry {
 
   Primitive_id primitive_id(unsigned int primID) const
   {
-    return std::make_pair(face_descriptor(primID), const_cast<TriangleMesh*>(surfaceMesh));
+    return std::make_pair(id2desc(primID), const_cast<TriangleMesh*>(surfaceMesh));
   }
 };
-
-
-// struct for polyhedron.
-template <typename TriangleMesh, typename GeomTraits>
-struct Triangle_mesh_geometry_poly {
-
-  typedef typename boost::graph_traits<TriangleMesh>::face_descriptor face_descriptor;
-  typedef typename boost::graph_traits<TriangleMesh>::halfedge_descriptor halfedge_descriptor;
-  typedef typename boost::graph_traits<TriangleMesh>::vertex_descriptor vertex_descriptor;
-
-  typedef typename boost::property_map< TriangleMesh, vertex_point_t>::type Vertex_point_map;
-
-  typedef std::pair<face_descriptor, TriangleMesh*> Primitive_id;
-  typedef typename GeomTraits::Point_3 Point;
-  typedef typename GeomTraits::Triangle_3 Triangle;
-  typedef typename GeomTraits::Ray_3 Ray;
-  typedef typename GeomTraits::Vector_3 Vector;
-
-  const TriangleMesh* Polyhedron;
-  RTCGeometry rtc_geometry;
-  unsigned int rtc_geomID;
-  Vertex_point_map vpm;
-  // added a vector for faceDescriptors.
-  std::vector<face_descriptor> faceDescriptors;
-
-  Triangle_mesh_geometry_poly()
-  {}
-
-  Triangle_mesh_geometry_poly(const TriangleMesh& tm)
-    : Polyhedron(&tm), vpm(get(CGAL::vertex_point, tm))
-  {
-    for (typename TriangleMesh::Facet_iterator f = tm.facets_begin(); f != tm.facets_end(); ++f){
-      faceDescriptors.push_back(*f);
-    }    
-  }
-
-  static void bound_function(const struct RTCBoundsFunctionArguments* args)
-  {
-    Triangle_mesh_geometry_poly* self = (Triangle_mesh_geometry_poly*) args->geometryUserPtr;
-    RTCBounds* bounds_o = args->bounds_o;
-    unsigned int primID = args->primID;
-
-    std::vector<Point> FacePoints;
-
-    // face_descriptor fd(primID);
-    face_descriptor fd = self->faceDescriptors[primID];
-    halfedge_descriptor hf = halfedge(fd, *self->Polyhedron);
-    for(halfedge_descriptor hi : halfedges_around_face(hf, *(self->Polyhedron))){
-        vertex_descriptor vi = target(hi, *(self->Polyhedron));
-        Point data = get(self->vpm,vi);
-        FacePoints.push_back(data);
-    }
-    bounds_o->lower_x = std::min({FacePoints[0].x(), FacePoints[1].x(), FacePoints[2].x()});
-    bounds_o->lower_y = std::min({FacePoints[0].y(), FacePoints[1].y(), FacePoints[2].y()});
-    bounds_o->lower_z = std::min({FacePoints[0].z(), FacePoints[1].z(), FacePoints[2].z()});
-    bounds_o->upper_x = std::max({FacePoints[0].x(), FacePoints[1].x(), FacePoints[2].x()});
-    bounds_o->upper_y = std::max({FacePoints[0].y(), FacePoints[1].y(), FacePoints[2].y()});
-    bounds_o->upper_z = std::max({FacePoints[0].z(), FacePoints[1].z(), FacePoints[2].z()});
-
-  }
-
-  static void intersection_function(const RTCIntersectFunctionNArguments* args)
-  {
-    Triangle_mesh_geometry_poly* self = (Triangle_mesh_geometry_poly*) args->geometryUserPtr;
-    int* valid = args->valid;
-    struct RTCRayHit* rayhit = (RTCRayHit*)args->rayhit;
-    unsigned int primID = args->primID;
-
-    assert(args->N == 1);
-    std::vector<Point> FacePoints;
-    if (!valid[0]) return;
-
-    // face_descriptor fd(primID);
-    face_descriptor fd = self->faceDescriptors[primID];
-    halfedge_descriptor hf = halfedge(fd, *self->Polyhedron);
-    for(halfedge_descriptor hi : halfedges_around_face(hf, *(self->Polyhedron))){
-        vertex_descriptor vi = target(hi, *(self->Polyhedron));
-        Point data = get(self->vpm,vi);
-        FacePoints.push_back(data);
-    }
-    Triangle face(FacePoints[0], FacePoints[1], FacePoints[2]);
-
-    Vector rayDirection(rayhit->ray.dir_x, rayhit->ray.dir_y, rayhit->ray.dir_z);
-    Point rayOrgin(rayhit->ray.org_x, rayhit->ray.org_y, rayhit->ray.org_z);
-    Ray ray(rayOrgin, rayDirection);
-
-    auto v = CGAL::intersection(ray, face);
-    if(v){
-        rayhit->hit.geomID = self->rtc_geomID;
-        rayhit->hit.primID = primID;
-        if (const Point *intersectionPoint = boost::get<Point>(&*v) ){
-            float _distance = sqrt(CGAL::squared_distance(rayOrgin, *intersectionPoint));
-            rayhit->ray.tfar = _distance;
-        }
-    }
-  }
-
-  void insert_primitives()
-  {
-    rtcSetGeometryUserPrimitiveCount(rtc_geometry, num_faces(*Polyhedron));
-    rtcSetGeometryUserData(rtc_geometry, this);
-
-    // AF: For the next two you have to find out how to write
-    // the function pointer for a static member function
-
-    // Ayush: for a static member function, we can directly pass a pointer, so the below should work fine.
-    // https://isocpp.org/wiki/faq/pointers-to-members
-
-    rtcSetGeometryBoundsFunction(rtc_geometry, bound_function, nullptr);
-    rtcSetGeometryIntersectFunction(rtc_geometry, intersection_function);
-    rtcCommitGeometry(rtc_geometry);
-
-    rtcReleaseGeometry(rtc_geometry);
-  }
-
-  Primitive_id primitive_id(unsigned int primID) const
-  {
-    return std::make_pair(face_descriptor(primID), const_cast<TriangleMesh*>(Polyhedron));
-  }
-};
-
-
 
 /**
  * \ingroup PkgEmbreeRef
@@ -283,25 +207,9 @@ public:
   RTCDevice device;
   RTCScene scene;
 
-  // AF: As the ray intersection returns a geomID we need a mapping
   std::unordered_map<unsigned int, Geometry> id2geometry;
   std::list<Geometry> geometries;
 
-  // void errorFunction(void* userPtr, enum RTCError error, const char* str)
-  // {
-  //   std::cout<<"error "<<error<<": "<<str<<std::endl;
-  // }
-
-  // RTCDevice initializeDevice()
-  // {
-  //   RTCDevice device = rtcNewDevice(NULL);
-
-  //   if (!device)
-  //     printf("error %d: cannot create device\n", rtcGetDeviceError(NULL));
-
-  //   rtcSetDeviceErrorFunction(device, errorFunction, NULL);
-  //   return device;
-  // }
 public:
   AABB_tree()
   {
