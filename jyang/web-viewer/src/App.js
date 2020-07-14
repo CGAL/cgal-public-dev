23f76ba54d7fb64171fba94c9c602fef8255ea19
Jing Yang <jingyang.carl.work@gmail.com>
2020-07-14 15:41:16 -0700

    Initialize three widget

--- jyang/web-viewer/src/App.js
+++ jyang/web-viewer/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as THREE from "three";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+class App extends Component {
+  componentDidMount() {
+    this.init();
+  }
+
+  init() {
+    // Create the scene
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 2;
+
+    // Create renderer
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById("widget").appendChild(renderer.domElement);
+
+    // Create geometry
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // Create render
+    var render = function () {
+      renderer.render(scene, camera);
+    }
+    render();
+
+    // Create resizer
+    window.addEventListener('resize', onWindowResize, false);
+    var onWindowResize = function () {
+      console.log("RESIZE");
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      render();
+    }
+
+    // Create OrbitControls
+    var controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render); //use if there is no animation loop
+    controls.minDistance = 2;
+    controls.maxDistance = 10;
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="widget"></div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
