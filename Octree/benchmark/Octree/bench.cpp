e456d67c431e261eb93a3ed0430a2c43be6c12ad
Jackson Campolattaro <jackcamp@vt.edu>
2020-07-17 15:52:10 -0400

    Add search benchmarking

--- Octree/benchmark/Octree/bench.cpp
+++ Octree/benchmark/Octree/bench.cpp
@@ -12,6 +12,7 @@
 #include <string>
 #include <chrono>
 #include <ctime>
+#include <cstdlib>
 #include <string>
 
 typedef CGAL::Simple_cartesian<double> Kernel;
@@ -25,10 +26,11 @@ typedef CGAL::Octree::Octree
 #define BUCKET_SIZE 20
 #define MAX_DEPTH 10
 #define NUM_RUNS 40
+#define NUM_NEAREST_SEARCHES 400
 
-std::chrono::duration<double> benchmark(Point_set points) {
+std::chrono::duration<double> benchmark_refine(Point_set points) {
 
-  std::cout << "Benchmarking" << std::endl;
+  std::cout << "Benchmarking refinement" << std::endl;
 
   std::chrono::duration<double> elapsed(0);
 
@@ -47,6 +49,38 @@ std::chrono::duration<double> benchmark(Point_set points) {
     std::cout << "." << std::flush;
 
     elapsed += (end - start) / NUM_RUNS;
+  }
+
+
+  std::cout << std::endl << "Averaged "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() << " ms" << std::endl;
+  return elapsed;
+}
+
+std::chrono::duration<double> benchmark_nearest_neighbour(Point_set points) {
+
+  std::cout << "Benchmarking search" << std::endl;
+
+  auto point_map = points.point_map();
+  Octree octree(points, point_map);
+  octree.refine(MAX_DEPTH, BUCKET_SIZE);
+
+  std::chrono::duration<double> elapsed(0);
+
+  for (int i = 0; i < NUM_NEAREST_SEARCHES; ++i) {
+
+    auto search_point = point_map[std::rand() % points.number_of_points()];
+    std::vector<Point> octree_nearest_neighbours;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    octree.nearest_k_neighbours(search_point, 16, std::back_inserter(octree_nearest_neighbours));
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "." << std::flush;
+
+    elapsed += (end - start) / NUM_NEAREST_SEARCHES;
   }
 
 
@@ -84,13 +118,16 @@ int main(void) {
   file << "Dataset Source: " << DATA_FILE_PATH << std::endl;
   file << "Point Count: " << points.number_of_points() << std::endl;
   file << "Number of Runs: " << NUM_RUNS << std::endl;
+  file << "Number of Searches: " << NUM_NEAREST_SEARCHES << std::endl;
   file << std::endl;
 
   // Show the results
   file << "Results" << std::endl;
   file << "~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
   file << "Tree construction time: "
-       << std::chrono::duration_cast<std::chrono::microseconds>(benchmark(points)).count() << " us" << std::endl;
+       << std::chrono::duration_cast<std::chrono::microseconds>(benchmark_refine(points)).count() << " us" << std::endl;
+  file << "Nearest K neighbours search time: "
+       << std::chrono::duration_cast<std::chrono::microseconds>(benchmark_nearest_neighbour(points)).count() << " us" << std::endl;
   file << std::endl;
 
   // Leave room for notes
