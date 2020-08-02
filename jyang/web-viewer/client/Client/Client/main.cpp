8c3e5928cedfd3a18b7318c1bcc1387e27d0b642
Jing Yang <jingyang.carl.work@gmail.com>
2020-08-01 19:28:47 -0700

    the client app is able to connet the server.

--- jyang/web-viewer/client/Client/Client/main.cpp
+++ jyang/web-viewer/client/Client/Client/main.cpp
@@ -12,14 +12,16 @@ int main(int argc, char *argv[])
     socket->connectToHost("127.0.0.1", 3001);
     if (!socket->waitForConnected()) {
         qDebug() << "Connection failed";
+        return -1;
     }
+    qDebug("Connected!");
+    //socket->waitForReadyRead();
 
     // disconnect to the server
     socket->disconnectFromHost();
-    if (!socket->waitForDisconnected()) {
-        qDebug() << "Disconnection failed";
+    if (socket->state() == QAbstractSocket::UnconnectedState || socket->waitForDisconnected(1000)) {
+        qDebug() << "Disconnected!";
     }
-    
 
     return a.exec();
 }
--- jyang/web-viewer/server/backend/routes/index.js
+++ jyang/web-viewer/server/backend/routes/index.js
@@ -2,16 +2,20 @@ var express = require('express');
 const app = require('../app');
 var router = express.Router();
 
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+// var http = require('http').createServer(app);
+// var io = require('socket.io')(http);
+var net = require('net');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// })
+net.createServer(function(connection) {
+  console.log('client connected');
 })
 
 module.exports = router;
