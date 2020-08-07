c15d0d7122cfd7f8e9d64165deebb7c09a586cb4
Jing Yang <jingyang.carl.work@gmail.com>
2020-08-06 19:52:02 -0700

    the client is able to send message and the server is able to receive the message

--- jyang/web-viewer/client/Client/Client/main.cpp
+++ jyang/web-viewer/client/Client/Client/main.cpp
@@ -1,5 +1,21 @@
 #include <QtCore/QCoreApplication>
 #include <QtNetwork/qtcpsocket.h>
+#include <qdatastream.h>
+
+/*
+Description:
+
+Input:
+@ qint32 source: use qint32 to ensure that the number has 4 bytes
+Output:
+
+*/
+QByteArray Int2Array(qint32 source) {
+    QByteArray data_array;
+    QDataStream data(&data_array, QIODevice::ReadWrite);
+    data << source;
+    return data_array;
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,7 +31,17 @@ int main(int argc, char *argv[])
         return -1;
     }
     qDebug("Connected!");
-    //socket->waitForReadyRead();
+
+    // send a piece of data
+    QByteArray data("Hello World");
+    if (socket->state() == QAbstractSocket::ConnectedState) {
+        socket->write(Int2Array(data.size()));
+        socket->write(data);
+        // send data
+        if (socket->waitForBytesWritten()) {
+            qDebug() << "Sending" << data.size() << "bytes data:" << data << "Complete!";
+        }
+    }
 
     // disconnect to the server
     socket->disconnectFromHost();
--- jyang/web-viewer/server/backend/routes/socket.js
+++ jyang/web-viewer/server/backend/routes/socket.js
@@ -1,9 +1,24 @@
 var net = require('net');
 
-var server = net.createServer(function(connection) {
-    console.log('client connected');
+var server = net.createServer(function(socket) {
+    console.log('client connected from', socket.remoteAddress, ':', socket.remotePort);
+    
+    // set data encoding
+    socket.setEncoding('utf-8');
+
+    // add 'data' event handler to this socket instance
+    socket.on('data', (data) => {
+      console.log(socket.bytesRead, 'bytes', typeof data, 'data :', data.toString('utf-8'));
+    });
+
+    socket.end('goodbye');
+  }).on('error', (err) => {
+    // handle errors here.
+    throw err;
   });
 
-server.listen(3001, '127.0.0.1');
+server.listen(3001, '127.0.0.1', ()=> {
+  console.log('running server on', server.address());
+});
 
 module.exports = server;
