13bb81c365831bba6caacfe29bc3a83df23832d0
Ayush Saraswat <ayushsara12@gmail.com>
2020-08-25 13:42:25 +0530

    updated distance query benchmark.

--- Embree/benchmark/Embree/closest_point_compare.cpp
+++ Embree/benchmark/Embree/closest_point_compare.cpp
@@ -11,6 +11,8 @@
 
 #include <CGAL/Real_timer.h>
 
+#include "../../../AABB_tree/benchmark/AABB_tree/include/RaysGenerate.h"
+
 typedef CGAL::Simple_cartesian<float> K;
 typedef K::Point_3 Point;
 typedef K::Ray_3 Ray;
@@ -31,17 +33,28 @@ int main(int argc, char const *argv[])
   Mesh triangle_mesh;
   input >> triangle_mesh;
 
+  int num_points = 10000; 
+  RaysGenerate rg(num_points);
+
   TreeEmbree tree1;
   tree1.insert(triangle_mesh);
+
+  Point centre(tree1.bbox().min(0)+tree1.bbox().max(0)/2, 
+               tree1.bbox().min(1)+tree1.bbox().max(1)/2,
+               tree1.bbox().min(2)+tree1.bbox().max(2)/2); 
 
   TreeCgal tree2(faces(triangle_mesh).first, faces(triangle_mesh).second, triangle_mesh);
   tree2.build();
 
-  Point origin(0.1f,0.1f,-1.0f);
   {
     CGAL::Real_timer time;
     time.start();
-    auto p1 = tree1.closest_point(origin);
+    for (int i=0; i<num_points;i++){
+      Point p (centre.x()+500*rg.normalisedRayDirections[i]._x, 
+               centre.y()+500*rg.normalisedRayDirections[i]._y,
+               centre.z()+500*rg.normalisedRayDirections[i]._z);
+      auto p1 = tree1.closest_point(p);
+    }
     time.stop();
     std::cout<<"Embree Closest_point : "<<time.time()<<std::endl;
     time.reset();
@@ -50,7 +63,12 @@ int main(int argc, char const *argv[])
   {
     CGAL::Real_timer time;
     time.start();
-    auto p2 = tree2.closest_point(origin);  
+        for (int i=0; i<num_points;i++){
+      Point p (centre.x()+500*rg.normalisedRayDirections[i]._x, 
+               centre.y()+500*rg.normalisedRayDirections[i]._y,
+               centre.z()+500*rg.normalisedRayDirections[i]._z);
+      auto p2 = tree2.closest_point(p); 
+    } 
     time.stop();
     std::cout<<"CGAL Closest_point : "<<time.time()<<std::endl;
     time.reset();
