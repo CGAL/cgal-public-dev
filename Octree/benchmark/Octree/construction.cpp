f570b32fd259bf24d9510f42e6b729df338bb611
Jackson Campolattaro <jackcamp@vt.edu>
2020-08-28 11:31:26 -0400

    Create csv files in both benchmarks

--- Octree/benchmark/Octree/construction.cpp
+++ Octree/benchmark/Octree/construction.cpp
@@ -1,5 +1,26 @@
 
-int main() {
+#define CGAL_TRACE_STREAM std::cerr
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv) {
+
+  // Set output file
+  std::ofstream file;
+  file.open((argc > 1) ? argv[1] : "construction_benchmark.csv");
+
+  file << "Number of Points,Build Time (ms) \n";
+
+  for (size_t num_points = 10; num_points < 10000; ++num_points) {
+
+    std::cout << num_points << std::endl;
+
+    file << num_points << ",";
+    file << 5 << "\n";
+  }
+
+  file.close();
 
   return 0;
 }
--- Octree/benchmark/Octree/nearest_neighbor.cpp
+++ Octree/benchmark/Octree/nearest_neighbor.cpp
@@ -1,5 +1,26 @@
 
-int main() {
+#define CGAL_TRACE_STREAM std::cerr
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv) {
+
+  // Set output file
+  std::ofstream file;
+  file.open((argc > 1) ? argv[1] : "nearest_neighbor_benchmark.csv");
+
+  file << "Number of Points,Search Time (ms) \n";
+
+  for (size_t num_points = 10; num_points < 10000; ++num_points) {
+
+    std::cout << num_points << std::endl;
+
+    file << num_points << ",";
+    file << 5 << "\n";
+  }
+
+  file.close();
 
   return 0;
 }
