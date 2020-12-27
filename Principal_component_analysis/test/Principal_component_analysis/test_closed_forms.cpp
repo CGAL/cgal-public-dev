37b6b233b9373d2c6b35fcdf69fac21181ee6c08
Pierre Alliez <pierre.alliez@inria.fr>
2020-12-27 17:22:50 +0100

    PCA tests: closed form formulae for 2D iso rectangles

--- Principal_component_analysis/test/Principal_component_analysis/test_closed_forms.cpp
+++ Principal_component_analysis/test/Principal_component_analysis/test_closed_forms.cpp
@@ -18,21 +18,36 @@ typedef Kernel::Segment_2 Segment_2;
 typedef Kernel::Iso_rectangle_2 Iso_rectangle_2;
 typedef CGAL::Default_diagonalize_traits<typename Kernel::FT, 2> Diagonalize_traits_2;
 
+void test_iso_rectangle_2()
+{
+	Point_2 origin_2(0.0, 0.0);
+
+	std::list<Iso_rectangle_2> Iso_rectangles;
+	Iso_rectangles.push_back(Iso_rectangle_2(origin_2, Point_2(1.0, 1.0)));
+
+	Diagonalize_traits_2::Covariance_matrix moment =
+		CGAL::order_2_moment_2(Iso_rectangles.begin(), Iso_rectangles.end(), origin_2,
+			Kernel(), CGAL::Dimension_tag<2>(), Diagonalize_traits_2());
+
+	std::cerr << "Iso rectangle: ";
+	std::cerr << moment[0] << " " << moment[1] << " " << moment[2];
+
+	// reference = 1/3, 1/4, 1/3
+	if(moment[0] != 1.0 / 3.0 || 
+	   moment[1] != 0.25 ||
+	   moment[2] != 1.0 / 3.0)
+	{
+		std::cerr << " (failure)" << std::endl;
+		std::exit(1); // failure
+	}
+	else
+		std::cerr << " (success)" << std::endl;
+}
+
 int main()
 {
-  std::cerr << "Closed form test in 2D" << std::endl;
-
-  Point_2 origin_2(0.0, 0.0);
-
-  std::list<Iso_rectangle_2> Iso_rectangles;
-  Iso_rectangles.push_back(Iso_rectangle_2(origin_2, Point_2(1.0, 1.0)));
-
-  Diagonalize_traits_2::Covariance_matrix moment = 
-  CGAL::order_2_moment_2(Iso_rectangles.begin(), Iso_rectangles.end(), origin_2,
-	  Kernel(), CGAL::Dimension_tag<2>(), Diagonalize_traits_2());
-
-  std::cerr << "Iso rectangle: ";
-  std::cerr << moment[0] << " " << moment[1] << " " << moment[2] << " " << std::endl;
+  std::cerr << "Closed form tests in 2D" << std::endl;
+  test_iso_rectangle_2();
 
   return EXIT_SUCCESS;
 }
