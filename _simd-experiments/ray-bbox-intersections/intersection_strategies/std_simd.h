3f7b4d4a20c8d25b3e658bc4e0af03efaf015847
jackcamp <jackcamp@vt.edu>
2021-06-13 12:14:10 -0400

    Change test topology to group bounding boxes together

--- _simd-experiments/ray-bbox-intersections/intersection_strategies/std_simd.h
+++ _simd-experiments/ray-bbox-intersections/intersection_strategies/std_simd.h
@@ -11,33 +11,39 @@ bool intersect_std_simd(const BBox &bbox, const Ray &ray, float rmin, float rmax
 
   // TODO: This is temporarily identical to the clarified approach
 
-  // Determine bounds for x and y
-  double xmin = (bbox.bounds()[ray.sign()[0]].x() - ray.origin().x()) * ray.inv_direction().x();
-  double xmax = (bbox.bounds()[1 - ray.sign()[0]].x() - ray.origin().x()) * ray.inv_direction().x();
+  std::experimental::fixed_size_simd<double, 3> min;
 
-  double ymin = (bbox.bounds()[ray.sign()[1]].y() - ray.origin().y()) * ray.inv_direction().y();
-  double ymax = (bbox.bounds()[1 - ray.sign()[1]].y() - ray.origin().y()) * ray.inv_direction().y();
+  // Determine intermediate value for minimum bounds
+  float xmin = box.bounds[ray.sign[0]].x;
+  float ymin = box.bounds[ray.sign[1]].y;
+  float zmin = box.bounds[ray.sign[2]].z;
+  /* pad_min = FLOAT_MAX; */
 
-  // If the x and y bounds don't overlap, the ray doesn't intersect with the box
-  if (xmin > ymax || ymin > xmax) return false;
+  // Determine intermediate value for maximum bounds
+  float xmax = box.bounds[1 - ray.sign[0]].x;
+  float ymax = box.bounds[1 - ray.sign[1]].y;
+  float zmax = box.bounds[1 - ray.sign[2]].z;
+  /* pad_max = FLOAT_MIN; */
 
-  // Determine the bounds of the overlapping region
-  double min = std::max(xmin, ymin);
-  double max = std::min(xmax, ymax);
+  // Apply transform to all bounds
+  float xmin = (xmin - r.origin.x) * r.inv_direction.x;
+  float ymin = (ymin - r.origin.y) * r.inv_direction.y;
+  float zmin = (zmin - r.origin.z) * r.inv_direction.z;
+  /* pad_min = (pading - 0.0)      * 1.0; /*
+  float xmax = (zmax - r.origin.x) * r.inv_direction.x;
+  float ymax = (ymax - r.origin.y) * r.inv_direction.y;
+  float zmax = (zmax - r.origin.z) * r.inv_direction.z;
+  /* pad_max = (pading - 0.0)      * 1.0; /*
 
-  // Determine bounds for z
-  double zmin = (bbox.bounds()[ray.sign()[2]].z() - ray.origin().z()) * ray.inv_direction().z();
-  double zmax = (bbox.bounds()[1 - ray.sign()[2]].z() - ray.origin().z()) * ray.inv_direction().z();
+  // Determine the minimum bounds of the region where x, y, and z overlap
+  min = MAX(xmin, ymin, zmin /*, pad_min */);
 
-  // If the z bounds don't overlap with the existing region, the ray doesn't intercept
-  if (min > zmax || zmin > max) return false;
+  // Determine the maximum bounds of the region where x, y, and z overlap
+  max = MIN(xmax, ymax, zmax /*, pad_max */);
 
-  // Update the bounds to find the region where x, y, and z overlap
-  min = std::max(min, zmin);
-  max = std::min(max, zmax);
+  // The ray intercepts if this region exists and overlaps with the bounds provided
+  return (max > min) && (min < rmax) && (max > rmin);
 
-  // The ray intercepts if this region overlaps with the bounds provided
-  return (min < rmax) && (max > rmin);
 }
 
 #endif //RAY_BBOX_INTERSECTIONS_STD_SIMD_H
--- _simd-experiments/ray-bbox-intersections/vector3.h
+++ _simd-experiments/ray-bbox-intersections/vector3.h
@@ -4,19 +4,25 @@
 class Vector3 {
 private:
 
-    double val[3];
+  double val[3];
 
 public:
 
-    Vector3(double x, double y, double z) : val{x, y, z} {}
+  Vector3(double x, double y, double z) : val{x, y, z} {}
 
-    [[nodiscard]] const decltype(val) &arr() const { return val; }
+  [[nodiscard]] const decltype(val) &arr() const { return val; }
 
-    [[nodiscard]] const double &x() const { return val[0]; }
+  [[nodiscard]] const double &x() const { return val[0]; }
 
-    [[nodiscard]] const double &y() const { return val[1]; }
+  [[nodiscard]] const double &y() const { return val[1]; }
 
-    [[nodiscard]] const double &z() const { return val[2]; }
+  [[nodiscard]] const double &z() const { return val[2]; }
+
+  bool operator==(const Vector3 &other) const {
+    return other.x() == this->x() &&
+           other.y() == this->y() &&
+           other.z() == this->z();
+  }
 };
 
 #endif //RAY_BBOX_INTERSECTIONS_VECTOR3_H
