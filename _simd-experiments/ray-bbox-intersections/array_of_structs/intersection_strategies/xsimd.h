996847dfe69480b6e95ad691d263e95dca18ab3c
jackcamp <jackcamp@vt.edu>
2021-06-15 21:54:12 -0400

    Modify XSimd method for compatibility with the new structs

--- _simd-experiments/ray-bbox-intersections/array_of_structs/intersection_strategies/xsimd.h
+++ _simd-experiments/ray-bbox-intersections/array_of_structs/intersection_strategies/xsimd.h
@@ -12,36 +12,36 @@ namespace xsimd {
   template<typename T>
   inline bool intersect(const BBox<T> &bbox, const Ray<T> &ray) {
 
-    // Determine intermediate value for minimum bounds
+    // Determine intermediate value for minimum bounds()
     xsimd::batch<double, 4> min(
-            bbox.bounds()[ray.sign()[0]].x(),
-            bbox.bounds()[ray.sign()[1]].y(),
-            bbox.bounds()[ray.sign()[2]].z(),
+            bbox.bounds()[ray.sign[0]].get().x,
+            bbox.bounds()[ray.sign[1]].get().y,
+            bbox.bounds()[ray.sign[2]].get().z,
             std::numeric_limits<double>::max()
     );
 
-    // Determine intermediate value for maximum bounds
+    // Determine intermediate value for maximum bounds()
     xsimd::batch<double, 4> max(
-            bbox.bounds()[1 - ray.sign()[0]].x(),
-            bbox.bounds()[1 - ray.sign()[1]].y(),
-            bbox.bounds()[1 - ray.sign()[2]].z(),
+            bbox.bounds()[1 - ray.sign[0]].get().x,
+            bbox.bounds()[1 - ray.sign[1]].get().y,
+            bbox.bounds()[1 - ray.sign[2]].get().z,
             std::numeric_limits<double>::min()
     );
 
-    // Apply transform to all bounds
-    xsimd::batch<double, 4> origin(ray.origin().x(), ray.origin().y(), ray.origin().z(), 1);
-    xsimd::batch<double, 4> inv_direction(ray.inv_direction().x(), ray.inv_direction().y(), ray.inv_direction().z(), 1);
+    // Apply transform to all bounds()
+    xsimd::batch<double, 4> origin(ray.origin.x, ray.origin.y, ray.origin.z, 1);
+    xsimd::batch<double, 4> inv_direction(ray.inv_direction.x, ray.inv_direction.y, ray.inv_direction.z, 1);
     min = (min - origin) * inv_direction;
     max = (max - origin) * inv_direction;
 
-    // Determine the minimum bounds of the region where x, y, and z overlap
+    // Determine the minimum bounds() of the region where x, y, and z overlap
     double overall_min = std::max({min[0], min[1], min[2]});
 
-    // Determine the maximum bounds of the region where x, y, and z overlap
+    // Determine the maximum bounds() of the region where x, y, and z overlap
     double overall_max = std::min({max[0], max[1], max[2]});
 
-    // The ray intercepts if this region exists and overlaps with the bounds provided
-    return (overall_max > overall_min);
+    // The ray intercepts if this region exists and overlaps with the bounds() provided
+    return (overall_max >= overall_min);
   }
 
 }
