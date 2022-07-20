fbf12247e85ec839a9b472e9e832c345baabd8c9
Ágoston Sipos <asipos@edu.bme.hu>
2022-07-20 08:54:58 +0200

    Adding license tag

--- Orthtree/include/CGAL/Orthtree/Edge.h
+++ Orthtree/include/CGAL/Orthtree/Edge.h
@@ -1,3 +1,14 @@
+// Copyright (c) 2022  INRIA (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Ágoston Sipos
+
 #ifndef CGAL_ORTHTREE_EDGE_H
 #define CGAL_ORTHTREE_EDGE_H
 
