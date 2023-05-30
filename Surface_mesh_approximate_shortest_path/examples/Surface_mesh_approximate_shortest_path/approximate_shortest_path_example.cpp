fcc81d092f2dc2ba2e3ac6bf161390e6fccc48df
rp01052 <r.piel@surrey.ac.uk>
2023-05-30 08:26:45 +0100

    Initial Commit. Set up folder structure and first headers.

---
+++ Surface_mesh_approximate_shortest_path/examples/Surface_mesh_approximate_shortest_path/approximate_shortest_path_example.cpp
@@ -0,0 +1,12 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Surface_mesh_approximate_shortest_path.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello!" << std::endl;
+
+    return 0;
+}
---
+++ Surface_mesh_approximate_shortest_path/include/CGAL/Surface_mesh_approximate_shortest_path/Surface_mesh_approximate_shortest_path.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 GeometryFactory
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Robert Piel
+
+#ifndef CGAL_SURFACE_MESH_APPROXIMATE_SHORTEST_PATH_SURFACE_MESH_APPROXIMATE_SHORTEST_PATH_H
+#define CGAL_SURFACE_MESH_APPROXIMATE_SHORTEST_PATH_SURFACE_MESH_APPROXIMATE_SHORTEST_PATH_H
+
+
+
+#endif // CGAL_SURFACE_MESH_APPROXIMATE_SHORTEST_PATH_SURFACE_MESH_APPROXIMATE_SHORTEST_PATH_H
