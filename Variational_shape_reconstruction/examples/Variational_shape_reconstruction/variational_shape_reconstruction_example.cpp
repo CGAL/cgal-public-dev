160d5d065fa029ca39ebfaf31ddbf13cb9785deb
PupsDev <Control3d@gmail.com>
2023-06-06 19:57:34 +0200

    Initial commit

---
+++ Variational_shape_reconstruction/examples/Variational_shape_reconstruction/variational_shape_reconstruction_example.cpp
@@ -0,0 +1,12 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Variational_shape_reconstruction.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world !" << std::endl;
+
+    return 0;
+}
---
+++ Variational_shape_reconstruction/include/CGAL/Variational_shape_reconstruction/Variational_shape_reconstruction.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 GeometryFactory
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : 
+
+#ifndef CGAL_VARIATIONAL_SHAPE_RECONSTRUCTION_VARIATIONAL_SHAPE_RECONSTRUCTION_H
+#define CGAL_VARIATIONAL_SHAPE_RECONSTRUCTION_VARIATIONAL_SHAPE_RECONSTRUCTION_H
+
+
+
+#endif // CGAL_VARIATIONAL_SHAPE_RECONSTRUCTION_VARIATIONAL_SHAPE_RECONSTRUCTION_H
