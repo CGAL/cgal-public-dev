a7f8513ba43015bb69cf6c7f867cbade86d065f8
Jeffrey Cochran <jeffrey.david.cochran@gmail.com>
2023-07-05 17:48:03 -0400

    move

--- .sandbox/src/bridson/draw_surface_mesh.cpp
+++ .sandbox/src/bridson/draw_surface_mesh.cpp
@@ -60,8 +60,10 @@
 #include <fstream>
 typedef CGAL::Simple_cartesian<double>                       Kernel;
 typedef Kernel::Point_3                                      Point;
+typedef Kernel::Vector_3                                     Vector;
 typedef Kernel::Tetrahedron_3                                Tetrahedron;
 typedef Kernel::Ray_3                                        Ray;
+typedef Kernel::Aff_transformation_3                         Transform;                      
 typedef CGAL::Surface_mesh<Point>                            Mesh;
 typedef CGAL::BilinearPatchC3<Kernel>                        BilinearPatch;  
 
@@ -82,39 +84,118 @@ int main(int argc, char* argv[])
   // std::cout << "Outside point is outside: " << t.has_on_unbounded_side(outside_point) << std::endl;
   // std::cout << "Should be zero: " << bp.phi(d) << std::endl;
   // std::cout << "Should be false: " << bp.is_planar() << std::endl;
-  Point a = Point(0, 0, 0);
-  Point b = Point(1, 0, 0);
-  Point c = Point(0, 1, 0);
-  Point d = Point(0, 0, 1);
-  Point outside_point = Point(1, 1, 1);
-  Point inside_point = Point(0.25, 0.25, 0.25);
+  // Point a = Point(0, 0, 0);
+  // Point b = Point(1, 0, 0);
+  // Point c = Point(0, 1, 0);
+  // Point d = Point(0, 0, 1);
+  // Point outside_point = Point(1, 1, 1);
+  // Point inside_point = Point(0.25, 0.25, 0.25);
 
-  BilinearPatch bp = BilinearPatch(a, b, c, d);
-  Tetrahedron t = bp.tetrahedron();
-  Ray r = Ray(a, outside_point);
+  // BilinearPatch bp = BilinearPatch(a, b, c, d);
+  // Tetrahedron t = bp.tetrahedron();
+  // Ray r = Ray(a, outside_point);
 
-  std::cout << "Orientation is positive: " << t.orientation() << std::endl;
-  std::cout << "Inside point is inside: " << t.has_on_bounded_side(inside_point) << std::endl;
-  std::cout << "Outside point is outside: " << t.has_on_unbounded_side(outside_point) << std::endl;
-  std::cout << "Should be true: " << bp.aux_phi(d).do_overlap(0) << std::endl;
-  std::cout << "Should be true: " << bp.has_on(d) << std::endl;
-  std::cout << "Should be false: " << bp.has_on(outside_point) << std::endl;
-  std::cout << "Should be false: " << bp.is_planar() << std::endl;
+  // std::cout << "Orientation is positive: " << t.orientation() << std::endl;
+  // std::cout << "Inside point is inside: " << t.has_on_bounded_side(inside_point) << std::endl;
+  // std::cout << "Outside point is outside: " << t.has_on_unbounded_side(outside_point) << std::endl;
+  // std::cout << "Should be true: " << bp.aux_phi(d).do_overlap(0) << std::endl;
+  // std::cout << "Should be true: " << bp.has_on(d) << std::endl;
+  // std::cout << "Should be false: " << bp.has_on(outside_point) << std::endl;
+  // std::cout << "Should be false: " << bp.is_planar() << std::endl;
 
-  bool intersects = CGAL::do_intersect(r, t);
+  const std::string inner_sphere_filename = CGAL::data_file_path("meshes/sphere_internal.off");
+  const std::string outer_sphere_filename = CGAL::data_file_path("meshes/sphere_external.off");
 
-  std::cout << "Should be true: " << intersects << std::endl;
+  Mesh inner_sphere_mesh;
+  Mesh outer_sphere_mesh;
 
-  intersects = CGAL::Intersections::internal::do_intersect_parity<Kernel>(bp, r);
+  if(!CGAL::IO::read_polygon_mesh(inner_sphere_filename, inner_sphere_mesh))
+  {
+    std::cerr << "Invalid input file for internal sphere." << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  std::cout << "Should be true: " << intersects << std::endl;
+  if(!CGAL::IO::read_polygon_mesh(outer_sphere_filename, outer_sphere_mesh))
+  {
+    std::cerr << "Invalid input file." << std::endl;
+    return EXIT_FAILURE;
+  }
+  // std::cout << "Should be true: " << intersects << std::endl;
+  auto vcm = inner_sphere_mesh.add_property_map<Mesh::Vertex_index, CGAL::IO::Color>("v:color").first;
+  auto ecm = inner_sphere_mesh.add_property_map<Mesh::Edge_index, CGAL::IO::Color>("e:color").first;
+  auto fcm = inner_sphere_mesh.add_property_map<Mesh::Face_index>("f:color", CGAL::IO::white() /*default*/).first; 
 
-  // Mesh sm;
-  // if(!CGAL::IO::read_polygon_mesh(filename, sm))
-  // {
-  //   std::cerr << "Invalid input file." << std::endl;
-  //   return EXIT_FAILURE;
-  // }
-  // CGAL::draw(sm);
+  for(auto v : vertices(inner_sphere_mesh))
+  {
+    if(v.idx()%2) 
+    {
+      Point& p = inner_sphere_mesh.point(v);
+      Vector vec = Vector(p.x(), p.y(), p.z());
+      Transform transformation(CGAL::TRANSLATION, vec);
+      std::cout << "X before: " << p.x() << std::endl;g
+      swap(p,p.transform(transformation));
+      std::cout << "X after: " << p.x() << std::endl;
+      put(vcm, v, CGAL::IO::black());
+    }
+    else
+      put(vcm, v, CGAL::IO::blue());
+  }
+
+  for(auto e : edges(inner_sphere_mesh))
+    put(ecm, e, CGAL::IO::gray());
+
+  CGAL_USE(fcm);
+
+
+
+  CGAL::draw_color(inner_sphere_mesh);
+  CGAL::draw_color(outer_sphere_mesh);
+
+  // Case 0: planar patch
+  // Point a = Point(0, 0, 0);
+  // Point b = Point(1, 0, 0);
+  // Point c = Point(0, 1, 0);
+  // Point d = Point(1, 1, 0);
+  // Point outside_point = Point(0.25, 0.25, 1);
+  // Point outside_point_2 = Point(-0.25, -0.25, 1);
+  // Point inside_point = Point(0.25, 0.25, 0);
+
+  // BilinearPatch bp = BilinearPatch(a, b, c, d); 
+
+  // Ray ray_true = Ray(outside_point, inside_point);
+  // Ray ray_false = Ray(outside_point, outside_point_2);
+
+  // bool intersects = CGAL::Intersections::internal::do_intersect_odd_parity(bp, ray_true);
+  // std::cout << "Case 0, True: " << intersects << std::endl;
+
+  // intersects = CGAL::Intersections::internal::do_intersect_odd_parity(bp, ray_false);
+  // std::cout << "Case 0, False: " << intersects << std::endl;
+
+  // // Case 1a: non-planar, ray origin on the bilinear patch
+  // a = Point(0, 0, 0);
+  // b = Point(1, 0, 0);
+  // c = Point(0, 1, 0);
+  // d = Point(0, 0, 1);
+  // outside_point = Point(-0.1, -0.1, -0.1);
+  // outside_point_2 = Point(1, 1, 1);
+  // inside_point = Point(0.333, 0.333, 0.333);
+
+  // bp = BilinearPatch(a, b, c, d); 
+
+  // ray_true = Ray(a, outside_point);
+
+  // intersects = CGAL::Intersections::internal::do_intersect_odd_parity(bp, ray_true);
+  // std::cout << "Case 1a, True: " << intersects << std::endl;
+
+  // // Case 1b: non-planar, ray origin inside bounding tetrahedron, but not on patch
+  // ray_true = Ray(inside_point, outside_point);
+  // ray_false = Ray(inside_point, outside_point_2);
+
+  // intersects = CGAL::Intersections::internal::do_intersect_odd_parity(bp, ray_true);
+  // std::cout << "Case 1b, True: " << intersects << std::endl;
+
+  // intersects = CGAL::Intersections::internal::do_intersect_odd_parity(bp, ray_false);
+  // std::cout << "Case 1b, False: " << intersects << std::endl;
+
   return EXIT_SUCCESS;
 }
--- .sandbox/src/bridson/include/Ray_3_Bilinear_patch_3_do_intersect.h
+++ .sandbox/src/bridson/include/Ray_3_Bilinear_patch_3_do_intersect.h
@@ -12,7 +12,7 @@
 
 #ifndef CGAL_INTERNAL_INTERSECTIONS_RAY_3_BILINEAR_PATCH_3_DO_INTERSECT_H
 #define CGAL_INTERNAL_INTERSECTIONS_RAY_3_BILINEAR_PATCH_3_DO_INTERSECT_H
-
+#include <iostream>
 #include <CGAL/enum.h>
 #include <CGAL/kernel_assertions.h>
 #include <CGAL/intersection_3.h>
@@ -22,182 +22,9 @@ namespace CGAL {
 namespace Intersections {
 namespace internal {
 
-// namespace R3BP3_intersection {
-
-// enum type {
-//   COPLANAR_RAY = 3,
-//   ENDPOINT_IN_TRIANGLE = 4,
-//   CROSS_SEGMENT = 1,
-//   CROSS_VERTEX = 2,
-//   CROSS_FACET = 0
-// };
-
-// } // namespace R3BP3_intersection
-
-// struct r3bp3_do_intersect_empty_visitor
-// {
-//   typedef bool result_type;
-//   result_type result(bool b){ return b; }
-//   void update(Orientation){}
-//   void ray_coplanar(){}
-//   void end_point_in_triangle(){}
-// };
-
-// struct r3bp3_do_intersect_endpoint_position_visitor
-// {
-//   int m_intersection_type;
-
-//   r3t3_do_intersect_endpoint_position_visitor() : m_intersection_type(0) { }
-
-//   typedef std::pair<bool,R3T3_intersection::type> result_type;
-//   result_type result(bool b)
-//   {
-//     CGAL_assertion(m_intersection_type>-1 && m_intersection_type<5);
-//     return std::make_pair(b,enum_cast<R3T3_intersection::type>(m_intersection_type));
-//   }
-
-//   void update(Orientation orient)
-//   {
-//     if (orient==ZERO)
-//       ++m_intersection_type;
-//   }
-
-//   void ray_coplanar(){ m_intersection_type = 3; }
-//   void end_point_in_triangle(){ m_intersection_type = 4; }
-// };
-
-// template <class K,class Visitor>
-// typename Visitor::result_type
-// do_intersect_coplanar(const typename K::Triangle_3& t,
-//                       const typename K::Ray_3& r,
-//                       const K& k,
-//                       Visitor visitor)
-// {
-//   visitor.ray_coplanar();
-//   CGAL_kernel_precondition(!k.is_degenerate_3_object()(t));
-//   CGAL_kernel_precondition(!k.is_degenerate_3_object()(r));
-
-//   typedef typename K::Point_3 Point_3;
-
-//   typename K::Construct_point_on_3 point_on = k.construct_point_on_3_object();
-//   typename K::Construct_vertex_3 vertex_on = k.construct_vertex_3_object();
-//   typename K::Coplanar_orientation_3 coplanar_orientation = k.coplanar_orientation_3_object();
-
-//   const Point_3& p = point_on(r,0);
-//   const Point_3& q = point_on(r,1);
-
-//   const Point_3& A = vertex_on(t,0);
-//   const Point_3& B = vertex_on(t,1);
-//   const Point_3& C = vertex_on(t,2);
-
-//   const Point_3* a = &A;
-//   const Point_3* b = &B;
-//   const Point_3* c = &C;
-
-//   // Determine the orientation of the triangle in the common plane
-//   if (coplanar_orientation(A,B,C) != POSITIVE)
-//     {
-//       // The triangle is not counterclockwise oriented
-//       // swap two vertices.
-//       b = &C;
-//       c = &B;
-//     }
-
-//   // Test whether the ray's supporting line intersects the
-//   // triangle in the common plane
-
-//   const Orientation pqa = coplanar_orientation(p,q,*a);
-//   const Orientation pqb = coplanar_orientation(p,q,*b);
-//   const Orientation pqc = coplanar_orientation(p,q,*c);
-
-//   switch ( pqa ) {
-//   case POSITIVE:
-//     switch ( pqb ) {
-//     case POSITIVE:
-//        if (pqc == POSITIVE)
-//          // the triangle lies in the positive halfspace
-//          // defined by the ray's supporting line.
-//          return visitor.result(false);
-//        // c is isolated on the negative side
-//        return visitor.result(coplanar_orientation(*a,*c,p) != POSITIVE);
-
-//     case NEGATIVE:
-//       if (pqc == POSITIVE) // b is isolated on the negative side
-//         return visitor.result(coplanar_orientation(*c,*b,p) != POSITIVE);
-//       // a is isolated on the positive side
-//       return visitor.result(coplanar_orientation(*a,*c,p) != POSITIVE);
-
-//     case COLLINEAR:
-//       if (pqc == POSITIVE) // b is isolated on the negative side
-//         return visitor.result(coplanar_orientation(*c,*b,p) != POSITIVE);
-//       // a is isolated on the positive side
-//       return visitor.result(coplanar_orientation(*a,*c,p) != POSITIVE);
-
-//     default: // should not happen.
-//       CGAL_kernel_assertion(false);
-//       return visitor.result(false);
-//     }
-
-//   case NEGATIVE:
-//     switch ( pqb ) {
-//     case POSITIVE:
-//       if (pqc == POSITIVE)         // a is isolated on the negative side
-//         return visitor.result(coplanar_orientation(*b,*a,p) != POSITIVE);
-//       // b is isolated on the positive side
-//       return visitor.result(coplanar_orientation(*b,*a,p) != POSITIVE);
-
-//     case NEGATIVE:
-//       if (pqc == NEGATIVE)
-//         // the triangle lies in the negative halfspace
-//         // defined by the ray's supporting line.
-//         return visitor.result( false);
-//       // c is isolated on the positive side
-//       return visitor.result(coplanar_orientation(*c,*b,p) != POSITIVE);
-//     case COLLINEAR:
-//       if (pqc == NEGATIVE) // b is isolated on the positive side
-//         return visitor.result(coplanar_orientation(*b,*a,p) != POSITIVE);
-//       // a is isolated on the negative side
-//       return visitor.result(coplanar_orientation(*b,*a,p) != POSITIVE);
-
-//     default: // should not happen.
-//       CGAL_kernel_assertion(false);
-//       return visitor.result(false);
-//     }
-
-//   case COLLINEAR:
-//     switch ( pqb ) {
-//     case POSITIVE:
-//       if (pqc == POSITIVE) // a is isolated on the negative side
-//         return visitor.result(coplanar_orientation(*b,*a,p) != POSITIVE);
-//       // b is isolated on the positive side
-//       return visitor.result(coplanar_orientation(*b,*a,p) != POSITIVE);
-//     case NEGATIVE:
-//       if (pqc == NEGATIVE) // a is isolated on the positive side
-//         return visitor.result(coplanar_orientation(*a,*c,p) != POSITIVE);
-//       // b is isolated on the negative side
-//       return visitor.result(coplanar_orientation(*c,*b,p) != POSITIVE);
-
-//     case COLLINEAR:
-//       if (pqc == POSITIVE) // c is isolated on the positive side
-//         return visitor.result(coplanar_orientation(*c,*b,p) != POSITIVE);
-//       // c is isolated on the negative side
-//       return visitor.result(coplanar_orientation(*a,*c,p) != POSITIVE);
-//       // case pqc == COLLINEAR is imposiible
-
-//     default: // should not happen.
-//       CGAL_kernel_assertion(false);
-//       return visitor.result(false);
-//     }
-
-//   default: // should not happen.
-//     CGAL_kernel_assertion(false);
-//     return visitor.result(false);
-//   }
-// }
-
 template <class K>
 bool
-do_intersect_parity(
+do_intersect_odd_parity(
   const typename CGAL::BilinearPatchC3<K> &bp,
   const typename K::Ray_3 &r
 ) {
@@ -210,6 +37,7 @@ do_intersect_parity(
   // Case 0
   // Bilinear patch is planar and can be treated as two triangles
   if (bp.is_planar()) {
+    std::cout << "Entering Case 0..."<< std::endl;
     return (
           do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(1), bp.vertex(2)), r) 
       ||  do_intersect(K::Triangle_3(bp.vertex(1), bp.vertex(2), bp.vertex(3)), r)
@@ -219,231 +47,66 @@ do_intersect_parity(
   // Case 1
   // Origin lies inside bounding tetrahedron
   K::Point_3 ray_source = r.source();
-  if (bp.tetrahedron().has_on_bounded_side(ray_source) || bp.tetrahedron().has_on_boundary(ray_source)) {
-    // If the ray's origin lies on the bilinear patch,
-    // count that as an intersection. The function phi(x) 
-    // returns zero <==> x is on the patch.
-    
-    if (bp.has_on(ray_source)) {
+  if (bp.tetrahedron().has_on_bounded_side(ray_source) || bp.tetrahedron().has_on_boundary(ray_source)) 
+  {
+    CGAL::Interval_nt_advanced phi_source = bp.aux_phi(ray_source);
+    if (bp.aux_phi(ray_source).do_overlap(0)) 
+    {    
+      // If the ray's origin lies on the bilinear patch,
+      // count that as an intersection. The function phi(x) 
+      // returns zero <==> x is on the patch.
+      std::cout << "Entering Case 1a..."<< std::endl;
       return true;
+    } else
+    {
+      // Otherwise, check the sign of phi(origin). Two of the bounding
+      // tetrahedron's four triangles lie on the positive side of phi(*)==0,
+      // and two lie on the negative side. If the origin is on one side, 
+      // check the ray for intersection with the two triangles on the other side
+      std::cout << "Entering Case 1b..."<< std::endl;
+      const K::Point_3 & a = bp.vertex(0);
+      const K::Point_3 & c = bp.vertex(2);
+      const K::Point_3 mid_point = K::Point_3( 
+        (a.x() + c.x())/2., 
+        (a.y() + c.y())/2., 
+        (a.z() + c.z())/2. 
+      ); //  This will determine which triangles are on the opposite side
+      CGAL::Interval_nt_advanced phi_midpoint = bp.aux_phi(mid_point);
+      if ( !(phi_midpoint > 0) == !(phi_source > 0) ) {
+        // The edge connecting 0--2 is on the same side as the ray's source
+        std::cout << "...same side..."<< std::endl;
+        std::cout << "...same side..."<< std::endl;
+        std::cout << "...intersects 123: " << do_intersect(K::Triangle_3(bp.vertex(1), bp.vertex(2), bp.vertex(3)), r) << std::endl;
+        std::cout << "...intersects 013: " << do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(1), bp.vertex(3)), r) << std::endl;
+        std::cout << "...intersects 012: " << do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(1), bp.vertex(2)), r) << std::endl;
+        std::cout << "...intersects 023: " << do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(2), bp.vertex(3)), r) << std::endl;
+        return (
+          do_intersect(K::Triangle_3(bp.vertex(1), bp.vertex(2), bp.vertex(3)), r) || do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(1), bp.vertex(3)), r) 
+        );
+      } else {
+        std::cout << "...opposite side..."<< std::endl;
+        // The edge connecting 0--2 is on the opposite side as the ray's source
+        return (
+          do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(1), bp.vertex(2)), r) || do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(2), bp.vertex(3)), r) 
+        );
+      }
     }
+  } // End Case 1
 
-    // // Otherwise, check the sign of phi(origin). Two of the bounding
-    // // tetrahedron's four triangles lie on the positive side of phi(*)==0,
-    // // and two lie on the negative side. If the origin is on one side, 
-    // // check the ray for intersection with the two triangles on the other side
-    // if ( phi > 0 ) {
-    //   continue;
-    // } else {
-    //   continue;
-    // }
+  // Case 2
+  // Origin lies outside the bounding tetrahedron
+  if (!(do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(1), bp.vertex(2)), r)) != !(do_intersect(K::Triangle_3(bp.vertex(0), bp.vertex(2), bp.vertex(3)), r))) {
+    // The ray intersects exactly one of the bounding tetrahedron's two 
+    // triangles on the positive/negative side of phi(*)==0 _if_and_only_if_ the ray
+    // intersects the bilinear patch an odd number of times.
+    return true;
   }
 
-  // // Case 2
-  // // Origin lies outside the bounding tetrahedron
-  // if (false) {
-  //   // The ray intersects exactly one of the bounding tetrahedron's two 
-  //   // triangles on the positive side of phi(*)==0 __if and only if__ the ray
-  //   // intersects the bilinear patch an odd number of times.
-  // }
-
   return false;
-
-
-  // typedef typename K::Point_3 Point_3;
-
-  // typename K::Construct_vertex_3 vertex_on = k.construct_vertex_3_object();
-  // typename K::Orientation_3 orientation = k.orientation_3_object();
-
-  // const Point_3& a = vertex_on(t,0);
-  // const Point_3& b = vertex_on(t,1);
-  // const Point_3& c = vertex_on(t,2);
-
-  // typename K::Construct_vector_3  construct_vector = k.construct_vector_3_object();
-  // typename K::Construct_ray_3 construct_ray = k.construct_ray_3_object();
-  // typename K::Construct_point_on_3 point_on = k.construct_point_on_3_object();
-
-  // const Point_3& p = point_on(r,0);
-  // const Point_3& q = point_on(r,1);
-
-  // const Orientation ray_direction =
-  //   orientation(a,b,c,point_on(construct_ray(a, construct_vector(r)),1));
-
-  // if (ray_direction == COPLANAR ) {
-  //   if (orientation(a,b,c,p) == COPLANAR)
-  //     return do_intersect_coplanar(t,r,k,visitor);
-  //   else return visitor.result(false);
-  // }
-
-  // const Orientation abcp = orientation(a,b,c,p);
-
-  // switch ( abcp ) {
-  // case POSITIVE:
-  //   switch ( ray_direction ) {
-  //   case POSITIVE:
-  //     // the ray lies in the positive open halfspaces defined by the
-  //     // triangle's supporting plane
-  //     return visitor.result(false);
-
-  //   case NEGATIVE:{
-  //     // The ray straddles the triangle's plane
-  //     // p sees the triangle in counterclockwise order
-  //     Orientation
-  //     orient=orientation(p,q,a,b);
-  //     if (orient == POSITIVE ) return visitor.result(false);
-  //     visitor.update(orient);
-  //     orient=orientation(p,q,b,c);
-  //     if (orient == POSITIVE ) return visitor.result(false);
-  //     visitor.update(orient);
-  //     orient=orientation(p,q,c,a);
-  //     if (orient == POSITIVE ) return visitor.result(false);
-  //     visitor.update(orient);
-  //     return visitor.result(true);
-  //   }
-  //   // case COPLANAR: should not happen
-  //   default: // should not happen.
-  //     CGAL_kernel_assertion(false);
-  //     return visitor.result(false);
-  //   }
-
-  // case NEGATIVE:
-  //   switch ( ray_direction ) {
-  //   case POSITIVE:{
-  //     // The ray straddles the triangle's plane
-  //     // q sees the triangle in counterclockwise order
-  //     Orientation
-  //     orient=orientation(q,p,a,b);
-  //     if (orient == POSITIVE ) return visitor.result(false);
-  //     visitor.update(orient);
-  //     orient=orientation(q,p,b,c);
-  //     if (orient == POSITIVE ) return visitor.result(false);
-  //     visitor.update(orient);
-  //     orient=orientation(q,p,c,a);
-  //     if (orient == POSITIVE ) return visitor.result(false);
-  //     visitor.update(orient);
-  //     return visitor.result(true);
-
-  //   }
-  //   case NEGATIVE:
-  //     // the ray lies in the negative open halfspaces defined by the
-  //     // triangle's supporting plane
-  //     return visitor.result(false);
-
-  //     // case COPLANAR: should not happen
-
-  //   default: // should not happen.
-  //     CGAL_kernel_assertion(false);
-  //     return visitor.result(false);
-  //   }
-
-  // case COPLANAR: // p belongs to the triangle's supporting plane
-  //   visitor.end_point_in_triangle();
-  //   switch ( ray_direction ) {
-  //   case POSITIVE:
-  //     // q sees the triangle in counterclockwise order
-  //     return visitor.result(
-  //          orientation(q,p,a,b) != POSITIVE
-  //       && orientation(q,p,b,c) != POSITIVE
-  //       && orientation(q,p,c,a) != POSITIVE);
-
-  //   case NEGATIVE:
-  //     // q sees the triangle in clockwise order
-  //     return visitor.result(
-  //          orientation(p,q,a,b) != POSITIVE
-  //       && orientation(p,q,b,c) != POSITIVE
-  //       && orientation(p,q,c,a) != POSITIVE);
-
-  //     // case COPLANAR: should not happen
-
-  //   default: // should not happen.
-  //     CGAL_kernel_assertion(false);
-  //     return visitor.result(false);
-  //   }
-
-  // default: // should not happen.
-  //   CGAL_kernel_assertion(false);
-  //   return visitor.result(false);
-  // }
 }
-
-// template <class K>
-// typename K::Boolean
-// do_intersect(const typename K::Triangle_3& t,
-//              const typename K::Ray_3& r,
-//              const K& k)
-// {
-//   return do_intersect(t, r, k, r3t3_do_intersect_empty_visitor());
-// }
-
-// template <class K>
-// inline
-// typename K::Boolean
-// do_intersect(const typename K::Ray_3& r,
-//              const typename K::Triangle_3& t,
-//              const K& k)
-// {
-//   return do_intersect(t, r, k);
-// }
 
 } // namespace internal
 } // namespace Intersections
 } // namespace CGAL
 
 #endif // CGAL_INTERNAL_INTERSECTIONS_RAY_3_BILINEAR_PATCH_3_DO_INTERSECT_H
-
-
-// Copyright (c) 2018  GeometryFactory Sarl (France).
-// All rights reserved.
-//
-// This file is part of CGAL (www.cgal.org)
-//
-// $URL$
-// $Id$
-// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
-//
-//
-// Author(s)     : Andreas Fabri
-
-// #ifndef CGAL_INTERNAL_INTERSECTIONS_3_TETRAHEDRON_3_DO_INTERSECT_H
-// #define CGAL_INTERNAL_INTERSECTIONS_3_TETRAHEDRON_3_DO_INTERSECT_H
-
-// #include <CGAL/Intersections_3/internal/Line_3_Triangle_3_do_intersect.h>
-// #include <CGAL/Intersections_3/internal/Plane_3_Triangle_3_do_intersect.h>
-// #include <CGAL/Intersections_3/internal/Ray_3_Triangle_3_do_intersect.h>
-// #include <CGAL/Intersections_3/internal/Sphere_3_Triangle_3_do_intersect.h>
-
-// namespace CGAL {
-// namespace Intersections {
-// namespace internal {
-
-// template<typename K, class Unbounded>
-// typename K::Boolean
-// do_intersect_tetrahedron_unbounded(const typename K::Tetrahedron_3& tet,
-//                                    const Unbounded& unb,
-//                                    const K& k)
-// {
-//   typedef typename K::Boolean Boolean;
-
-//   Boolean result = false;
-//   for(int i = 0; i < 4; ++i)
-//   {
-//     const Boolean b = do_intersect(unb,
-//                                    k.construct_triangle_3_object()(tet[i],
-//                                                                    tet[(i+1)%4],
-//                                                                    tet[(i+2)%4]),
-//                                    k);
-//     if(certainly(b))
-//       return b;
-
-//     if(is_indeterminate(b))
-//       result = b;
-//   }
-
-//   return result;
-// }
-
-// } // namespace internal
-// } // namespace Intersections
-// } // namespace CGAL
-
-// #endif // CGAL_INTERNAL_INTERSECTIONS_3_TETRAHEDRON_3_DO_INTERSECT_H
