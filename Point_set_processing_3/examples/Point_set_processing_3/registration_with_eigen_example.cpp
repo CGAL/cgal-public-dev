1828f4afbff3dd66d3290c4cf2efad3cffd17ee4
daniel-unyi-42 <daniel.unyi.42@gmail.com>
2023-07-11 12:04:08 +0200

    Registration with Eigen example - the rigid part
    Nonrigid part will be added soon

--- Point_set_processing_3/examples/Point_set_processing_3/registration_with_eigen_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/registration_with_eigen_example.cpp
@@ -1,170 +1,141 @@
 #include <CGAL/Simple_cartesian.h>
-#include <CGAL/Point_set_3.h>
-#include <CGAL/draw_point_set_3.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/draw_surface_mesh.h>
 #include <CGAL/Aff_transformation_3.h>
-#include <CGAL/aff_transformation_tags.h>
+#include <CGAL/Polygon_mesh_processing/transform.h>
 #include <CGAL/Search_traits_3.h>
 #include <CGAL/Orthogonal_k_neighbor_search.h>
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
-#include <fstream>
-#include <chrono>
-#include <random>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-
 typedef CGAL::Simple_cartesian<double>                       Kernel;
 typedef Kernel::Point_3                                      Point;
-typedef CGAL::Point_set_3<Point>                             PointSet;
+typedef Kernel::Vector_3                                     Vector;
+typedef CGAL::Surface_mesh<Point>                            Mesh;
 typedef CGAL::Aff_transformation_3<Kernel>                   Transform;
-//typedef CGAL::Search_traits_3<Kernel>                        TreeTraits;
-//typedef CGAL::Orthogonal_k_neighbor_search<TreeTraits>       Neighbor_search;
-//typedef Neighbor_search::Tree                                Tree;
+typedef CGAL::Search_traits_3<Kernel>                        TreeTraits;
+typedef CGAL::Orthogonal_k_neighbor_search<TreeTraits>       Neighbor_search;
+typedef Neighbor_search::Tree                                Tree;
 
 
-int closest_point(const PointSet& sp, const Eigen::Vector3f& p) {
-    int closest = 0;
-    float min_dist = std::numeric_limits<float>::max();
-    for (auto i = 0; i < sp.size(); ++i) {
-        Eigen::Vector3f p_other(sp.point(i)[0], sp.point(i)[1], sp.point(i)[2]);
-        auto dist = (p - p_other).norm();
-        if (dist < min_dist) {
-            min_dist = dist;
-            closest = i;
+Mesh merge_meshes(Mesh a, Mesh b) {
+    for (auto f : a.faces()) {
+        std::array<Mesh::Vertex_index, 3> triangle;
+        int i = 0;
+        for (Mesh::Vertex_index v : a.vertices_around_face(a.halfedge(f))) {
+            triangle[i++] = b.add_vertex(a.point(v));
         }
+        b.add_face(triangle[0], triangle[1], triangle[2]);
     }
-    return closest;
+    return b;
 }
 
 
-Transform register_point_sets(const PointSet& source, const PointSet& target, float w1 = 1.0, float w2 = 1.0, bool point2plane = true) {
-
-    float ratio = w1 / w2;
-    // intermediate representation
+std::pair<Transform, Mesh> rigid_registration(const Mesh& source, const Mesh& target,
+                                              double w1 = 0.1, double w2 = 1.0, int max_iter = 100) {
+    // build k-d tree for nearest neighbor search
+    Tree tree(target.points().begin(), target.points().end());
+    // resulting transformation is (R, t)
+    Eigen::Matrix3d R_all = Eigen::Matrix3d::Identity();
+    Eigen::Vector3d t_all = Eigen::Vector3d::Zero();
+    int N = source.number_of_vertices();
     auto z = source;
-    // build tree
-    // Tree tree(target.points().begin(), target.points().end());
-    // initialize rigid transformation
-    Eigen::Matrix3f R_all = Eigen::Matrix3f::Identity();
-    Eigen::Vector3f t_all = Eigen::Vector3f::Zero();
-    for (auto time = 0; time < 100; ++time) {
-        // matrix A
-        Eigen::SparseMatrix<float> A(6 + 3 * z.size(), 6 + 3 * z.size());
+    // solver
+    Eigen::ConjugateGradient<Eigen::SparseMatrix<double>> cg;
+    cg.setMaxIterations(1000);
+    for (int iter = 0; iter < max_iter; ++iter) {
+        // build blocks of sparse coefficient matrix A
+        Eigen::SparseMatrix<double> A(6 + 3 * N, 6 + 3 * N);
         // column-wise sparsity pattern
-        Eigen::VectorXf a = Eigen::VectorXf::Ones(6 + 3 * z.size());
-        a.head(6) *= 6 + 3 * z.size();
-        a.tail(3 * z.size()) *= 9;
+        Eigen::VectorXd a = Eigen::VectorXd::Ones(6 + 3 * N);
+        a.head(6) *= 6 + 3 * N;
+        a.tail(3 * N) *= 6 + 1;
         A.reserve(a);
-        // vector b
-        Eigen::VectorXf b(6 + 3 * z.size());
+        // build blocks of result vector b
+        Eigen::VectorXd b(6 + 3 * N);
         b.setZero();
-        // fill matrix A and vector b
         auto start = std::chrono::high_resolution_clock::now();
-        for (auto i = 0; i < z.size(); ++i) {
-            Eigen::Vector3f sp(source.point(i)[0], source.point(i)[1], source.point(i)[2]);
-            Eigen::Vector3f p = R_all * sp + t_all;
-            //Eigen::Vector3f p = Eigen::Vector3f(z.point(i)[0], z.point(i)[1], z.point(i)[2]);
-            Eigen::Matrix3f Z;
-            Z << 0, -p[2], p[1], p[2], 0, -p[0], -p[1], p[0], 0;
-            Eigen::Matrix3f A_rr = Z * Z;
-            Eigen::Matrix3f dyad = ratio * Eigen::Matrix3f::Identity();
-            Eigen::Vector3f zp(z.point(i)[0], z.point(i)[1], z.point(i)[2]);
-            int closest = closest_point(target, zp);
-            if (point2plane) {
-                Eigen::Vector3f tn(target.normal(closest)[0], target.normal(closest)[1], target.normal(closest)[2]);
-                dyad *= tn * tn.transpose();
-            }
-            Eigen::Matrix3f A_zz = Eigen::Matrix3f::Identity() + dyad;
-            for (auto j = 0; j < 3; ++j) {
-                for (auto k = 0; k < 3; ++k) {
-                    A.coeffRef(j, k) += A_rr(j, k);
-                    A.coeffRef(j, 3 + k) -= Z(j, k);
-                    A.coeffRef(3 + k, j) -= Z(j, k);
-                    A.insert(6 + 3 * i + j, k) = Z(j, k);
-                    A.insert(k, 6 + 3 * i + j) = Z(j, k);
-                    A.insert(6 + 3 * i + j, 6 + 3 * i + k) = A_zz(j, k);
+        for (auto it = source.vertices().begin(); it != source.vertices().end(); ++it) {
+            auto i = it->idx();
+            Point xp = source.point(*it);
+            Eigen::Vector3d x(xp.x(), xp.y(), xp.z());
+            // apply transformation to original x
+            Eigen::Vector3d x_t = R_all * x + t_all;
+            // build a matrix equivalent to cross product
+            Eigen::Matrix3d X_t;
+            X_t << 0, -x_t(2), x_t(1),
+                   x_t(2), 0, -x_t(0),
+                   -x_t(1), x_t(0), 0;
+            Eigen::Matrix3d XX_t(X_t * X_t);
+            for (int j = 0; j < 3; ++j) {
+                for (int k = 0; k < 3; ++k) {
+                    A.coeffRef(j, k) -= XX_t(j, k); // A_rr
+                    A.coeffRef(j, 3 + k) += X_t(j, k); // A_tr
+                    A.coeffRef(3 + k, j) -= X_t(k, j); // A_rt
+                    A.insert(6 + 3 * i + j, k) = X_t(j, k); // A_zir
+                    A.insert(k, 6 + 3 * i + j) = -X_t(k, j); // A_rzj
                 }
-                A.coeffRef(3 + j, 3 + j) += 1;
-                A.insert(6 + 3 * i + j, 3 + j) = -1;
-                A.insert(3 + j, 6 + 3 * i + j) = -1;
-            }
-            b.segment<3>(3) -= p;
-            //Neighbor_search search(tree, z.point(i), 1);
-            //Point pnn = search.begin()->first;
-            Eigen::Vector3f tp(target.point(closest)[0], target.point(closest)[1], target.point(closest)[2]);
-            b.segment<3>(6 + 3 * i) = p + dyad * tp;
+                A.coeffRef(3 + j, 3 + j) += 1; // A_tt
+                A.insert(6 + 3 * i + j, 3 + j) = -1; // A_zit
+                A.insert(3 + j, 6 + 3 * i + j) = -1; // A_tzi
+                A.insert(6 + 3 * i + j, 6 + 3 * i + j) = 1 + w1 / w2; // A_zizj
+			}
+            b.segment<3>(3) -= x_t;
+            // search nearest neighbor
+            Point zp = z.point(*it);
+            Point pnn = Neighbor_search(tree, zp, 1).begin()->first;
+            Eigen::Vector3d PI(pnn.x(), pnn.y(), pnn.z());
+            b.segment<3>(6 + 3 * i) = w1 / w2 * PI + x_t;
         }
         auto stop = std::chrono::high_resolution_clock::now();
-        std::cout << time << ' ' << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << std::endl;
-        // solve the linear system
-        Eigen::ConjugateGradient<Eigen::SparseMatrix<float>> cg;
-        cg.setMaxIterations(20);
+        // solve system of linear equations
         cg.compute(A);
-        Eigen::VectorXf x = cg.solve(b);
+        Eigen::VectorXd x = cg.solve(b);
         if (cg.info() == Eigen::Success) {
             std::cout << "Convergence achieved!" << std::endl;
         }
         std::cout << "#iterations:     " << cg.iterations() << std::endl;
         std::cout << "estimated error: " << cg.error() << std::endl;
-        Eigen::Vector3f r(x[0], x[1], x[2]);
-        auto roll = Eigen::AngleAxisf(x[0], Eigen::Vector3f::UnitX());
-        auto pitch = Eigen::AngleAxisf(x[1], Eigen::Vector3f::UnitY());
-        auto yaw = Eigen::AngleAxisf(x[2], Eigen::Vector3f::UnitZ());
-        Eigen::Matrix3f R = (roll * pitch * yaw).matrix();
-        Eigen::Vector3f t(x[3], x[4], x[5]);
-        //std::cout << "R = " << R << std::endl;
-        //std::cout << "t = " << t << std::endl;
+        // update r
+        Eigen::Vector3d r(x[0], x[1], x[2]);
+        auto roll = Eigen::AngleAxisd(x[0], Eigen::Vector3d::UnitX());
+        auto pitch = Eigen::AngleAxisd(x[1], Eigen::Vector3d::UnitY());
+        auto yaw = Eigen::AngleAxisd(x[2], Eigen::Vector3d::UnitZ());
+        Eigen::Matrix3d R = (roll * pitch * yaw).matrix();
         R_all *= R;
+        // update t
+        Eigen::Vector3d t(x[3], x[4], x[5]);
         t_all += t;
-        std::cout << "R_all = " << std::endl << R_all << std::endl;
-        std::cout << "t_all = " << std::endl << t_all << std::endl;
         // update z
-        for (auto i = 0; i < z.size(); ++i) {
-            Eigen::Vector3f p(x[6 + 3 * i], x[6 + 3 * i + 1], x[6 + 3 * i + 2]);
-            z.point(i) = Point(p[0], p[1], p[2]);
+        for (auto it = z.vertices().begin(); it != z.vertices().end(); ++it) {
+            auto i = it->idx();
+            Point p(x[6 + 3*i], x[6+ 3*i + 1], x[6 + 3*i+2]);
+            z.point(*it) = p;
         }
-        CGAL::draw(z);
+        auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start).count();
+        std::cout << iter << ' ' << duration << '\n' << R_all << '\n' << t_all << std::endl;
     }
-    Transform transform(R_all(0, 0), R_all(0, 1), R_all(0, 2), t_all[0],
-        				R_all(1, 0), R_all(1, 1), R_all(1, 2), t_all[1],
-        				R_all(2, 0), R_all(2, 1), R_all(2, 2), t_all[2], 1);
-    return transform;
+    Transform transform(R_all(0, 0), R_all(0, 1), R_all(0, 2), t_all(0),
+                        R_all(1, 0), R_all(1, 1), R_all(1, 2), t_all(1),
+                        R_all(2, 0), R_all(2, 1), R_all(2, 2), t_all(2), 1);
+    return std::make_pair(transform, z);
 }
 
-
 int main(int argc, char* argv[]) {
-
-    const std::string filename = argv[1];
-    PointSet sp1;
-    if (!CGAL::IO::read_PLY(filename, sp1)) {
-        std::cerr << "Invalid input file." << std::endl;
-        return EXIT_FAILURE;
-    }
-    // copy the point set
-    PointSet sp2 = sp1;
-
-    // define a transformation
-    const float alpha = M_PI / 6;
-    const float sin_alpha = sin(alpha);
-    const float cos_alpha = cos(alpha);
-    const Transform transform(cos_alpha, -sin_alpha, 0, 0,
-                              sin_alpha,  cos_alpha, 0, 0,
-                              0,          0,         1, 0, 1);
-    std::cout << std::fixed << std::setprecision(4) << "transform = " << std::endl << transform << std::endl;
-    // transform the points
-    for (auto i = 0; i < sp2.size(); ++i) {
-        Point& p = sp2.point(i);
-        p = transform.transform(p);
-    }
-    //auto g = std::default_random_engine(42);
-    //for (auto i = sp2.size() - 1; i > 0; --i) {
-    //    std::uniform_int_distribution<decltype(i)> d(0, i);
-    //    std::swap(sp2.point(i), sp2.point(d(g)));
-    //}
-    CGAL::draw(sp1);
-    CGAL::draw(sp2);
-    // register the point sets
-    auto result = register_point_sets(sp2, sp1);
+    const std::string filename1 = argv[1]; //CGAL::data_file_path("meshes/wolf1.ply");
+    Mesh mesh1;
+    CGAL::IO::read_PLY(filename1, mesh1);
+    const std::string filename2 = argv[2]; //CGAL::data_file_path("meshes/wolf2.ply");
+    Mesh mesh2;
+    CGAL::IO::read_PLY(filename2, mesh2);
+    CGAL::draw(merge_meshes(mesh1, mesh2));
+    auto result = rigid_registration(mesh1, mesh2, 0.1, 1.0, 10);
+    Transform transform = result.first;
+    Mesh z = result.second;
+    CGAL::Polygon_mesh_processing::transform(transform, mesh1);
+    //CGAL::Polygon_mesh_processing::transform(Transform(CGAL::Translation(), Vector(-40, 0, 0)), mesh1);
+    CGAL::draw(merge_meshes(mesh1, mesh2));
     return EXIT_SUCCESS;
 }
