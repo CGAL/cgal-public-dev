1092aeddd76163119006c399bd15bb0d3e843cfa
pups <Control3d@gmail.com>
2023-08-25 13:39:01 +0200

    added deleted file

---
+++ Variational_shape_reconstruction/include/CGAL/Variational_shape_reconstruction.h
@@ -0,0 +1,32 @@
+// Copyright(c) 2023 GeometryFactory
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : 
+
+#ifndef CGAL_VARIATIONAL_SHAPE_RECONSTRUCTION_H
+#define CGAL_VARIATIONAL_SHAPE_RECONSTRUCTION_H
+
+    
+//#include <CGAL/license/Variational_shape_reconstruction.h>
+
+
+/**
+ * \ingroup PkgVariational_shape_reconstructionRef
+ * \file CGAL/Variational_shape_reconstruction.h
+ * Convenience header file only including `CGAL/Variational_shape_reconstruction/Variational_shape_reconstruction.h`
+ * and `CGAL/Variational_shape_reconstruction/Variational_shape_reconstruction_traits.h`.
+ */
+
+#include <CGAL/Variational_shape_reconstruction/Variational_shape_reconstruction.h>
+#include <CGAL/Variational_shape_reconstruction/Variational_shape_reconstruction_traits.h>
+
+#include <CGAL/Variational_shape_reconstruction/internal/Variational_shape_reconstruction.h>
+
+
+#endif // CGAL_VARIATIONAL_SHAPE_RECONSTRUCTION_H
