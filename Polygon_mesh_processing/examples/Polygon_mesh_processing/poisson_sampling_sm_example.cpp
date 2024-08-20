cef6e6eaf78372d3b1cd65f3eed22b3d8bc876e2
Sébastien Loriot <sebastien.loriot@cgal.org>
2024-08-20 15:26:23 +0200

    add example using poisson disk sampling

---
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/poisson_sampling_sm_example.cpp
@@ -0,0 +1,44 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Polygon_mesh_processing/distance.h>
+
+#include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel   K;
+typedef K::Point_3                                            Point;
+
+typedef CGAL::Surface_mesh<Point>                             Mesh;
+typedef boost::graph_traits<Mesh>::vertex_descriptor          vertex_descriptor;
+typedef boost::graph_traits<Mesh>::face_descriptor            face_descriptor;
+
+namespace PMP = CGAL::Polygon_mesh_processing;
+
+int main(int argc, char* argv[])
+{
+  const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/eight.off");
+
+  Mesh mesh;
+  if(!PMP::IO::read_polygon_mesh(filename, mesh))
+  {
+    std::cerr << "Invalid input." << std::endl;
+    return 1;
+  }
+
+  const int points_per_face = (argc > 2) ? std::stoi(argv[2]) : 10;
+
+  std::vector<Point> points;
+  PMP::sample_triangle_mesh(mesh,
+                            std::back_inserter(points),
+                            CGAL::parameters::number_of_points_per_face(points_per_face).
+                                              use_poisson_disk_sampling_euclidean(true));
+
+  std::ofstream out("sampling.xyz");
+  out << std::setprecision(17);
+  std::copy(points.begin(), points.end(), std::ostream_iterator<Point>(out, "\n"));
+
+  std::cout << points.size() << std::endl;
+  return 0;
+}
