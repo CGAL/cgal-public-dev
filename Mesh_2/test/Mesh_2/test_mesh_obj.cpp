20490661a989c7e184c7baae7dcfd2848507ce3f
Laurent Rineau <laurent.rineau@cgal.org>
2026-02-05 17:20:23 +0100

    test_mesh_obj: refactor, and use bisect_failure

--- Mesh_2/test/Mesh_2/test_mesh_obj.cpp
+++ Mesh_2/test/Mesh_2/test_mesh_obj.cpp
@@ -1,16 +1,31 @@
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/IO/OBJ.h>
+// #define CGAL_MESH_2_VERBOSE 1
+// #define CGAL_MESH_2_DEBUG_REFINEMENT_POINTS 1
+// #define CGAL_MESHES_DEBUG_REFINEMENT_POINTS 1
+// #define CGAL_MESH_2_DEBUG_BAD_EDGES 1
+// #define CGAL_MESH_2_DEBUG_BAD_FACES 1
+// #define CGAL_MESH_2_DEBUG_CLUSTERS 1
+// #define CGAL_MESH_2_DEBUG_INSERTIONS 1
+#include <CGAL/bisect_failures.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/Constrained_triangulation_plus_2.h>
-#include <CGAL/Delaunay_mesher_2.h>
 #include <CGAL/Delaunay_mesh_face_base_2.h>
 #include <CGAL/Delaunay_mesh_size_criteria_2.h>
+#include <CGAL/Delaunay_mesher_2.h>
 #include <CGAL/Delaunay_mesher_no_edge_refinement_2.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/IO/OBJ.h>
+#include <CGAL/IO/write_VTU.h>
 #include <CGAL/Triangulation_conformer_2.h>
+#include <CGAL/Triangulation_simplex_base_with_time_stamp.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
 
+#include <algorithm>
+#include <cassert>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <limits>
+#include <string>
 #include <vector>
 
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
@@ -19,48 +34,152 @@ using FT = K::FT;
 using Point_2 = K::Point_2;
 using Vector_2 = K::Vector_2;
 
-using Vb = CGAL::Triangulation_vertex_base_2<K>;
+using Vb = CGAL::Triangulation_simplex_base_with_time_stamp<
+    CGAL::Triangulation_vertex_base_with_info_2<std::size_t, K, CGAL::Triangulation_vertex_base_2<K>>>;
 using Fb = CGAL::Delaunay_mesh_face_base_2<K>;
 using Tds = CGAL::Triangulation_data_structure_2<Vb, Fb>;
 using CDT = CGAL::Constrained_Delaunay_triangulation_2<K, Tds, CGAL::Exact_predicates_tag>;
 
-int main(int argc, char*argv[] )
+struct Obj_data {
+  std::vector<Point_2> points;
+  std::vector<std::vector<std::size_t> > polylines;
+};
+
+static std::ofstream save_obj(const Obj_data& data, const std::string& filename)
+{
+  std::ofstream out(filename);
+  out.precision(17);
+  if(!out)
+    return out;
+
+  for(const Point_2& p : data.points) {
+    out << "v " << p.x() << " " << p.y() << " 0\n";
+  }
+  for(const std::vector<std::size_t>& pl : data.polylines) {
+    if(pl.size() < 2)
+      continue;
+    out << "l";
+    for(std::size_t id : pl) {
+      out << " " << (id + 1);
+    }
+    out << "\n";
+  }
+  return out;
+}
+
+static int run_mesh(const Obj_data& data)
 {
   CDT cdt;
-  // std::ifstream in(argv[1]);
-  std::ifstream in(argc>1?argv[1]:"mini.obj");
-  if(!in)
-    return EXIT_FAILURE;
-
-  std::vector<Point_2> points;
-  std::vector<std::vector<std::size_t> > id_polylines;
-  std::vector<std::vector<std::size_t> > unused_id_polygons;
-  bool success = CGAL::IO::internal::read_OBJ(in, points, id_polylines, unused_id_polygons);
-  if(!success)
-    return EXIT_FAILURE;
 
   double squared_distance = (std::numeric_limits<double>::max)();
-  for(const std::vector<std::size_t>& id_pl : id_polylines) {
+  for(const std::vector<std::size_t>& id_pl : data.polylines) {
     for(std::size_t pid = 1; pid < id_pl.size(); ++pid) {
-      assert(points[id_pl[pid-1]] != points[id_pl[pid]]);
-      if(CGAL::squared_distance(points[id_pl[pid-1]], points[id_pl[pid]]) < squared_distance)
-        squared_distance = CGAL::squared_distance(points[id_pl[pid-1]], points[id_pl[pid]]);
-      cdt.insert_constraint(points[id_pl[pid-1]], points[id_pl[pid]]);
+      assert(data.points[id_pl[pid - 1]] != data.points[id_pl[pid]]);
+      squared_distance =
+          (std::min)(squared_distance, CGAL::squared_distance(data.points[id_pl[pid - 1]], data.points[id_pl[pid]]));
+      auto va = cdt.insert(data.points[id_pl[pid - 1]]);
+      va->info() = id_pl[pid - 1];
+      auto vb = cdt.insert(data.points[id_pl[pid]]);
+      vb->info() = id_pl[pid];
+      // std::cerr << "Inserting constraint between " << CGAL::IO::oformat(va, CGAL::With_point_tag{})
+      //           << " and " << CGAL::IO::oformat(vb, CGAL::With_point_tag{}) << "\n";
+      cdt.insert_constraint(va, vb);
     }
   }
 
-  CDT::Face_circulator fc = cdt.incident_faces(cdt.infinite_vertex()), done = fc;
-  do{
-    fc->set_constraint(fc->index(cdt.infinite_vertex()), true);
-    ++fc;
-  } while (fc != done);
+  auto file_stream = save_obj(data, "initial_cdt.obj");
+
+  if(cdt.dimension() == 2) {
+    CDT::Face_circulator fc = cdt.incident_faces(cdt.infinite_vertex()), done = fc;
+    do{
+      auto edge_index = fc->index(cdt.infinite_vertex());
+      if(!fc->is_constrained(edge_index)) {
+        auto va = fc->vertex(cdt.cw(edge_index));
+        auto vb = fc->vertex(cdt.ccw(edge_index));
+      //   std::cerr << "Add constrained edge on the convex hull: ";
+      //   std::cerr << CGAL::IO::oformat(va, CGAL::With_point_tag{}) << "  "
+      //             << CGAL::IO::oformat(vb, CGAL::With_point_tag{}) << "\n";
+        cdt.insert_constraint(va, vb);
+        file_stream << "l " << (va->info() + 1) << " " << (vb->info() + 1) << "\n";
+      }
+      ++fc;
+    } while (fc != done);
+  }
+
+  file_stream.close();
 
   std::cout << "Before conforming Gabriel: "
             << cdt.number_of_vertices() << " vertices.\n"
             << "  Smallest squared distance between constraint endpoints: "
             << squared_distance << "\n";
 
-  CGAL::make_conforming_Gabriel_2(cdt);
+  CGAL::refine_Delaunay_mesh_2(cdt, CGAL::parameters::criteria(CGAL::Delaunay_mesh_criteria_2<CDT>{}));
+
+  // CGAL::Triangulation_conformer_2<CDT> conform(cdt);
+  // conform.init_Gabriel();
+  // auto i = 0u;
+  // while(!conform.is_conforming_done()) {
+  //   conform.try_one_step_conforming_Gabriel();
+  //   // std::string filename = "debug-conforming-gabriel-step-" + std::to_string(i) + ".vtu";
+  //   // CGAL::IO::write_VTU(filename, cdt, CGAL::IO::ASCII);
+  //   ++i;
+  // }
   std::cout << "done" << std::endl;
+  CGAL::IO::write_VTU("conformed_cdt.vtu", cdt, CGAL::IO::ASCII);
   return EXIT_SUCCESS;
+}
+
+static std::string join_path(const std::string& dir, const std::string& name)
+{
+  if(dir.empty())
+    return name;
+  if(dir.back() == '/')
+    return dir + name;
+  return dir + "/" + name;
+}
+
+int main(int argc, char*argv[] )
+{
+  std::cerr.precision(17);
+  std::cout.precision(17);
+  std::clog.precision(17);
+  std::ifstream in(argc > 1 ? argv[1] : "mini.obj");
+  if(!in)
+    return EXIT_FAILURE;
+
+  Obj_data data;
+  std::vector<std::vector<std::size_t> > unused_id_polygons;
+  bool success = CGAL::IO::internal::read_OBJ(in, data.points, data.polylines, unused_id_polygons);
+  if(!success)
+    return EXIT_FAILURE;
+
+  auto get_size_fn = [](const Obj_data& d) -> std::size_t {
+    return d.polylines.size();
+  };
+
+  auto simplify_fn = [](Obj_data& d, std::size_t start, std::size_t end) -> bool {
+    if(start >= end || start >= d.polylines.size())
+      return false;
+    end = (std::min)(end, d.polylines.size());
+    d.polylines.erase(d.polylines.begin() + start, d.polylines.begin() + end);
+    return true;
+  };
+
+  auto run_fn = [](const Obj_data& d) -> int {
+    return run_mesh(d);
+  };
+
+  std::string output_dir = "./";
+
+  auto save_fn = [&](const Obj_data& d, const std::string& prefix) {
+    const std::string filename = join_path(output_dir, "bisect_" + prefix + ".obj");
+    save_obj(d, filename);
+  };
+
+  if(argc > 2) {
+    output_dir = argv[2];
+    return CGAL::bisect_failures(data, get_size_fn, simplify_fn, run_fn, save_fn);
+  } else {
+    return run_mesh(data);
+  }
 }
