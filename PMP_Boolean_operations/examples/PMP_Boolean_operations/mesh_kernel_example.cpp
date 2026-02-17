85f70cf9ea44f9a62bd027e2e6c91fd17c847c18
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2026-02-17 16:18:59 +0100

    Misc fix/improvements

--- PMP_Boolean_operations/examples/PMP_Boolean_operations/mesh_kernel_example.cpp
+++ PMP_Boolean_operations/examples/PMP_Boolean_operations/mesh_kernel_example.cpp
@@ -1,13 +1,14 @@
 #include <CGAL/Surface_mesh.h>
-#include <CGAL/Polygon_mesh_processing/kernel.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <CGAL/Polygon_mesh_processing/kernel.h>
 
 #include <CGAL/Real_timer.h>
 
-using K=CGAL::Exact_predicates_inexact_constructions_kernel;
-using Mesh=CGAL::Surface_mesh<K::Point_3>;
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Mesh = CGAL::Surface_mesh<K::Point_3>;
 
-namespace PMP=CGAL::Polygon_mesh_processing;
+namespace PMP = CGAL::Polygon_mesh_processing;
 
 int main(int argc, char** argv)
 {
@@ -15,16 +16,18 @@ int main(int argc, char** argv)
   const std::string oname = (argc > 2) ? argv[2] : "kernel.off";
 
   Mesh m;
-  if (!CGAL::IO::read_polygon_mesh(fname, m)|| is_empty(m))
+  if (!CGAL::IO::read_polygon_mesh(fname, m) || is_empty(m))
   {
     std::cerr << "ERROR: cannot read " << fname << "\n";
-    exit(1);
+    return EXIT_FAILURE;
   }
 
   CGAL::Real_timer timer;
   timer.start();
+
   Mesh kernel;
   PMP::kernel(m, kernel);
+
   timer.stop();
 
   std::cout << "Kernel computation of " << fname << " done in " << timer.time() << std::endl;
@@ -32,5 +35,5 @@ int main(int argc, char** argv)
 
   CGAL::IO::write_polygon_mesh("kernel.off", kernel, CGAL::parameters::stream_precision(17));
 
-  return 0;
+  return EXIT_SUCCESS;
 }
