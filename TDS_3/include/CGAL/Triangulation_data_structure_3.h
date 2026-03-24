34ec4aeb1ea6bbbde8bee1fa66dcda12767bd87a
Andreas Fabri <andreas.fabri@geometryfactory.com>
2026-03-24 17:33:27 +0100

    Apply suggestions from code review
    Co-authored-by: Sebastien Loriot <sloriot.ml@gmail.com>

--- TDS_3/include/CGAL/Triangulation_data_structure_3.h
+++ TDS_3/include/CGAL/Triangulation_data_structure_3.h
@@ -1604,7 +1604,7 @@ public:
   Vertex_range & vertices() const
   { return const_cast<Tds*>(this)->_vertices; }
 
-  /// @TODO document?
+  /// @todo document?
   /// Vertex ranges defining a simplex
   static std::array<Vertex_handle, 2> vertices(const Edge& e)
   {
