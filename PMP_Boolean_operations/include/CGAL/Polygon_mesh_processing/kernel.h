c878e3d10bfa54b1518eb960e01809855da01871
Sebastien Loriot <sloriot.ml@gmail.com>
2026-04-07 17:14:28 +0200

    doc precision
    Co-authored-by: Laurent Rineau <Laurent.Rineau@cgal.org>

--- PMP_Boolean_operations/include/CGAL/Polygon_mesh_processing/kernel.h
+++ PMP_Boolean_operations/include/CGAL/Polygon_mesh_processing/kernel.h
@@ -457,7 +457,7 @@ bool has_empty_kernel(const PolygonMesh& pm,
   *
   * See `CGAL::Polygon_mesh_processing::kernel()` for a comprehensive description of the parameters.
   *
-  * In addition to the parameters available in `CGAL::Polygon_mesh_processing::kernel()`, the following parameter is also available:
+  * In addition to the parameters available in `CGAL::Polygon_mesh_processing::kernel()`, the following named parameter is also available:
   *
   * \cgalNamedParamsBegin
   *   \cgalParamNBegin{strictly_inside}
