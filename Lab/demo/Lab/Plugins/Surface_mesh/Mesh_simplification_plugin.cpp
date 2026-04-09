10415b62ccb78b078b363367de65cb37932ba8a9
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2026-04-09 16:49:36 +0200

    Misc cleaning

--- Lab/demo/Lab/Plugins/Surface_mesh/Mesh_simplification_plugin.cpp
+++ Lab/demo/Lab/Plugins/Surface_mesh/Mesh_simplification_plugin.cpp
@@ -1,14 +1,7 @@
-#include <CGAL/Three/CGAL_Lab_plugin_interface.h>
+#include "ui_Mesh_simplification_dialog.h"
 
 #include "Scene_surface_mesh_item.h"
 #include "Scene_polyhedron_selection_item.h"
-
-#include <QApplication>
-#include <QMainWindow>
-#include <QInputDialog>
-#include <QElapsedTimer>
-#include <QAction>
-#include <QMessageBox>
 
 #include <CGAL/Surface_mesh_simplification/edge_collapse.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_count_stop_predicate.h>
@@ -17,21 +10,33 @@
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_placement.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_normal_change_placement.h>
 
-#include "ui_Mesh_simplification_dialog.h"
+#include <CGAL/Three/CGAL_Lab_plugin_interface.h>
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QInputDialog>
+#include <QElapsedTimer>
+#include <QAction>
+#include <QMessageBox>
 
 typedef Scene_surface_mesh_item Scene_facegraph_item;
 typedef Scene_facegraph_item::Face_graph FaceGraph;
 
+namespace SMS = ::CGAL::Surface_mesh_simplification;
+
 class Custom_stop_predicate
 {
   bool m_and;
-  CGAL::Surface_mesh_simplification::Edge_count_stop_predicate<FaceGraph> m_count_stop;
-  CGAL::Surface_mesh_simplification::Edge_length_stop_predicate<double> m_length_stop;
+  SMS::Edge_count_stop_predicate<FaceGraph> m_count_stop;
+  SMS::Edge_length_stop_predicate<double> m_length_stop;
 
 public:
-
-  Custom_stop_predicate (bool use_and, std::size_t nb_edges, double edge_length)
-    : m_and (use_and), m_count_stop (nb_edges), m_length_stop (edge_length)
+  Custom_stop_predicate(const bool use_and,
+                        const std::size_t nb_edges,
+                        const double edge_length)
+    : m_and (use_and),
+      m_count_stop (nb_edges),
+      m_length_stop (edge_length)
   {
     std::cerr << "Simplifying until:" << std::endl
               << " * Number of edges = " << nb_edges << std::endl
@@ -40,31 +45,35 @@ public:
   }
 
   template <typename Profile>
-  bool operator() (const double& current_cost, const Profile& edge_profile,
-                   std::size_t initial_count, std::size_t current_count) const
+  bool operator()(const double& current_cost,
+                  const Profile& edge_profile,
+                  const std::size_t initial_count,
+                  const std::size_t current_count) const
   {
     if (m_and)
-      return (m_count_stop(current_cost, edge_profile, initial_count, current_count)
-              && m_length_stop(current_cost, edge_profile, initial_count, current_count));
+    {
+      return (m_count_stop(current_cost, edge_profile, initial_count, current_count) &&
+              m_length_stop(current_cost, edge_profile, initial_count, current_count));
+    }
     else
-      return (m_count_stop(current_cost, edge_profile, initial_count, current_count)
-              || m_length_stop(current_cost, edge_profile, initial_count, current_count));
+    {
+      return (m_count_stop(current_cost, edge_profile, initial_count, current_count) ||
+              m_length_stop(current_cost, edge_profile, initial_count, current_count));
+    }
   }
-
 };
 
-
 using namespace CGAL::Three;
-class CGAL_Lab_mesh_simplification_plugin :
-  public QObject,
-  public CGAL_Lab_plugin_interface
+
+class CGAL_Lab_mesh_simplification_plugin
+  : public QObject,
+    public CGAL_Lab_plugin_interface
 {
   Q_OBJECT
   Q_INTERFACES(CGAL::Three::CGAL_Lab_plugin_interface)
   Q_PLUGIN_METADATA(IID "com.geometryfactory.CGALLab.PluginInterface/1.0")
 
 public:
-
   QList<QAction*> actions() const {
     return _actions;
   }
@@ -73,34 +82,32 @@ public:
             Scene_interface* scene_interface,
             Messages_interface*)
   {
-      mw = mainWindow;
-      scene = scene_interface;
-     QAction *actionSimplify = new QAction("Simplification", mw);
-      actionSimplify->setProperty("subMenuName",
-                                  "Triangulated Surface Mesh Simplification");
-      connect(actionSimplify, SIGNAL(triggered()), this, SLOT(on_actionSimplify_triggered()));
-      _actions <<actionSimplify;
+    mw = mainWindow;
+    scene = scene_interface;
+    QAction *actionSimplify = new QAction("Simplification", mw);
+    actionSimplify->setProperty("subMenuName",
+                                "Triangulated Surface Mesh Simplification");
+    connect(actionSimplify, SIGNAL(triggered()), this, SLOT(on_actionSimplify_triggered()));
+    _actions <<actionSimplify;
+  }
 
-  }
   bool applicable(QAction*) const {
-    return qobject_cast<Scene_facegraph_item*>(scene->item(scene->mainSelectionIndex()))
-      || qobject_cast<Scene_polyhedron_selection_item*>(scene->item(scene->mainSelectionIndex()));
+    return qobject_cast<Scene_facegraph_item*>(scene->item(scene->mainSelectionIndex())) ||
+           qobject_cast<Scene_polyhedron_selection_item*>(scene->item(scene->mainSelectionIndex()));
   }
+
 public Q_SLOTS:
   void on_actionSimplify_triggered();
+
 private :
   Scene_interface *scene;
   QMainWindow *mw;
   QList<QAction*> _actions;
-
 }; // end CGAL_Lab_mesh_simplification_plugin
 
 void CGAL_Lab_mesh_simplification_plugin::on_actionSimplify_triggered()
 {
   const CGAL::Three::Scene_interface::Item_id index = scene->mainSelectionIndex();
-
-  Scene_facegraph_item* poly_item =
-    qobject_cast<Scene_facegraph_item*>(scene->item(index));
 
   Scene_polyhedron_selection_item* selection_item =
     qobject_cast<Scene_polyhedron_selection_item*>(scene->item(index));
@@ -109,11 +116,14 @@ void CGAL_Lab_mesh_simplification_plugin::on_actionSimplify_triggered()
     QMessageBox::warning(mw, "Empty Edges", "There are no selected edges. Aborting.");
     return;
   }
+
+  Scene_facegraph_item* poly_item =
+    qobject_cast<Scene_facegraph_item*>(scene->item(index));
+
   if (poly_item || selection_item)
   {
-    FaceGraph& pmesh = (poly_item != nullptr)
-      ? *poly_item->polyhedron()
-      : *selection_item->polyhedron();
+    FaceGraph& pmesh = (poly_item != nullptr) ? *poly_item->polyhedron()
+                                              : *selection_item->polyhedron();
 
     // get option
     QDialog dialog(mw);
@@ -125,16 +135,16 @@ void CGAL_Lab_mesh_simplification_plugin::on_actionSimplify_triggered()
             &dialog, SLOT(reject()));
 
     Scene_interface::Bbox bbox = poly_item != nullptr ? poly_item->bbox()
-      : (selection_item != nullptr ? selection_item->bbox()
-        : scene->bbox());
+                                                      : (selection_item != nullptr ? selection_item->bbox()
+                                                                                   : scene->bbox());
 
-    double diago_length = CGAL::sqrt((bbox.xmax()-bbox.xmin())*(bbox.xmax()-bbox.xmin())
-                                     + (bbox.ymax()-bbox.ymin())*(bbox.ymax()-bbox.ymin()) +
-                                     (bbox.zmax()-bbox.zmin())*(bbox.zmax()-bbox.zmin()));
+    double diago_length = CGAL::sqrt((bbox.xmax() - bbox.xmin())*(bbox.xmax() - bbox.xmin()) +
+                                     (bbox.ymax() - bbox.ymin())*(bbox.ymax() - bbox.ymin()) +
+                                     (bbox.zmax() - bbox.zmin())*(bbox.zmax() - bbox.zmin()));
 
-    ui.m_nb_edges->setValue ((int)(num_halfedges(pmesh) / 4));
-    ui.m_nb_edges->setMaximum ((int)(num_halfedges(pmesh)));
-    ui.m_edge_length->setValue (diago_length * 0.05);
+    ui.m_nb_edges->setValue((int)(num_halfedges(pmesh) / 4));
+    ui.m_nb_edges->setMaximum((int)(num_halfedges(pmesh)));
+    ui.m_edge_length->setValue(diago_length * 0.05);
 
     // check user cancellation
     if(dialog.exec() == QDialog::Rejected)
@@ -144,46 +154,35 @@ void CGAL_Lab_mesh_simplification_plugin::on_actionSimplify_triggered()
     QElapsedTimer time;
     time.start();
     std::cout << "Simplify...";
+
     QApplication::setOverrideCursor(Qt::WaitCursor);
     QApplication::processEvents();
-    Custom_stop_predicate stop ((ui.m_combinatorial->currentIndex() == 0)
-                                && !(ui.m_use_nb_edges->isChecked())
-                                && !(ui.m_use_edge_length->isChecked()),
-                                (ui.m_use_nb_edges->isChecked()
-                                 ? ui.m_nb_edges->value()
-                                 : 0),
-                                (ui.m_use_edge_length->isChecked()
-                                 ? ui.m_edge_length->value()
-                                 : (std::numeric_limits<double>::max)()));
 
-    if (selection_item)
-      {
-        CGAL::Surface_mesh_simplification::Constrained_placement
-          <CGAL::Surface_mesh_simplification::Bounded_normal_change_placement
-           <CGAL::Surface_mesh_simplification::LindstromTurk_placement
-            <FaceGraph> >,
-           Scene_polyhedron_selection_item::Is_constrained_map
-           <Scene_polyhedron_selection_item::Selection_set_edge> >
-          placement (selection_item->constrained_edges_pmap());
+    Custom_stop_predicate stop(
+      (ui.m_combinatorial->currentIndex() == 0) && !(ui.m_use_nb_edges->isChecked()) && !(ui.m_use_edge_length->isChecked()),
+      (ui.m_use_nb_edges->isChecked() ? ui.m_nb_edges->value() : 0),
+      (ui.m_use_edge_length->isChecked() ? ui.m_edge_length->value() : (std::numeric_limits<double>::max)()));
 
-        CGAL::Surface_mesh_simplification::edge_collapse
-          (pmesh, stop,
-           CGAL::parameters::edge_is_constrained_map(selection_item->constrained_edges_pmap())
-           .get_placement(placement));
-      }
+    if(selection_item)
+    {
+      SMS::Constrained_placement<SMS::Bounded_normal_change_placement<SMS::LindstromTurk_placement<FaceGraph> >,
+        Scene_polyhedron_selection_item::Is_constrained_map<
+          Scene_polyhedron_selection_item::Selection_set_edge> > placement(selection_item->constrained_edges_pmap());
+
+      SMS::edge_collapse(pmesh, stop,
+                         CGAL::parameters::edge_is_constrained_map(selection_item->constrained_edges_pmap())
+                                          .get_placement(placement));
+    }
     else
-      {
-        CGAL::Surface_mesh_simplification::Bounded_normal_change_placement
-          <CGAL::Surface_mesh_simplification::LindstromTurk_placement
-           <FaceGraph> > placement;
+    {
+      SMS::Bounded_normal_change_placement<SMS::LindstromTurk_placement<FaceGraph> > placement;
 
-        CGAL::Surface_mesh_simplification::edge_collapse
-          (pmesh, stop,
-           CGAL::parameters::vertex_index_map(get(boost::vertex_index, pmesh)).get_placement(placement));
-      }
+      SMS::edge_collapse(pmesh, stop,
+                         CGAL::parameters::vertex_index_map(get(boost::vertex_index, pmesh))
+                                          .get_placement(placement));
+    }
 
-    std::cout << "ok (" << time.elapsed() << " ms, "
-      << num_halfedges(pmesh) / 2 << " edges)" << std::endl;
+    std::cout << "ok (" << time.elapsed() << " ms, " << num_halfedges(pmesh) / 2 << " edges)" << std::endl;
 
     // update scene
     if (poly_item != nullptr)
@@ -192,12 +191,12 @@ void CGAL_Lab_mesh_simplification_plugin::on_actionSimplify_triggered()
       poly_item->polyhedron()->collect_garbage();
     }
     else
-      {
+    {
       selection_item->polyhedron_item()->polyhedron()->collect_garbage();
       selection_item->poly_item_changed();
       selection_item->changed_with_poly_item();
       selection_item->invalidateOpenGLBuffers();
-      }
+    }
 
     scene->itemChanged(index);
     QApplication::restoreOverrideCursor();
