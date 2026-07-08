a954dcef0294e7eed1fa0b22deb652aa2fffb22e
Raphaël Costil <raphaelcostil@macbook-air-de-raphael-2.home>
2026-07-08 20:05:01 +0200

    Created the main file for the project

---
+++ Surface_mesh_topology/include/CGAL/Surface_mesh_topology/internal/Minimal_length_homotopy_basis.h
@@ -0,0 +1,11 @@
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+
+template <typename FaceGraph, typename OutputIterator>
+typename typename boost::graph_traits<FaceGraph>::vertex_descriptor vertex_descriptor;
+void min_length_homotopy_basis(const FaceGraph& mesh,
+												vertex_descriptor basepoint,
+												OutputIterator basis_out) {
+													
+}
