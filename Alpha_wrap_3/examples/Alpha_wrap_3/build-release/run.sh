b8731d01ff59869f4da9e86cc21382ada03d2701
Théo Tyburn <theo.tyburn@posteo.net>
2026-07-21 13:37:19 +0200

    Update experiment runner

--- Alpha_wrap_3/examples/Alpha_wrap_3/build-release/run.sh
+++ Alpha_wrap_3/examples/Alpha_wrap_3/build-release/run.sh
@@ -1,13 +1,22 @@
-for mesh in ~/projects/GSoC-CGAL/dev/models/convavities/*off; do
-    for ((alpha=50; alpha<=300; alpha+=50)); do
-		for ((offset=50; offset<=300; offset+=50)); do
-			./triangle_mesh_wrap $mesh $alpha $offset
-			output=$(echo $mesh | awk -F"/" '{ print $NF }')
-			output+=_
-			output+=$alpha
-			output+=_
-			output+=$offset
-			./stats.sh $output 1.2
+# Cleanup
+rm *off
+rm steps.csv
+rm -r results
+mkdir results
+root=$(pwd)
+
+# Run
+for mesh_path in ~/projects/GSoC-CGAL/dev/models/convavities/*off; do
+    for ((alpha=20; alpha<=200; alpha+=20)); do
+		for ((offset=20; offset<=200; offset+=20)); do
+			mesh_name=$(echo $mesh_path | awk -F"/" '{ print $NF }')
+			base_name=$(echo $mesh_name | awk -F"." '{ print $1 }')
+			output_name="$base_name"_"$alpha"_"$offset"
+			output_dir=$root/results/$output_name
+			mkdir $output_dir
+			cd $output_dir
+			RELAXED=1 SPHERE_MARCHING_OLD=1 DICHOTOMY=1 SPHERE_MARCHING=2\
+					  "$root"/triangle_mesh_wrap $mesh_path $alpha $offset
 		done        
     done
 done
